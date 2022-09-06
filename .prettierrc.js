d890fd80589349b5d1da92722eead967dcbd2d23
夕云葛城 <kuroneko7520@outlook.com>
2022-09-07 00:33:45 +0800

    chore: prettier 完善

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Options} */
+module.exports = {
+  /** 尽可能打印尾随逗号 */
+  trailingComma: 'all',
+  /** 在语句的末尾打印分号 */
+  semi: false,
+  /** 指定每个缩进级别的空格数 */
+  tabWidth: 2,
+  /** 使用单引号而不是双引号 */
+  singleQuote: true,
+  /** 应用哪个行尾字符 */
+  endOfLine: 'crlf',
+}
+
