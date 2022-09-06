68b6f8795b7c0ee8387f7ce8f9e8238742e7b9dd
夕云葛城 <kuroneko7520@outlook.com>
2022-09-07 01:34:19 +0800

    style: code format

--- environ.js
+++ environ.js
@@ -1,19 +1,18 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('path')
+const fs = require('fs')
 const dotenv = require('dotenv')
 
-let configPath = '';
-let configPathEnv = path.resolve(__dirname, './.env');
-let configPathEnvLocal = path.resolve(__dirname, './.env.local');
-let env = {
-};
+let configPath = ''
+let configPathEnv = path.resolve(__dirname, './.env')
+let configPathEnvLocal = path.resolve(__dirname, './.env.local')
+let env = {}
 
-configPath = fs.existsSync(configPathEnvLocal) ? configPathEnvLocal : configPathEnv
+configPath = fs.existsSync(configPathEnvLocal)
+  ? configPathEnvLocal
+  : configPathEnv
 
-env = dotenv
-  .config({
-    path: configPath
-  })
-  .parsed;
+env = dotenv.config({
+  path: configPath,
+}).parsed
 
-module.exports = env;
+module.exports = env
