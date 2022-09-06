708443bd39fad194780485e8113a42ddbf54b9ee
夕云葛城 <kuroneko7520@outlook.com>
2022-09-07 01:32:43 +0800

    chore: 完善单文件组件类型

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,10 @@
+/** 为单文件组件提供类型声明 */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent
+  export default component
+}
+
 /** 拓展 ENV 环境变量对象的类型 */
 interface ImportMetaEnv {
   /** 标题 */
