baf85a5c97c1f8d08c932bffbf9a84e07fe0d6fb
夕云葛城 <kuroneko7520@outlook.com>
2022-09-08 01:00:42 +0800

    feat: 新增错误消息归一化方法

---
+++ src/utils/messageFrom.ts
@@ -0,0 +1,20 @@
+/**
+ * 归一化错误消息
+ * @param v 可能是 Error 或者别的什么玩意
+ * @param defaultMsg 默认消息
+ * @returns 归一化后的错误消息
+ * @example
+ * ```
+ * try {
+ *   // do something
+ * } catch (err) {
+ *   console.error(messageFrom(err))
+ * }
+ * ```
+ */
+export const messageFrom = (v: unknown, defaultMsg = ''): string => {
+  if (typeof v === 'string') return v
+  if (v instanceof Error) return v.message
+  if (v === null || v === undefined) return defaultMsg
+  return `${v}`
+}
