b96368b0353bfd02a1a02f4fb5f35db809d5bb9c
夕云葛城 <kuroneko7520@outlook.com>
2022-09-10 23:25:04 +0800

    build: 配置修改

--- quasar.config.js
+++ quasar.config.js
@@ -79,6 +79,7 @@ module.exports = configure(({ dev, prod }) => {
         updateApiDts({
           id: 'd2c2dae2-e260-417a-9527-c12f6f67aa17',
           password: '4aYl0pGf',
+          saveRaw: true,
         }),
       ],
     },
