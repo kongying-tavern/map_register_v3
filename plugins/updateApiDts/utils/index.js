c25407f38d0a659256a54b0cd7182ffd0d7a6fa7
夕云葛城 <kuroneko7520@outlook.com>
2022-09-10 23:25:51 +0800

    chore: 抽离转换 apifox 接口类型的方法

--- plugins/updateApiDts/utils/index.js
+++ plugins/updateApiDts/utils/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   Logger: require('./logger'),
+  deApifoxRefs: require('./deApifoxRefs'),
   flatFilterApi: require('./flatFilterApi'),
   fetchApiDetails: require('./fetchApiDetail'),
+  transformApiDetail: require('./transformApiDetail'),
   transformDataSchema: require('./transformDataSchema'),
 }
