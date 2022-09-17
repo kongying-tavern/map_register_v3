914dcbe2c8ac7c8e9e1af4cddb652303fab8a6d0
夕云葛城 <kuroneko7520@outlook.com>
2022-09-17 12:58:36 +0800

    refactor: 路由配置结构优化

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,17 +1,32 @@
 import type { RouteRecordRaw } from 'vue-router'
 
+// TODO: 后期按模块抽离或做动态化
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('@/pages/index.vue'),
+    meta: {
+      title: 'Home',
+      icon: 'home',
+    },
+    component: () => import('@/layout/LayoutIndex.vue'),
+    redirect: '/users',
+    children: [
+      {
+        path: '/users',
+        meta: {
+          title: '用户管理',
+          icon: 'widgets',
+        },
+        component: () => import('@/pages/pageUserManager/UserManager.vue'),
+      },
+    ],
   },
   {
     path: '/login',
+    meta: {
+      title: '登录页',
+    },
     component: () => import('@/pages/pageLogin/PageLogin.vue'),
-  },
-  {
-    path: '/users',
-    component: () => import('@/pages/pageUserManager/UserManager.vue'),
   },
 ]
 
