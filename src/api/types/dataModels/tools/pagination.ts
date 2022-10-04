e1fdaa16b2e86424aa71c734cf34c6687f4a88ae
夕云葛城 <kuroneko7520@outlook.com>
2022-10-04 14:10:47 +0800

    chore: 类型名称修改

--- src/api/types/dataModels/tools/pagination.ts
+++ src/api/types/dataModels/tools/pagination.ts
@@ -1,5 +1,5 @@
 /** 分页请求 */
-export interface Pagination {
+export interface PaginationRequest {
   /**
    * 当前页，从0开始，从0开始
    */
@@ -8,4 +8,20 @@ export interface Pagination {
    * 每页大小，默认为10，默认为10
    */
   size?: number
+}
+
+/** 分页请求 */
+export interface PaginationResponse<T> {
+  /**
+   * 分页响应数据
+   */
+  record: T
+  /**
+   * 当前页数量
+   */
+  size: number
+  /**
+   * 总数
+   */
+  total: number
 }
