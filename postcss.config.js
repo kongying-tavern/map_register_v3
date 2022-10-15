4f489fe70a9926dc078bc099b1d90589f2d8d7c0
夕云葛城 <kuroneko7520@outlook.com>
2022-10-16 00:42:53 +0800

    chore: 格式化，删除不需要的依赖

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,18 @@
-/* eslint-disable */
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
   plugins: [
     // https://github.com/postcss/autoprefixer
-    require("autoprefixer")({
+    require('autoprefixer')({
       overrideBrowserslist: [
-        "last 4 Chrome versions",
-        "last 4 Firefox versions",
-        "last 4 Edge versions",
-        "last 4 Safari versions",
-        "last 4 Android versions",
-        "last 4 ChromeAndroid versions",
-        "last 4 FirefoxAndroid versions",
-        "last 4 iOS versions",
+        'last 4 Chrome versions',
+        'last 4 Firefox versions',
+        'last 4 Edge versions',
+        'last 4 Safari versions',
+        'last 4 Android versions',
+        'last 4 ChromeAndroid versions',
+        'last 4 FirefoxAndroid versions',
+        'last 4 iOS versions',
       ],
     }),
 
@@ -24,4 +23,4 @@ module.exports = {
     // 3. uncomment the following line:
     // require('postcss-rtlcss')
   ],
-};
+}
