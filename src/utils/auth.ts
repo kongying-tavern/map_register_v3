d87ac310fac4205fe45797da5cfc7c32d7df7d46
夕云葛城 <kuroneko7520@outlook.com>
2022-10-16 15:54:06 +0800

    reafactor: 鉴权信息本地存储重构完毕

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,3 @@
-import { LocalStorage } from 'quasar'
-
-const KEY_ACCESS_TOKEN = '__ys_dadian_access_token'
-const KEY_REFRESH_TOKEN = '__ys_dadian_refresh_token'
-const KEY_EXPIRES = '__ys_dadian_expires'
-const KEY_USER_ID = '__ys_dadian_user_id'
-const KEY_USER_ROLES = '__ys_dadian_user_roles'
-
 export interface UserAuthOptions {
   access_token?: string
   refresh_token?: string
@@ -15,30 +7,19 @@ export interface UserAuthOptions {
   userRoles?: string[]
 }
 
+const authInfo = useLocalStorage<Partial<UserAuthOptions>>('__ys_dadian_auth', {})
+
 /** 将用户凭证存储到本地 */
 export const saveUser = (auth: UserAuthOptions = {}) => {
-  const {
-    access_token = '',
-    refresh_token = '',
-    expires_in = 3600,
-    expiresTime = new Date().getTime() + expires_in * 1e3,
-    userId = '0',
-    userRoles = [],
-  } = auth
-
-  LocalStorage.set(KEY_ACCESS_TOKEN, access_token)
-  LocalStorage.set(KEY_REFRESH_TOKEN, refresh_token)
-  LocalStorage.set(KEY_EXPIRES, expiresTime)
-  LocalStorage.set(KEY_USER_ID, userId)
-  LocalStorage.set(KEY_USER_ROLES, userRoles)
+  authInfo.value = auth
 }
 
 export const getUserToken = () => {
-  return LocalStorage.getItem(KEY_ACCESS_TOKEN)
+  return authInfo.value.access_token
 }
 
 export const validateUserToken = (debug = false) => {
-  const expires: number | null = LocalStorage.getItem(KEY_EXPIRES)
+  const expires: number | null = authInfo.value.expiresTime ?? 0
   if (!expires || !getUserToken())
     return false
   debug
