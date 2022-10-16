b2c9565c3dbf11ea682e9ff40433fc9277bcdab0
夕云葛城 <kuroneko7520@outlook.com>
2022-10-16 15:53:13 +0800

    refactor: router 重构完毕

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,3 @@
-import { route } from 'quasar/wrappers'
 import {
   createMemoryHistory,
   createRouter,
@@ -9,40 +8,30 @@ import type { RouterHistory, RouterScrollBehavior } from 'vue-router'
 import routes from './routes'
 import { beforeEachGuard } from './guards'
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
-export default route((/* { store, ssrContext } */) => {
-  const history: RouterHistory = (
-    {
-      history: createWebHistory,
-      hash: createWebHashHistory,
-      memory: createMemoryHistory,
-    }[import.meta.env.VITE_ROUTER_MODE] ?? createWebHashHistory
-  )(import.meta.env.BASE_URL)
+const history: RouterHistory = (
+  {
+    history: createWebHistory,
+    hash: createWebHashHistory,
+    memory: createMemoryHistory,
+  }[import.meta.env.VITE_ROUTER_MODE] ?? createWebHashHistory
+)(import.meta.env.BASE_URL)
 
-  const scrollBehavior: RouterScrollBehavior = () => ({
-    top: 0,
-    left: 0,
-    behavior: 'smooth',
-  })
-
-  const Router = createRouter({
-    routes,
-    history,
-    scrollBehavior,
-  })
-
-  Router.beforeEach(
-    beforeEachGuard(Router, {
-      // debug: import.meta.env.DEV,
-    }),
-  )
-
-  return Router
+const scrollBehavior: RouterScrollBehavior = () => ({
+  top: 0,
+  left: 0,
+  behavior: 'smooth',
 })
+
+const router = createRouter({
+  routes,
+  history,
+  scrollBehavior,
+})
+
+router.beforeEach(
+  beforeEachGuard(router, {
+    // debug: import.meta.env.DEV,
+  }),
+)
+
+export { router }
