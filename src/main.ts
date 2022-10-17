26e49b35e20ca805b15db89c429cc01278b33f41
夕云葛城 <kuroneko7520@outlook.com>
2022-10-18 01:49:24 +0800

    feat: 添加 element-plus 国际化配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import App from './App.vue'
 import './style/app.scss'
 import { router } from '@/router'
@@ -7,5 +8,7 @@ import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(router)
-  .use(ElementPlus)
+  .use(ElementPlus, {
+    locale: zhCn,
+  })
   .mount('#app')
