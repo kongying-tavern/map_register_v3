c43eafd181b0d7bc66f86fd9fb7bf337293e3626
夕云葛城 <kuroneko7520@outlook.com>
2022-10-31 01:16:47 +0800

    feat: 物品列表 hook

--- src/pages/pageItemManager/ItemManager.vue
+++ src/pages/pageItemManager/ItemManager.vue
@@ -1,5 +1,20 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useItemList } from './hooks'
+
+const { itemlist, loading, updateList } = useItemList({
+  onSuccess: list => console.log('[物品列表]', list),
+  onError: err => console.log('[物品列表]', err),
+})
+</script>
 
 <template>
-  <div>物品管理</div>
+  <div class="h-full flex flex-col gap-2 overflow-hidden">
+    <div>
+      <el-button @click="updateList">
+        debug
+      </el-button>
+    </div>
+    <div>loading: {{ loading }}</div>
+    <div>len: {{ itemlist.length }}</div>
+  </div>
 </template>
