e7fbee3b9544a12201ea9c0f4b7b309d78bc30b1
夕云葛城 <kuroneko7520@outlook.com>
2022-10-31 01:10:22 +0800

    refactor: update api

--- src/api/api/markerDoc.ts
+++ src/api/api/markerDoc.ts
@@ -1,13 +1,13 @@
 import { request } from '@/utils';
 
-/** 返回点位分页bz2的md5数组 返回点位分页bz2的md5数组 GET /marker_doc/get/list_page_bz2_md5 */
+/** 返回点位分页bz2的md5数组 返回点位分页bz2的md5数组 GET /marker_doc/list_page_bz2_md5 */
 export async function listMarkerBz2MD5(
   params: {
     // header
 },
   options?: { [key: string]: any },
 ) {
-  return request<API.RListString>(`/api/marker_doc/get/list_page_bz2_md5`, {
+  return request<API.RListString>(`/api/marker_doc/list_page_bz2_md5`, {
     method: 'GET',
     headers: {},
     params: { ...params },
@@ -15,7 +15,7 @@ export async function listMarkerBz2MD5(
   });
 }
 
-/** 通过bz2返回点位分页 查询分页点位信息，返回bz2压缩格式的byte数组 GET /marker_doc/get/list_page_bz2/${param0} */
+/** 通过bz2返回点位分页 查询分页点位信息，返回bz2压缩格式的byte数组 GET /marker_doc/list_page_bz2/${param0} */
 export async function listPageMarkerBy7zip(
   params: {
     // header
@@ -25,7 +25,7 @@ export async function listPageMarkerBy7zip(
   options?: { [key: string]: any },
 ) {
   const { index: param0, ...queryParams } = params;
-  return request<string[]>(`/api/marker_doc/get/list_page_bz2/${param0}`, {
+  return request<string[]>(`/api/marker_doc/list_page_bz2/${param0}`, {
     method: 'GET',
     headers: {},
     params: { ...queryParams },
