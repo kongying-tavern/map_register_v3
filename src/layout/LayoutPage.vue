c14e55dab5706f85977413c6a8e593fdb9c86aff
夕云葛城 <kuroneko7520@outlook.com>
2022-12-01 01:17:55 +0800

    style: 优化样式组织

--- src/layout/LayoutPage.vue
+++ src/layout/LayoutPage.vue
@@ -6,12 +6,12 @@ const ctx = getCurrentInstance()
 </script>
 
 <template>
-  <div class="layout-page p-4" v-bind="$attrs">
+  <div class="flex flex-col p-4 overflow-hidden transition-all duration-200" v-bind="$attrs">
     <div v-if="ctx?.slots.header" class="layout-page__header">
       <slot name="header" />
     </div>
 
-    <div class="layout-page__main">
+    <div class="flex-1 overflow-hidden">
       <router-view>
         <template #default="{ Component, route }">
           <transition name="scale" mode="out-in" appear>
@@ -25,17 +25,3 @@ const ctx = getCurrentInstance()
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.layout-page {
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-  transition: width 0.2s ease, background-color 0.2s;
-
-  .layout-page__main {
-    flex: 1;
-    overflow: hidden;
-  }
-}
-</style>
