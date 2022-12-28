101fbd7299060da07c00245201f3565caea291cd
夕云葛城 <kuroneko7520@outlook.com>
2022-12-28 14:53:10 +0800

    feat: 新增枚举数据

---
+++ src/shared/hiddenFlagEnum.ts
@@ -0,0 +1,9 @@
+/** 隐藏标记枚举 */
+export enum HiddenFlagEnum {
+  /** 显示 */
+  SHOW = 0,
+  /** 隐藏 */
+  HIDDEN = 1,
+  /** 内鬼（测试服） */
+  NEIGUI = 2,
+}
---
+++ src/shared/iconStyleTypeEnum.ts
@@ -0,0 +1,11 @@
+/** 图标样式类型枚举 */
+export enum IconStyleTyleEnum {
+  /** 默认 */
+  DEFAULT = 0,
+  /** 无边框 */
+  NO_BORDER = 1,
+  /** 类神瞳 */
+  PUPIL = 2,
+  /** 类神瞳无对钩 */
+  NO_TICK = 3,
+}
