cde11b77674cf5bc53bebe2b9ad1b0b88676562c
夕云葛城 <kuroneko7520@outlook.com>
2023-01-01 02:00:52 +0800

    fix: console 清理

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,18 @@
+const debug = import.meta.env.DEV
+
 export class Logger {
   constructor(private prefix = '') {}
 
   info = (...args: any[]) => {
     if (import.meta.env.MODE !== 'development')
       return
-    console.log(this.prefix, ...args)
+    // eslint-disable-next-line no-console
+    debug && console.log(this.prefix, ...args)
   }
 
   error = (...args: any[]) => {
     if (import.meta.env.MODE !== 'development')
       return
-    console.error(this.prefix, ...args)
+    debug && console.error(this.prefix, ...args)
   }
 }
