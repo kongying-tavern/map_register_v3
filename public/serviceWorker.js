26adf1f4858eabb68fb776e67760004c70571ee2
夕云葛城 <kuroneko7520@outlook.com>
2023-02-18 00:09:26 +0800

    feat: 为 SW 添加分库缓存以及错误处理

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,22 +1,59 @@
+/** 强制等待 SW 激活 */
+self.oninstall = () => {
+  self.skipWaiting()
+}
+
+/** 设置为主域下的 controller */
+self.onactivate = (ev) => {
+  ev.waitUntil(clients.claim())
+}
+
 /**
  * 使用缓存优先策略拦截全局图片类请求
+ * @note 放置在 public 以便监听以 / 为基础路径发出的全部请求
  * @note `onfetch` 回调必须为同步函数
  * @todo 待封装
  */
 self.onfetch = (ev) => {
   const { url } = ev.request
 
-  if (!url.match(/^.+\.(png|jpeg|jpg|jxl|jp2|webp|bmp)$/)?.[1]) return
+  // 只拦截图片类型请求
+  if (url.match(/^.+\.(png|jpeg|jpg|jxl|jp2|webp|bmp)$/)?.['index'] !== 0)
+    return
 
-  // TODO 错误处理
+  // 只缓存以下几种情况
+  let cacheStorageName = undefined
+
+  // 1. 地图图片
+  if (url.match(/http(s?):\/\/assets.yuanshen.site\/tiles_/)?.['index'] === 0)
+    cacheStorageName = 'map-tiles'
+
+  // 2. 物品图标
+  else if (url.match(/http(s?):\/\/assets.yuanshen.site\/icons/)?.['index'] === 0)
+    cacheStorageName = 'item-icons'
+
+  // 3. 其他图片
+  else
+    cacheStorageName = 'other-image'
+  
   const cacheRequest = async () => {
-    const cache = await caches.match(url)
-    if (cache) return cache
-    const res = await fetch(ev.request)
-    const cacheStorage = await caches.open('v1')
-    cacheStorage.put(ev.request, res.clone())
-    return res
+    try {
+      // 如果匹配到缓存则直接返回
+      const cachedResult = await caches.match(ev.request)
+      if (cachedResult) {
+        ev.respondWith(cachedResult)
+        return
+      }
+  
+      // 如果匹配不到则拦截请求
+      const res = await fetch(ev.request)
+      ;(await caches.open(cacheStorageName)).put(ev.request, res.clone())
+      ev.respondWith(res)
+    }
+    catch (err) {
+      console.log(`[Service Worker] 缓存失败，已返回原始响应对象。${err.message}`)
+    }
   }
 
-  ev.respondWith(cacheRequest())
+  ev.waitUntil(cacheRequest)
 }
