51f222b168341f2feb38b40d4f0e69da5bcac618
夕云葛城 <kuroneko7520@outlook.com>
2023-02-18 15:09:06 +0800

    style: 圆角样式修改

--- src/components/AppUserAvatar.vue
+++ src/components/AppUserAvatar.vue
@@ -19,7 +19,7 @@ const handleCommand = (command: string) => ({
 </script>
 
 <template>
-  <el-dropdown trigger="click" @command="handleCommand">
+  <el-dropdown trigger="click" style="--el-border-radius-base: 8px" @command="handleCommand">
     <el-button v-bind="$attrs" text size="large" :style="{ padding: '4px 8px' }">
       <el-avatar :size="30" src="https://uploadstatic.mihoyo.com/contentweb/20210817/2021081714114216212.png" />
       <el-icon class="pl-1">
