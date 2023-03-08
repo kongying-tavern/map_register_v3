721a08ae556cefd26bafbd4388c3f99f7ae9afe3
夕云葛城 <kuroneko7520@outlook.com>
2023-03-08 15:07:08 +0800

    feat: 添加 GSCard 样式组件

---
+++ src/components/GenshinUI/GSCard.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+defineProps<{
+  title?: string
+}>()
+</script>
+
+<template>
+  <div class="gs-card-dark flex flex-col" v-bind="$attrs">
+    <slot name="header">
+      <div v-if="title" class="gs-card-title genshin-text text-center">
+        {{ title }}
+      </div>
+    </slot>
+
+    <slot name="default" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.gs-card-dark {
+  background: paint(dark-card-border);
+  padding: 36px;
+}
+
+.gs-card-title {
+  color: #D3BC8E;
+  font-size: 30px;
+}
+</style>
--- src/components/GenshinUI/index.ts
+++ src/components/GenshinUI/index.ts
@@ -1,4 +1,5 @@
 export { default as GSButton } from './GSButton.vue'
+export { default as GSCard } from './GSCard.vue'
 export { default as GSInput } from './GSInput.vue'
 export { default as GSTab } from './GSTab.vue'
 export { default as GSDivider } from './GSDivider.vue'
