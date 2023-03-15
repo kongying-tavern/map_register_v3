73af5f34683684ab8f7d8bcf25cf5b50148043ca
夕云葛城 <kuroneko7520@outlook.com>
2023-03-15 18:23:57 +0800

    refactor: 重构点位编辑逻辑使其符合权限设置

--- src/pages/pageMap/hooks/useMarkerEdit.ts
+++ src/pages/pageMap/hooks/useMarkerEdit.ts
@@ -1,46 +1,41 @@
 import type { Ref } from 'vue'
+import { ElMessage } from 'element-plus'
 import Api from '@/api/api'
+import { useUserStore } from '@/stores'
 import { useFetchHook } from '@/hooks'
 import db from '@/database'
 
-export interface MarkerFormData extends API.MarkerVo, API.MarkerPunctuateVo {}
+/** 编辑点位，已自动处理 version 和 methodType 字段 */
+export const useMarkerEdit = (markerData: Ref<API.MarkerVo>) => {
+  const userStore = useUserStore()
 
-export const useMarkerEdit = (markerData: Ref<MarkerFormData>) => {
-  const { refresh: createMarker, loading: createLoading, onSuccess: onCreateSuccess, onError: onCreateError } = useFetchHook({
-    onRequest: () => Api.marker.createMarker(markerData.value),
-  })
-
-  const { refresh: updateMarker, loading: editLoading, onSuccess: onEditSuccess, onError: onEditError } = useFetchHook({
+  const { refresh: editMarker, onSuccess, onError, ...rest } = useFetchHook({
     onRequest: async () => {
-      await Api.marker.updateMarker(markerData.value)
-      // 编辑成功后同步更新本地数据库
-      await db.marker.put(JSON.parse(JSON.stringify(markerData.value)), markerData.value.id)
+      const { id: markerId, version = 0 } = markerData.value
+      const { id: authorId } = userStore.info
+      if (markerId === undefined || authorId === undefined)
+        return
+
+      // 管理员直接编辑点位
+      if (userStore.isAdmin) {
+        await Api.marker.updateMarker(markerData.value)
+        await db.marker.put(JSON.parse(JSON.stringify(markerData.value)), markerId)
+        return
+      }
+
+      // 打点员提交编辑申请
+      const form: API.MarkerPunctuateVo = {
+        ...markerData.value,
+        version: version + 1,
+        methodType: 2,
+      }
+      await Api.punctuate.addPunctuate({}, form)
+      await Api.punctuate.pushPunctuate({ authorId })
     },
   })
 
-  const { refresh: deleteMarker, loading: deleteLoading, onSuccess: onDeleteSuccess, onError: onDeleteError } = useFetchHook({
-    onRequest: () => {
-      if (markerData.value.id === undefined)
-        throw new Error('点位 id 为空')
-      return Api.marker.deleteMarker({ markerId: markerData.value.id })
-    },
-  })
+  onSuccess(() => ElMessage.success(`${userStore.isAdmin ? '编辑点位' : '提交编辑点位申请'}成功`))
+  onError(err => ElMessage.error(err.message))
 
-  return {
-    markerData,
-    createLoading,
-    editLoading,
-    deleteLoading,
-
-    createMarker,
-    updateMarker,
-    deleteMarker,
-
-    onCreateSuccess,
-    onCreateError,
-    onEditSuccess,
-    onEditError,
-    onDeleteSuccess,
-    onDeleteError,
-  }
+  return { editMarker, onSuccess, onError, ...rest }
 }
