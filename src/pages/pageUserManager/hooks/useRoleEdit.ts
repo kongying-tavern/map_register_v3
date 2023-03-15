b50d82b65f3ba10af2b00d0ff2ca38d2b4c0698d
夕云葛城 <kuroneko7520@outlook.com>
2023-03-15 18:23:36 +0800

    fix: 修复类型错误

--- src/pages/pageUserManager/hooks/useRoleEdit.ts
+++ src/pages/pageUserManager/hooks/useRoleEdit.ts
@@ -1,8 +1,6 @@
-// import { ElMessage } from 'element-plus'
-// import { cloneDeep } from 'lodash'
 import type { Ref } from 'vue'
+import { omit } from 'lodash'
 import { UserPasswordEditor } from '../components'
-// import { messageFrom } from '@/utils'
 import Api from '@/api/api'
 import { useGlobalDialog } from '@/hooks'
 import { useRowEdit } from '@/hooks/useTableManipulation'
@@ -18,8 +16,8 @@ export const useRoleEdit = (options: RoleEditHookOptions) => {
   const editOptions = useRowEdit({
     rowList: userList,
     saveHandler: async (_, newRowData, oldRowData) => {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { id: userId, username, roleList = [], ...rest } = newRowData
+      const { id: userId, roleList = [] } = newRowData
+      const rest = omit(newRowData, 'id', 'username', 'roleList')
       // 更新用户信息需要的全部请求
       const mission = [
         // 修改用户信息
@@ -39,7 +37,7 @@ export const useRoleEdit = (options: RoleEditHookOptions) => {
 
   const openPwdEditorDialog = async (index: number) => {
     await DialogService
-      .config({ title: '修改密码', width: '400px' })
+      .config({ title: '修改密码', width: '400px', alignCenter: true })
       .props({ user: userList.value[index] })
       .open(UserPasswordEditor)
       .afterClosed<boolean>()
