3336f9eaa72696981eec3f175380f3d61091fac9
夕云葛城 <kuroneko7520@outlook.com>
2023-03-17 19:51:03 +0800

    refactor: 删除多余文件

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-export {}
-
-/**
- * 注册函数
- * @link https://developer.mozilla.org/en-US/docs/Web/API/PaintWorklet/registerPaint
- */
-declare global {
-  const registerPaint: (name: string, classRef: typeof Painter) => void
-}
