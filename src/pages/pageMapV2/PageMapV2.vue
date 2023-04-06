1c47ca1956052c114bdde35c6f7bbc56871a54d9
夕云葛城 <kuroneko7520@outlook.com>
2023-04-07 00:04:54 +0800

    fix: 抽离 useMap

--- src/pages/pageMapV2/PageMapV2.vue
+++ src/pages/pageMapV2/PageMapV2.vue
@@ -1,46 +1,13 @@
 <script lang="ts" setup>
 import { Filter, Setting } from '@element-plus/icons-vue'
-import { GenshinMap } from './core'
 import { LAYER_CONFIGS } from './config'
-import { CollapseButton, SiderMenu, SiderMenuItem } from './components'
-import { Logger } from '@/utils'
+import { useMap } from './hooks'
+import { CollapseButton, MarkerFilter, SiderMenu, SiderMenuItem } from './components'
 import { AppUserAvatar } from '@/components'
-
-const logger = new Logger('[MapV2]')
 
 const canvasRef = ref<HTMLCanvasElement | null>(null)
 
-const map = shallowRef<GenshinMap | null>(null)
-
-const baseLayerCode = computed({
-  get: () => map.value?.baseLayerCode,
-  set: v => map.value && (map.value.setBaseLayer(v)),
-})
-
-const showBorder = computed({
-  get: () => map.value?.showBorder ?? false,
-  set: v => map.value && (map.value.showBorder = v),
-})
-
-const showTag = computed({
-  get: () => map.value?.showTag ?? false,
-  set: v => map.value && (map.value.showTag = v),
-})
-
-const initMap = async () => {
-  if (!canvasRef.value)
-    return
-
-  map.value = new GenshinMap({
-    canvas: canvasRef.value,
-  })
-
-  map.value.ready.then((readyMap) => {
-    logger.info('map is ready', readyMap)
-    baseLayerCode.value = LAYER_CONFIGS[0].code
-  })
-}
-onMounted(initMap)
+const { baseLayerCode, showBorder, showTag, showTooltip } = useMap(canvasRef)
 
 const collapse = ref(true)
 useEventListener('keypress', (ev) => {
@@ -61,7 +28,7 @@ const tabName = ref('filter')
 
     <SiderMenu v-model:collapse="collapse" v-model="tabName">
       <SiderMenuItem name="filter" label="筛选" :icon="Filter">
-        点位筛选器
+        <MarkerFilter />
       </SiderMenuItem>
 
       <SiderMenuItem name="setting" label="设置" :icon="Setting">
@@ -76,6 +43,7 @@ const tabName = ref('filter')
           </el-select>
           <el-switch v-model="showBorder" inline-prompt active-text="显示边框" inactive-text="隐藏边框" />
           <el-switch v-model="showTag" inline-prompt active-text="显示标签" inactive-text="隐藏标签" />
+          <el-switch v-model="showTooltip" inline-prompt active-text="显示坐标" inactive-text="隐藏坐标" />
         </div>
       </SiderMenuItem>
     </SiderMenu>
---
+++ src/pages/pageMapV2/hooks/useMap.ts
@@ -0,0 +1,50 @@
+import type { Ref } from 'vue'
+import { GenshinMap } from '@/pages/pageMapV2/core'
+import { Logger } from '@/utils'
+import { LAYER_CONFIGS } from '@/pages/pageMapV2/config'
+
+const map = shallowRef<GenshinMap>()
+
+export const useMap = (canvasRef?: Ref<HTMLCanvasElement | null>) => {
+  const baseLayerCode = computed({
+    get: () => map.value?.baseLayerCode,
+    set: v => map.value && (map.value.setBaseLayer(v)),
+  })
+
+  const showBorder = computed({
+    get: () => map.value?.showBorder ?? false,
+    set: v => map.value && (map.value.showBorder = v),
+  })
+
+  const showTag = computed({
+    get: () => map.value?.showTag ?? false,
+    set: v => map.value && (map.value.showTag = v),
+  })
+
+  const showTooltip = computed({
+    get: () => map.value?.showTooltip ?? false,
+    set: v => map.value && (map.value.showTooltip = v),
+  })
+
+  const logger = new Logger('[MapV2]')
+
+  const initMap = () => {
+    if (!canvasRef?.value || map.value)
+      return
+
+    const newMap = new GenshinMap({
+      canvas: canvasRef.value,
+    })
+
+    newMap.ready.then((readyMap) => {
+      logger.info('map is ready', readyMap)
+      baseLayerCode.value = LAYER_CONFIGS[0].code
+    })
+
+    map.value = newMap
+  }
+
+  onMounted(initMap)
+
+  return { map, baseLayerCode, showBorder, showTag, showTooltip }
+}
