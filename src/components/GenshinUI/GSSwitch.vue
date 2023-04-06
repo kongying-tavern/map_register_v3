2d0e4049f0e84fd71bbcb14f7459ff5cd4727d0e
夕云葛城 <kuroneko7520@outlook.com>
2023-04-06 13:16:17 +0800

    feat: GSSwitch

---
+++ src/components/GenshinUI/GSSwitch.vue
@@ -0,0 +1,106 @@
+<script lang="ts" setup>
+import { CloseBold, Select } from '@element-plus/icons-vue'
+
+export interface GSSwitchProps {
+  modelValue?: boolean
+  size?: 'small' | 'large'
+}
+
+export interface GSSwitchEmits {
+  (e: 'update:modelValue', v?: boolean): void
+}
+
+defineProps<GSSwitchProps>()
+defineEmits<GSSwitchEmits>()
+</script>
+
+<template>
+  <div
+    class="gs-switch"
+    :class="{
+      actived: modelValue,
+      [`size-${size ?? 'default'}`]: true,
+    }"
+    @click="$emit('update:modelValue', !modelValue)"
+  >
+    <div class="gs-switch__slider">
+      <Transition name="pure-scale" appear>
+        <component :is="modelValue ? Select : CloseBold" class="absolute" style="padding: calc(0.12 * var(--height))" />
+      </Transition>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@mixin sliderBackground() {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+}
+
+.gs-switch {
+  --width: 50px;
+  --height: 25px;
+  --border-width: 1px;
+  --bg: #353D4F;
+  --slider-tr-x: 0;
+  --icon-color: #D3BC8E;
+
+  &.size-small {
+    --width: 40px;
+    --height: 20px;
+  }
+  &.size-large {
+    --width: 60px;
+    --height: 30px;
+  }
+
+  width: var(--width);
+  height: var(--height);
+  border: var(--border-width) solid #EBE4D8;
+  border-radius: var(--height);
+  padding: calc(0.08 * var(--height));
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+  user-select: none;
+  color: var(--icon-color);
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background: var(--bg);
+    transition: all ease 150ms;
+    z-index: -1;
+    border: calc(0.04 * var(--height)) solid rgb(100 100 100 / 0.2);
+    border-radius: var(--height);
+  }
+
+  &.actived {
+    --slider-tr-x: calc(var(--width) - var(--height));
+    --active-bg-scale: 1;
+    --inactive-bg-scale: 0;
+    --bg: #D3BC8E;
+    --bg-border-color: #BFA57A;
+    --icon-color: #818D9D;
+  }
+}
+
+.gs-switch__slider {
+  height: 100%;
+  aspect-ratio: 1 / 1;
+  border-radius: 100%;
+  background-color: #FFF8DE;
+  transition: all ease 150ms;
+  translate: var(--slider-tr-x) 0;
+  filter: drop-shadow(0 0 1.5px #FFF8DE);
+  position: relative;
+}
+</style>
--- src/components/GenshinUI/index.ts
+++ src/components/GenshinUI/index.ts
@@ -1,5 +1,6 @@
 export { default as GSButton } from './GSButton.vue'
 export { default as GSCard } from './GSCard.vue'
 export { default as GSInput } from './GSInput.vue'
+export { default as GSSwitch } from './GSSwitch.vue'
 export { default as GSTab } from './GSTab.vue'
 export { default as GSDivider } from './GSDivider.vue'
