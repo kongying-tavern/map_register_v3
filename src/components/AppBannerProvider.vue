bfc67692a2762393d7094a8e794d9e6b43655874
夕云葛城 <kuroneko7520@outlook.com>
2023-04-17 23:36:35 +0800

    style: 为全局 el-overlay 添加当 banner 出现时的顶部偏移量

--- src/components/AppBannerProvider.vue
+++ src/components/AppBannerProvider.vue
@@ -1,12 +1,17 @@
 <script lang="ts" setup>
 import { content, visible } from '@/hooks/useBanner/bannerContext'
+
+const height = useCssVar('--gs-banner-height')
+watch(visible, (isVisibile) => {
+  height.value = isVisibile ? '32px' : '0px'
+}, { immediate: true })
 </script>
 
 <template>
   <Transition name="draw-y" mode="out-in" appear>
     <div
       v-show="visible"
-      class="genshin-text absolute w-full top-0 text-md p-1 px-2 text-center"
+      class="genshin-text absolute w-full top-0 text-md p-1 h-8 px-2 text-center"
       :style="{
         background: '#E2DED5',
         color: '#4A4F50',
