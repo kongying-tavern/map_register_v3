674c0c41ae77c04b15cc7bc27abeb27497932bc7
夕云葛城 <kuroneko7520@outlook.com>
2023-04-21 01:36:45 +0800

    feat: 完善点位抽屉

--- src/pages/pageMapV2/components/MarkerDrawer/MarkerDrawer.vue
+++ src/pages/pageMapV2/components/MarkerDrawer/MarkerDrawer.vue
@@ -1,47 +1,189 @@
 <script lang="ts" setup>
-import { useMap } from '@/pages/pageMapV2/hooks'
+import { GSButton } from '@/components'
+import { useMarkerDrawer } from '@/pages/pageMapV2/hooks'
+import db from '@/database'
 
-const { map } = useMap()
+const { focus, blur } = useMarkerDrawer()
 
-const isMarkerVo = (v: unknown): v is API.MarkerVo => {
-  return v !== null && typeof v === 'object' && 'markerTitle' in v
-}
+const containerRef = ref<HTMLElement>()
+onClickOutside(containerRef, blur)
 
-const marker = computed(() => {
-  if (!map.value || !isMarkerVo(map.value.focus))
-    return
-  return map.value.focus
-})
+const icon = asyncComputed(() => {
+  const iconTag = focus.value?.itemList?.[0]?.iconTag ?? 'unknown'
+  return db.iconTag.where('tag').equals(iconTag).first()
+}, {})
 
-const blurMarker = () => {
-  if (!map.value)
-    return
-  map.value.focus = null
-  map.value.active = null
-  map.value.hover = null
-}
+const area = asyncComputed(async () => {
+  const itemId = focus.value?.itemList?.[0]?.itemId ?? -9999
+  const item = await db.item.get(itemId)
+  if (!item)
+    return {}
+  return await db.area.get(item.areaId as number)
+}, {})
+
+const parentArea = asyncComputed(() => {
+  const parentId = area.value?.parentId ?? -9999
+  return db.area.get(parentId)
+}, {})
 
 const { width } = useWindowSize()
 </script>
 
 <template>
   <el-drawer
-    :model-value="Boolean(marker)"
-    :title="`点位：${marker?.markerTitle}`"
-    class="marker-info-drawer"
+    v-if="focus"
+    :model-value="Boolean(focus)"
+    :title="`点位：${focus?.markerTitle}`"
+    :size="width < 420 ? width : 420"
+    :modal="false"
+    :with-header="false"
     append-to-body
-    :size="width < 400 ? width : 400"
-    @close="blurMarker"
+    class="genshin-marker-drawer genshin-text"
+    @close="blur"
   >
-    {{ marker }}
+    <div ref="containerRef" class="genshin-marker-drawer__wrapper">
+      <div class="genshin-marker-drawer__header">
+        <div class="marker-title">
+          <img v-if="icon" class="w-8 h-8 object-contain" crossorigin="" :src="icon.url">
+          {{ focus.markerTitle }}
+        </div>
+        <div class="close-button" @click="blur">
+          <svg viewBox="0 0 100 100">
+            <path fill="currentColor" d="m 0 0 l 34 5 l -7 7 l 23 23 l 23 -23 l -7 -7 l 34 -5 l -5 34 l -7 -7 l -23 23 l 23 23 l 7 -7 l 5 34 l -34 -5 l 7 -7 l -23 -23 l -23 23 l 7 7 l -34 5 l 5 -34 l 7 7 l 23 -23 l -23 -23 l -7 7 z" />
+          </svg>
+        </div>
+      </div>
+
+      <div class="genshin-marker-drawer__body">
+        <img v-if="focus.picture" class="w-full h-44 object-cover" :src="focus.picture">
+
+        <div class="marker-info">
+          <div class="info-area flex items-center gap-2">
+            <el-icon color="#676A74" :size="24">
+              <LocationFilled />
+            </el-icon>
+            {{ parentArea?.name }} - {{ area?.name }}
+          </div>
+
+          <div class="flex-1">
+            <p v-for="p in focus.content?.split('\n')" :key="p">
+              {{ p }}
+            </p>
+          </div>
+
+          <div class="flex items-center gap-4">
+            <GSButton dark class="flex-1">
+              <template #icon>
+                <el-icon color="#DAAF32">
+                  <Edit />
+                </el-icon>
+              </template>
+              编辑
+            </GSButton>
+            <GSButton dark class="flex-1">
+              <template #icon>
+                <el-icon color="var(--gs-color-danger)">
+                  <DeleteFilled />
+                </el-icon>
+              </template>
+              删除
+            </GSButton>
+          </div>
+        </div>
+      </div>
+    </div>
   </el-drawer>
 </template>
 
 <style lang="scss">
-.el-drawer.marker-info-drawer {
+.el-drawer.genshin-marker-drawer {
   transition: all ease 150ms;
+  background: transparent;
+
+  &.rtl {
+    top: var(--gs-banner-height);
+    height: calc(100% - var(--gs-banner-height));
+  }
+
   .el-drawer__body {
     padding: 0;
+  }
+
+  .genshin-marker-drawer__wrapper {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    background: #D9D3C8F4;
+  }
+
+  .genshin-marker-drawer__header {
+    display: flex;
+    justify-content: flex-end;
+    padding: 16px;
+    background: #3D4555;
+
+    .marker-title {
+      flex: 1;
+      color: #D3BC8E;
+      font-size: 24px;
+      line-height: 1;
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+    }
+
+    .close-button {
+      width: 32px;
+      height: 32px;
+      color: #EBE4D7;
+      transition: all ease 150ms;
+      mask: radial-gradient(#FFF 68%, #FFFFFFE0);
+      cursor: pointer;
+      &:hover {
+        opacity: 0.9;
+      }
+      &:active {
+        opacity: 0.5;
+      }
+    }
+  }
+
+  .genshin-marker-drawer__body {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    position: relative;
+
+    .marker-info {
+      --border-width: 2px;
+
+      flex: 1;
+      padding: 1rem;
+      display: flex;
+      gap: 8px;
+      flex-direction: column;
+      position: relative;
+      color: #686D7A;
+      font-size: 20px;
+
+      &::before {
+        position: absolute;
+        content: '';
+        border: var(--border-width) solid #3D455520;
+        left: var(--border-width);
+        top: var(--border-width);
+        width: calc(100% - 2 * var(--border-width));
+        height: calc(100% - 2 * var(--border-width));
+        pointer-events: none;
+      }
+    }
+
+    .info-area {
+      background: #D6C7AB;
+      padding: 4px 8px;
+    }
   }
 }
 </style>
--- src/pages/pageMapV2/hooks/useMarkerDrawer.ts
+++ src/pages/pageMapV2/hooks/useMarkerDrawer.ts
@@ -1,39 +1,53 @@
+import type { PickingInfo } from '@deck.gl/core/typed'
 import type { Ref } from 'vue'
+import type { MarkerWithExtra } from '../core'
 import { useMap } from '@/pages/pageMapV2/hooks'
 
-export const useMarkerDrawer = (canvasRef: Ref<HTMLCanvasElement | null>) => {
+const focus = shallowRef<MarkerWithExtra | null>(null)
+const triggerTimestamp = ref(new Date().getTime())
+
+const isMarkerVo = (v: unknown): v is MarkerWithExtra => {
+  if (typeof v !== 'object' || v === null)
+    return false
+  return ['markerTitle', 'position', 'content', 'extraObject'].every(property => property in v)
+}
+
+export const useMarkerDrawer = (canvasRef?: Ref<HTMLCanvasElement | null>) => {
   const { map } = useMap()
 
-  const hasBeenDragged = ref(false)
-
-  useEventListener(canvasRef, 'pointerdown', () => {
-    const stopListenMove = useEventListener('pointermove', () => {
-      if (!map.value)
-        return
-      hasBeenDragged.value = true
-      map.value.hover = null
-    })
-    useEventListener('pointerup', () => {
-      stopListenMove()
-    }, { once: true })
-    if (!map.value?.hover)
+  const handleMapClick = (info: PickingInfo, ev: { srcEvent: Event }) => {
+    if (!(ev.srcEvent instanceof PointerEvent) || ev.srcEvent.button !== 0)
       return
-    map.value.active = map.value.hover
+    triggerTimestamp.value = new Date().getTime()
+    focus.value = isMarkerVo(info.object) ? info.object : null
+    map.value && (map.value.focus = focus.value)
+  }
+
+  const blur = () => {
+    const current = new Date().getTime()
+    if (current - triggerTimestamp.value < 20)
+      return
+    focus.value = null
+    map.value && (map.value.focus = focus.value)
+  }
+
+  canvasRef && onMounted(async () => {
+    await nextTick()
+    map.value && (map.value.event.on('click', handleMapClick))
   })
 
-  useEventListener(canvasRef, 'pointerup', () => {
-    if (!map.value)
-      return
-    map.value.active = map.value.hover
+  canvasRef && onBeforeUnmount(async () => {
+    await nextTick()
+    map.value && (map.value.event.off('click', handleMapClick))
   })
 
-  useEventListener(canvasRef, 'click', () => {
-    if (!map.value)
-      return
-    if (hasBeenDragged.value) {
-      hasBeenDragged.value = false
-      return
-    }
-    map.value.focus = map.value?.hover ?? null
+  canvasRef && useEventListener(canvasRef, 'pointerdown', () => {
+    map.value && (map.value.active = map.value.hover)
   })
+
+  canvasRef && useEventListener('pointerup', () => {
+    map.value && (map.value.active = null)
+  })
+
+  return { focus, blur }
 }
