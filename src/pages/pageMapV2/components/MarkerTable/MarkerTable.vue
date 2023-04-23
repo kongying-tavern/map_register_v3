9a5dc02db16e90a543066d216cc2fe7b7016e245
夕云葛城 <kuroneko7520@outlook.com>
2023-04-24 02:25:32 +0800

    feat: 点位列表（未完成）

--- src/pages/pageMapV2/components/MarkerTable/MarkerTable.vue
+++ src/pages/pageMapV2/components/MarkerTable/MarkerTable.vue
@@ -1,17 +1,36 @@
 <script lang="ts" setup>
-import { useCondition } from '@/pages/pageMapV2/hooks'
+import type { Column } from 'element-plus'
+import type { MarkerWithExtra } from '../../core'
+import { useCurrentLayerMarkers } from '@/pages/pageMapV2/hooks'
 
-const conditionManager = useCondition()
+const tableContainerRef = ref<HTMLElement | null>()
+const { width, height } = useElementSize(tableContainerRef)
 
-const test = () => {
-  console.log(Object.fromEntries(conditionManager.conditions.entries()))
-}
+const { markers } = useCurrentLayerMarkers()
+
+const columns = ref<Column<MarkerWithExtra>[]>([
+  { title: 'id', dataKey: 'id', width: 80 },
+  { title: '名称', dataKey: 'markerTitle', width: 100 },
+])
 </script>
 
 <template>
   <div class="marker-filter h-full flex flex-col">
-    <el-button @click="test">
-      打印点位
-    </el-button>
+    <div ref="tableContainerRef" class="flex-1 overflow-hidden">
+      <el-table-v2
+        :columns="columns"
+        :data="markers"
+        :width="width"
+        :height="height"
+        class="marker-table"
+      />
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.marker-table {
+  --el-bg-color: transparent;
+  --el-table-bg-color: transparent;
+}
+</style>
