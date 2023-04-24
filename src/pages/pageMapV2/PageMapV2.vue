fb25ac88bf0d8ab2131989f4e1280d41d5d8913f
夕云葛城 <kuroneko7520@outlook.com>
2023-04-25 02:39:22 +0800

    feat: 添加部分属性设置器到地图界面

--- src/pages/pageMapV2/PageMapV2.vue
+++ src/pages/pageMapV2/PageMapV2.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 import { useMap, useMarkerDrawer } from './hooks'
 import { CollapseButton, MapSiderMenu, MarkerDrawer } from './components'
+import { GSSwitch } from '@/components'
 
 const canvasRef = ref<HTMLCanvasElement | null>(null)
-useMap(canvasRef)
+const { showTag, showUndergroundLayer, showBorder, showTooltip } = useMap(canvasRef)
 
 const collapse = ref(true)
 useEventListener('keypress', (ev) => {
@@ -20,6 +21,13 @@ useMarkerDrawer(canvasRef)
 
     <div class="absolute top-2 left-2">
       <CollapseButton v-model:collapse="collapse" />
+    </div>
+
+    <div class="absolute bottom-2 left-2 flex flex-col gap-2 invisible sm:visible">
+      <GSSwitch v-model="showTag" label="显示地图标签" size="large" />
+      <GSSwitch v-model="showUndergroundLayer" label="显示地下图层" size="large" />
+      <GSSwitch v-model="showBorder" label="显示图层边界" size="large" />
+      <GSSwitch v-model="showTooltip" label="显示调试信息" size="large" />
     </div>
 
     <MapSiderMenu v-model:collapse="collapse" />
