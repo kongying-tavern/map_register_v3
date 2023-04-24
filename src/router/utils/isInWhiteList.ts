c1961b2d287f27ba5c7040471a0041aedc7d9461
夕云葛城 <kuroneko7520@outlook.com>
2023-04-25 01:24:30 +0800

    feat: 为地图页添加访问白名单

--- src/router/utils/isInWhiteList.ts
+++ src/router/utils/isInWhiteList.ts
@@ -4,6 +4,8 @@ import type { RouteLocationNormalized } from 'vue-router'
 export const ROUTE_WHITE_LIST = Object.freeze([
   '/login',
   '/register',
+  '/map',
+  '/map-v2',
 ])
 
 /** 判断跳转目标是否处于白名单内 */
