7ecc90a1d9471237bfd989cfb44bb096362ce53a
夕云葛城 <kuroneko7520@outlook.com>
2023-05-12 02:06:03 +0800

    feat: 存档分析组件（未完成）

--- src/components/AppUserInfo/ArchiveAnalyser.vue
+++ src/components/AppUserInfo/ArchiveAnalyser.vue
@@ -1,58 +1,106 @@
 <script lang="ts" setup>
+import type { Ref } from 'vue'
+import { liveQuery } from 'dexie'
 import db from '@/database'
+import { useFetchHook } from '@/hooks'
+import { useArchiveStore } from '@/stores'
 
-/** 该组件为存档数据分析和可视化组件 */
+const archiveStore = useArchiveStore()
 
-const props = defineProps<{
-  archiveBody: {
-    Data_KYJG: Set<number>
-    Time_KYJG: Record<number, string>
+interface GroupedMarkers {
+  [key: string]: {
+    area: API.AreaVo
+    markers: API.MarkerVo[]
   }
-}>()
-
-interface MarkerWithArea extends API.MarkerVo {
-  itemId: number
-  country: string
 }
 
-/** 为每个点位匹配其地区数据 */
-const matchMarkerArea = async (markerIds: number[]) => {
+const isMarkerInstances = (v: (number[]) | (API.MarkerVo[])): v is API.MarkerVo[] => typeof v[0] !== 'number'
+
+/** 为每个宝箱点位匹配其地区数据 */
+const matchMarkerArea = async (markerParams: (number[]) | (API.MarkerVo[]), handleRef: Ref<GroupedMarkers>) => {
   /** 需要查询的物品 id */
   const queryItemIds = new Set<number>()
 
-  const markers = ((await db.marker.bulkGet(markerIds)).filter(Boolean) as API.MarkerVo[]).map((marker) => {
-    const itemId = marker.itemList?.[0]?.itemId ?? -1
-    queryItemIds.add(itemId)
-    return {
-      ...marker,
-      itemId: marker.itemList?.[0].itemId ?? -1,
-      country: '',
-    } as MarkerWithArea
-  })
-
-  /** 物品与地区映射表 */
-  const itemAreaMap = (await db.item.bulkGet([...queryItemIds])).reduce((seed, item) => {
-    item && (seed[item.id as number] = item.areaId as number)
-    return seed
-  }, {} as Record<number, number>)
-
-  /** 地区 id 与国家代码映射表 */
-  const countries = (await db.area.bulkGet([...new Set(Object.values(itemAreaMap))])).reduce((seed, area) => {
-    seed[area?.id as number] = area?.code?.split(':')?.[1] ?? 'UNKNOWN'
-    return seed
-  }, {} as Record<number, string>)
-
+  const markers = isMarkerInstances(markerParams) ? markerParams : await db.marker.bulkGet(markerParams)
   markers.forEach((marker) => {
-    marker.country = countries[itemAreaMap[marker.itemId]]
+    const itemId = marker?.itemList?.[0]?.itemId
+    itemId !== undefined && queryItemIds.add(itemId)
   })
+
+  const treasureChestType = await db.itemType.where('name').equals('宝箱品质').first()
+  if (!treasureChestType)
+    return {}
+
+  // 宝箱品质类型对应的物品
+  const items = await db.item.where('typeIdList').equals(treasureChestType.id as number).toArray()
+  const itemMap = items.reduce((seed, item) => {
+    seed[item.id as number] = item
+    return seed
+  }, {} as Record<number, API.ItemVo>)
+
+  const areas = await db.area.toArray()
+  const areaMap = areas.reduce((seed, area) => {
+    area !== undefined && (seed[area.id as number] = area)
+    return seed
+  }, {} as Record<string, API.AreaVo>)
+
+  const res = markers.reduce((seed, marker) => {
+    if (marker === undefined)
+      return seed
+
+    // 检查点位是否为宝箱类型
+    const findItem = marker.itemList?.find(item => (item.itemId as number) in itemMap)
+    if (!findItem)
+      return seed
+
+    const markerItem = itemMap[findItem.itemId as number]
+    const markerArea = areaMap[markerItem.areaId as number]
+    if (markerArea === undefined)
+      return seed
+
+    const parent = areaMap[markerArea.parentId as number]
+    const areaParentCode = parent?.code
+    if (areaParentCode === undefined)
+      return seed
+
+    if (!seed[areaParentCode]) {
+      seed[areaParentCode] = {
+        area: parent,
+        markers: [],
+      }
+    }
+    seed[areaParentCode].markers.push(marker)
+    return seed
+  }, {} as GroupedMarkers)
+
+  handleRef.value = res
 }
 
-onMounted(() => matchMarkerArea([...props.archiveBody.Data_KYJG]))
+const rawMarkerGroup = ref<GroupedMarkers>({})
+liveQuery(() => db.marker.toArray()).subscribe((markers) => {
+  matchMarkerArea(markers, rawMarkerGroup)
+})
+
+const markersGroup = ref<GroupedMarkers>({})
+const { refresh: analyse, loading } = useFetchHook({
+  onRequest: () => matchMarkerArea([...archiveStore.currentArchive.body.Data_KYJG], markersGroup),
+})
+
+watch(() => archiveStore.currentArchive.body.Data_KYJG.size, analyse, { immediate: true })
 </script>
 
 <template>
   <div class="archive-analyser">
-    存档数据
+    <div v-if="loading">
+      分析中...
+    </div>
+
+    <div v-else class="flex flex-col">
+      <div>一级地区已标记的宝箱数</div>
+      <div v-for="(area, code) in markersGroup" :key="code">
+        {{ area.area.name }}: {{ area.markers.length }} / {{ rawMarkerGroup[code].markers.length }}
+      </div>
+    </div>
   </div>
 </template>
 
