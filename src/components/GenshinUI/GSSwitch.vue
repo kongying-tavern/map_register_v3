424ea00c8b3a71c97ec9785f726fce7a49151f68
夕云葛城 <kuroneko7520@outlook.com>
2023-05-16 18:42:41 +0800

    style: switch 组件样式优化

--- src/components/GenshinUI/GSSwitch.vue
+++ src/components/GenshinUI/GSSwitch.vue
@@ -5,6 +5,9 @@ export interface GSSwitchProps {
   modelValue?: boolean
   size?: 'small' | 'large'
   label?: string
+  labelPosition?: 'left' | 'right'
+  labelActiveColor?: string
+  labelInactiveColor?: string
 }
 
 export interface GSSwitchEmits {
@@ -17,11 +20,16 @@ defineEmits<GSSwitchEmits>()
 
 <template>
   <div
-    class="gs-switch genshin-text"
+    class="gs-switch gs-switch-variable genshin-text"
     :class="{
       actived: modelValue,
       label,
+      [`label-position-${labelPosition ?? 'right'}`]: true,
       [`size-${size ?? 'default'}`]: true,
+    }"
+    :style="{
+      '--label-inactive-color': labelInactiveColor ?? '#CCC',
+      '--label-active-color': labelActiveColor ?? '#D3BC8E',
     }"
     :data-label="label"
     @click="$emit('update:modelValue', !modelValue)"
@@ -44,15 +52,15 @@ defineEmits<GSSwitchEmits>()
   top: 0;
 }
 
-.gs-switch {
+.gs-switch-variable {
   --width: 50px;
   --height: 25px;
   --border-width: 1px;
   --bg: #353D4F;
   --slider-tr-x: 0;
   --icon-color: #D3BC8E;
-  --label-color: #CCC;
-  --label-size: 16px;
+  --label-color: var(--label-inactive-color);
+  --label-font-size: 16px;
 
   &.size-small {
     --width: 40px;
@@ -61,9 +69,11 @@ defineEmits<GSSwitchEmits>()
   &.size-large {
     --width: 60px;
     --height: 30px;
-    --label-size: 20px;
+    --label-font-size: 20px;
   }
+}
 
+.gs-switch {
   width: var(--width);
   height: var(--height);
   border: var(--border-width) solid #EBE4D8;
@@ -74,7 +84,6 @@ defineEmits<GSSwitchEmits>()
   cursor: pointer;
   user-select: none;
   color: var(--icon-color);
-  filter: drop-shadow(0 0 2px #333);
 
   &::before {
     content: '';
@@ -88,18 +97,26 @@ defineEmits<GSSwitchEmits>()
     z-index: -1;
     border: calc(0.04 * var(--height)) solid rgb(100 100 100 / 0.2);
     border-radius: var(--height);
+    filter: drop-shadow(0 0 2px #666);
   }
 
   &.label::after {
     content: attr(data-label);
-    font-size: var(--label-size);
+    width: fit-content;
+    font-size: var(--label-font-size);
+    display: flex;
+    align-items: center;
     position: absolute;
     height: 100%;
     top: 0;
-    left: 100%;
-    padding: 0 0 0 8px;
     white-space: nowrap;
     color: var(--label-color);
+  }
+  &.label.label-position-right::after {
+    left: calc(100% + 0.5em);
+  }
+  &.label.label-position-left::after {
+    right: calc(100% + 0.5em);
   }
 
   &.actived {
@@ -109,7 +126,7 @@ defineEmits<GSSwitchEmits>()
     --bg: #D3BC8E;
     --bg-border-color: #BFA57A;
     --icon-color: #818D9D;
-    --label-color: #D3BC8E;
+    --label-color: var(--label-active-color);
   }
 }
 
