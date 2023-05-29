f44936526860c61f5d4e4535c35d063dc9869e94
夕云葛城 <kuroneko7520@outlook.com>
2023-05-29 16:02:42 +0800

    feat: 为分隔线添加颜色与分隔距离属性

--- src/components/GenshinUI/GSDivider.vue
+++ src/components/GenshinUI/GSDivider.vue
@@ -1,13 +1,32 @@
+<script lang="ts" setup>
+withDefaults(defineProps<{
+  height?: number
+  color?: string
+}>(), {
+  height: 48,
+  color: '#E2DACE',
+})
+</script>
+
 <template>
-  <div class="gs-divider" v-bind="$attrs" />
+  <div
+    v-bind="$attrs"
+    class="gs-divider"
+    :style="{
+      '--height': `${height}px`,
+      '--color': color,
+    }"
+  />
 </template>
 
 <style lang="scss" scoped>
 .gs-divider {
   --arrow-size: 8px;
+
   width: 100%;
-  height: 48px;
+  height: var(--height);
   position: relative;
+
   &::before {
     position: absolute;
     width: 100%;
@@ -17,8 +36,8 @@
       to bottom,
       transparent,
       transparent calc(50% - 1px),
-      #E2DACE calc(50% - 1px),
-      #E2DACE calc(50% + 1px),
+      var(--color) calc(50% - 1px),
+      var(--color) calc(50% + 1px),
       transparent calc(50% + 1px),
       transparent,
     );
@@ -37,10 +56,10 @@
     position: absolute;
     width: var(--arrow-size);
     height: var(--arrow-size);
-    border: 3px solid #E2DACE;
+    border: 3px solid var(--color);
     left: calc(50% - calc(var(--arrow-size) / 2));
     top: calc(50% - calc(var(--arrow-size) / 2));
-    outline: 2px solid #E2DACE;
+    outline: 2px solid var(--color);
     outline-offset: 2px;
     rotate: 135deg;
     clip-path: polygon(-100% -100%, 200% -100%, 200% 200%);
