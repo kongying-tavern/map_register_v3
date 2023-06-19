bf9144ef54eb35abd4699f9140f3c060abe91705
夕云葛城 <kuroneko7520@outlook.com>
2023-06-19 18:26:10 +0800

    fix: version 字段不再由前端修改，大幅简化逻辑

--- src/pages/pageMapV2/hooks/useMarkerCreate.ts
+++ src/pages/pageMapV2/hooks/useMarkerCreate.ts
@@ -1,22 +1,43 @@
 import { ElMessage } from 'element-plus'
 import type { Ref } from 'vue'
+import { pick } from 'lodash'
 import Api from '@/api/api'
 import { useFetchHook } from '@/hooks'
+import { useUserStore } from '@/stores'
 
 /** 新增点位，已自动处理 version 和 methodType 字段 */
-export const useMarkerCreate = (markerData: Ref<API.MarkerVo>) => {
-  const { refresh: createMarker, onSuccess, onError, ...rest } = useFetchHook({
-    onRequest: async () => {
-      const { version = 0 } = markerData.value
+export const useMarkerCreate = (markerData: Ref<API.MarkerVo | null>) => {
+  const userStore = useUserStore()
 
-      const form: API.MarkerVo = {
-        ...markerData.value,
-        version: version + 1,
-      }
+  const buildAdminMarkerForm = (marker: API.MarkerVo): API.MarkerVo => {
+    return {
+      ...pick(marker, 'itemList', 'position', 'content', 'picture', 'videoPath', 'refreshTime', 'hiddenFlag', 'markerTitle', 'extra'),
+      pictureCreatorId: marker.picture ? userStore.info.id : undefined,
+    }
+  }
 
+  const buildModifyMarkerForm = (marker: API.MarkerVo): API.MarkerPunctuateVo => {
+    return {
+      ...pick(marker, 'itemList', 'position', 'content', 'picture', 'videoPath', 'refreshTime', 'hiddenFlag', 'markerTitle', 'extra') as API.MarkerPunctuateVo,
+      pictureCreatorId: marker.picture ? userStore.info.id : undefined,
+      methodType: 1,
+    }
+  }
+
+  const request = async () => {
+    if (!markerData.value)
+      throw new Error('所需的点位数据为空')
+    if (userStore.isAdmin) {
+      const form = buildAdminMarkerForm(markerData.value)
       await Api.marker.createMarker(form)
-    },
-  })
+    }
+    else {
+      const form = buildModifyMarkerForm(markerData.value)
+      await Api.punctuate.addPunctuate({}, form)
+    }
+  }
+
+  const { refresh: createMarker, onSuccess, onError, ...rest } = useFetchHook({ onRequest: request })
 
   onSuccess(() => ElMessage.success('新增点位成功'))
   onError(err => ElMessage.error(err.message))
--- src/pages/pageMapV2/hooks/useMarkerEdit.ts
+++ src/pages/pageMapV2/hooks/useMarkerEdit.ts
@@ -1,19 +1,20 @@
 import type { Ref } from 'vue'
 import { ElMessage } from 'element-plus'
+import { pick } from 'lodash'
 import Api from '@/api/api'
 import { useFetchHook } from '@/hooks'
 import { useUserStore } from '@/stores'
 
-/** 编辑点位，已自动处理 version 和 methodType 字段 */
-export const useMarkerEdit = (markerData: Ref<API.MarkerVo>) => {
+/** 编辑点位，已自动处理 methodType 字段 */
+export const useMarkerEdit = (markerData: Ref<API.MarkerVo | null>) => {
   const userStore = useUserStore()
 
   /**
    * 基于 url 参数传递来判断 picture 是否已经更改
    * 此处对 picture 传递过来的 url 上的参数 timestamp、和 last-modified 进行判断和处理
    */
-  const checkPictureChange = () => {
-    const { picture = '' } = markerData.value
+  const checkPictureChange = (marker: API.MarkerVo) => {
+    const { picture = '' } = marker
     if (!picture)
       return false
 
@@ -27,66 +28,48 @@ export const useMarkerEdit = (markerData: Ref<API.MarkerVo>) => {
     if (isChanged) {
       !lastModified && params.set('last-modified', `${new Date().getTime()}`)
       params.delete('timestamp')
-      markerData.value.picture = `${pictureURL.origin}${pictureURL.pathname}?${params.toString()}`
+      marker.picture = `${pictureURL.origin}${pictureURL.pathname}?${params.toString()}`
     }
 
     return isChanged
   }
 
-  const buildAdminMarkerForm = (): API.MarkerVo => {
-    const marker = markerData.value
-    const isPictureChanged = checkPictureChange()
+  const buildAdminMarkerForm = (marker: API.MarkerVo): API.MarkerVo => {
+    const isPictureChanged = checkPictureChange(marker)
     return {
-      id: marker.id,
-      itemList: marker.itemList,
-      position: marker.position,
-      content: marker.content,
-      picture: marker.picture,
+      ...pick(marker, 'id', 'itemList', 'position', 'content', 'picture', 'videoPath', 'refreshTime', 'hiddenFlag', 'markerTitle', 'extra', 'version'),
       pictureCreatorId: isPictureChanged ? userStore.info.id : marker.pictureCreatorId,
-      videoPath: marker.videoPath,
-      refreshTime: marker.refreshTime,
-      hiddenFlag: marker.hiddenFlag,
-      markerTitle: marker.markerTitle,
-      extra: marker.extra,
-      version: marker.version ?? 0 + 1,
     }
   }
 
-  const buildModifyMarkerForm = (): API.MarkerPunctuateVo => {
-    const marker = markerData.value
-    const isPictureChanged = checkPictureChange()
+  const buildModifyMarkerForm = (marker: API.MarkerVo): API.MarkerPunctuateVo => {
+    const isPictureChanged = checkPictureChange(marker)
     return {
-      id: marker.id,
-      // itemList: marker.itemList,
-      position: marker.position,
-      content: marker.content,
-      picture: marker.picture,
+      ...pick(marker, 'id', 'itemList', 'position', 'content', 'picture', 'videoPath', 'refreshTime', 'hiddenFlag', 'markerTitle', 'extra', 'version') as API.MarkerPunctuateVo,
       pictureCreatorId: isPictureChanged ? userStore.info.id : marker.pictureCreatorId,
-      videoPath: marker.videoPath,
-      refreshTime: marker.refreshTime,
-      hiddenFlag: marker.hiddenFlag,
-      markerTitle: marker.markerTitle,
-      extra: marker.extra,
-      version: marker.version ?? 0 + 1,
       methodType: 2,
     }
   }
 
-  const { refresh: editMarker, onSuccess, onError, ...rest } = useFetchHook({
-    onRequest: async () => {
-      if (userStore.isAdmin) {
-        const form: API.MarkerVo = buildAdminMarkerForm()
-        await Api.marker.updateMarker(form)
-      }
-      else {
-        const form: API.MarkerPunctuateVo = buildModifyMarkerForm()
-        await Api.punctuate.addPunctuate({}, form)
-      }
-    },
-  })
+  /** 原始操作 */
+  const request = async () => {
+    if (!markerData.value)
+      throw new Error('所需的点位数据为空')
+    const shallowCopy = { ...markerData.value }
+    if (userStore.isAdmin) {
+      const form: API.MarkerVo = buildAdminMarkerForm(shallowCopy)
+      await Api.marker.updateMarker(form)
+    }
+    else {
+      const form: API.MarkerPunctuateVo = buildModifyMarkerForm(shallowCopy)
+      await Api.punctuate.addPunctuate({}, form)
+    }
+  }
+
+  const { refresh: editMarker, onSuccess, onError, ...rest } = useFetchHook({ onRequest: request })
 
   onSuccess(() => ElMessage.success('编辑点位成功'))
   onError(err => ElMessage.error(err.message))
 
-  return { editMarker, onSuccess, onError, ...rest }
+  return { editMarker, request, onSuccess, onError, ...rest }
 }
