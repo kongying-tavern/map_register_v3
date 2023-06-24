353ddd56b0203d2989402e28c643e6a8a12d3b90
夕云葛城 <kuroneko7520@outlook.com>
2023-06-25 02:17:02 +0800

    style: logo 交互样式优化

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-full grid place-items-center text-sm gap-1">
-    <div class="kongying-logo w-full h-full flex justify-center items-center gap-1 select-none hover:bg-slate-100 active:bg-slate-200 bg-opacity-70 transition-all cursor-pointer duration-100">
+    <div class="kongying-logo w-full h-full flex justify-center items-center gap-1 select-none bg-opacity-70 cursor-pointer">
       <img class="w-8 h-8 object-contain" src="/favicon.ico">
       <div class="h-8 text-xs genshin-text">
         空荧<br>酒馆后厨
@@ -12,5 +12,13 @@
 <style lang="scss" scoped>
 .kongying-logo {
   clip-path: inset(5px round 6px);
+  transition: all ease 300ms;
+
+  &:hover {
+    background-color: var(--el-color-primary-light-9);
+  }
+  &:active {
+    background-color: var(--el-color-primary-light-7);
+  }
 }
 </style>
