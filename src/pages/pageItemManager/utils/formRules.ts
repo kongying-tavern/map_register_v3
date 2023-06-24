abced0999da243b20e35fec628483c1640946ec2
夕云葛城 <kuroneko7520@outlook.com>
2023-06-24 18:34:06 +0800

    feat: 通用表单校验添加空白校验

--- src/pages/pageItemManager/utils/formRules.ts
+++ src/pages/pageItemManager/utils/formRules.ts
@@ -7,9 +7,15 @@ export type ItemFormRules<T> = {
   [P in keyof T]: Arrayable<FormItemRule> | undefined
 }
 
+export const requireCheck = (trigger: Trigger, name: string) => ({
+  required: true,
+  message: `${name}不能为空`,
+  trigger,
+})
+
 export const lengthCheck = (trigger: Trigger, name: string, max: number, min = 1): FormItemRule => ({
   required: true,
-  message: `${name}需要 ${min} - ${max} 个字符`,
+  message: `${name}需要 ${min} - ${max} 个非空白字符`,
   pattern: new RegExp(`\\S{${min},${max}}`),
   trigger,
 })
