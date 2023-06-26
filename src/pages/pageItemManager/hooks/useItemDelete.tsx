bb5ab504c75b55de68f93d1cba3f5a88c7f623db
夕云葛城 <kuroneko7520@outlook.com>
2023-06-26 17:05:20 +0800

    feat: 批量删除

--- src/pages/pageItemManager/hooks/useItemDelete.tsx
+++ src/pages/pageItemManager/hooks/useItemDelete.tsx
@@ -1,22 +1,26 @@
 import { ElMessageBox } from 'element-plus'
 import { useFetchHook } from '@/hooks'
+import { GSMessageService } from '@/components'
 import Api from '@/api/api'
 
-const sharedItemId = ref<number>()
+export interface ItemDeleteHookOptions {
+  isRoot?: boolean
+}
 
-export const useItemDelete = () => {
-  const { refresh: deleteItem, onFinish, ...rest } = useFetchHook({
-    onRequest: async () => {
-      if (sharedItemId.value === undefined)
-        throw new Error('所需的物品 id 为空')
-      await Api.item.deleteItem({
-        itemId: sharedItemId.value,
-      })
-    },
-  })
+const { refresh: deleteItem, onSuccess, onError, ...rest } = useFetchHook({
+  onRequest: async (itemIds?: number | number[]) => {
+    if (itemIds === undefined)
+      throw new Error('物品 id 为空')
+    if (typeof itemIds === 'number')
+      return await Api.item.deleteItem({ itemId: itemIds })
+    return await Promise.allSettled(itemIds.map(itemId => Api.item.deleteItem({ itemId })))
+  },
+})
+
+export const useItemDelete = (options: ItemDeleteHookOptions = {}) => {
+  const { isRoot } = options
 
   const handleDelete = async (item: API.ItemVo) => {
-    sharedItemId.value = item.id
     const isConfirm = await ElMessageBox
       .confirm(
         <span>删除物品<span style="color: #FB923C">{item.name} (id: {item.id})</span>，该操作不可逆，请确认？</span>,
@@ -25,12 +29,31 @@ export const useItemDelete = () => {
       .catch(() => false)
     if (!isConfirm)
       return
-    await deleteItem()
+    await deleteItem(item.id)
   }
 
-  onFinish(() => {
-    sharedItemId.value = undefined
-  })
+  const handleBatchDelete = async (items: API.ItemVo[]) => {
+    const isConfirm = await ElMessageBox
+      .confirm(
+        <span>删除所选的 <span style="color: #FB923C">{items.length}</span> 个物品，该操作不可逆，请确认？</span>,
+        '确认操作',
+      )
+      .catch(() => false)
+    if (!isConfirm)
+      return
+    await deleteItem(items.map(item => item.id as number))
+  }
 
-  return { handleDelete, ...rest }
+  if (isRoot) {
+    onSuccess(() => GSMessageService.info('删除成功，数据同步可能需要几分钟时间', {
+      type: 'success',
+      duration: 5000,
+    }))
+    onError(err => GSMessageService.info(`删除失败：${err.message}`, {
+      type: 'error',
+      duration: 5000,
+    }))
+  }
+
+  return { handleDelete, handleBatchDelete, onSuccess, onError, ...rest }
 }
