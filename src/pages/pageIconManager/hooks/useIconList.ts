22288304b29738f10bef753f7b8208016897beb6
mikezhuang <2541413819@qq.com>
2023-06-30 11:12:13 +0800

    fix: 图标管理优化

--- src/pages/pageIconManager/hooks/useIconList.ts
+++ src/pages/pageIconManager/hooks/useIconList.ts
@@ -3,38 +3,10 @@ import { useFetchHook } from '@/hooks'
 
 /** 图标信息相关业务逻辑封装 */
 export const useIconList = (params: ComputedRef<API.IconSearchVo>) => {
-  const userNameCache = ref<Map<number, string>>(new Map())
-
-  Api.sysUserController.getUserList({
-    current: 0,
-    size: 1000,
-    sort: [
-      'create_time-',
-    ],
-  }).then((res) => {
-    if (!res.data)
-      return
-    const userList = res.data.record || []
-    userList.forEach((item) => {
-      if (item.id === undefined || item.nickname === undefined)
-        return
-      userNameCache.value.set(item.id, item.nickname)
-    })
-  })
-
   const { refresh, onSuccess, onError } = useFetchHook({
     onRequest: async () => {
       const { data: { record = [], total = 0 } = {} } = await Api.icon.listIcon(params.value)
 
-      // 增加更新者名称
-      type IconWithName = API.IconVo & { updaterName?: string }
-      record.forEach((item: IconWithName) => {
-        item.updaterId = item.updaterId || 1
-        if (userNameCache.value.has(item.updaterId))
-          item.updaterName = userNameCache.value.get(item.updaterId)
-        else
-          item.updaterName = '未知'
-      })
       return { record, total }
     },
   })
