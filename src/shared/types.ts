e8d77c3dd2d42db3abda3515b60ed3739796f2e2
夕云葛城 <kuroneko7520@outlook.com>
2023-07-09 15:06:15 +0800

    feat: 添加 Uploader 实例类型

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { ElForm } from 'element-plus'
+import type { ElForm, ElUpload } from 'element-plus'
 
 export type AnyObject = Record<string, any>
 export type AnyArray = any[]
 export type AnyFunction<T = any> = (...args: AnyArray) => T
 export type ElFormType = InstanceType<typeof ElForm>
+export type ElUploadType = InstanceType<typeof ElUpload>
