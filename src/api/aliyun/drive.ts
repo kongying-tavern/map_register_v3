de00359de9d0039c442c5490708cf44b6698945f
夕云葛城 <kuroneko7520@outlook.com>
2023-07-17 16:43:32 +0800

    fix: 为图床请求添加错误拦截

--- src/api/aliyun/drive.ts
+++ src/api/aliyun/drive.ts
@@ -4,7 +4,7 @@ import type { AxiosRequestConfig } from 'axios'
 
 /** 云盘 - 登录 */
 export async function token(options: AxiosRequestConfig = {}) {
-  return request<API.RDriveToken>(`${import.meta.env.VITE_ALIYUN_BASE}/api/auth/login`, {
+  const res = await request<API.RDriveToken>(`${import.meta.env.VITE_ALIYUN_BASE}/api/auth/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -15,7 +15,10 @@ export async function token(options: AxiosRequestConfig = {}) {
       password: import.meta.env.VITE_IMG_SERVER_PASSWORD,
     },
     ...options,
-  })
+  }) ?? {}
+  if (res.code !== 200)
+    throw new Error(res.message ?? '请求失败')
+  return res
 }
 
 /** 云盘 - 上传图片到指定目录 */
@@ -26,7 +29,7 @@ export async function upload(
   const { file, path, password = '', authorization } = body
   const formData = new FormData()
   formData.append('file', file)
-  return request<API.RBase>(`${import.meta.env.VITE_ALIYUN_BASE}/api/fs/form`, {
+  const res = await request<API.RBase>(`${import.meta.env.VITE_ALIYUN_BASE}/api/fs/form`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'multipart/form-data',
@@ -36,5 +39,8 @@ export async function upload(
     },
     data: formData,
     ...options,
-  })
+  }) ?? {}
+  if (res.code !== 200)
+    throw new Error(res.message ?? '请求失败')
+  return res
 }
