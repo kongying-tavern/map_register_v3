fd1acda3487faf0da9a848caf1c3ed16d2bbce74
Alex Fang <deeno@qq.com>
2023-07-22 20:15:12 +0800

    feat: 修改1.6海岛字段插件

--- src/components/plugins/1_6_island/edit.vue
+++ src/components/plugins/1_6_island/edit.vue
@@ -1,10 +1,44 @@
+<script setup>
+import _ from "lodash";
+import { ref, computed, onMounted, nextTick } from "vue";
+import {
+  markerExtraEntryGetter,
+  markerExtraEntrySetter,
+} from "src/components/extra-data";
+
+const extraData = ref([]);
+
+const extraValue = computed(() => {
+  if (!_.isArray(extraData.value) || extraData.value.length <= 0) {
+    return null;
+  }
+
+  return extraData.value || [];
+});
+
+const islandOptions = [
+  { label: "第一阶段", value: "1" },
+  { label: "第二阶段", value: "2" },
+];
+
+const extraDataUpdate = () => {
+  markerExtraEntrySetter("1_6_island", extraValue.value);
+};
+
+onMounted(() => {
+  nextTick(() => {
+    extraData.value = markerExtraEntryGetter("1_6_island") || [];
+  });
+});
+</script>
+
 <template>
   <q-item>
     <q-item-section side top> 阶段限定 </q-item-section>
     <q-item-section>
       <q-select
-        v-model="island_data"
-        :options="island_options"
+        v-model="extraData"
+        :options="islandOptions"
         emit-value
         map-options
         label="选择阶段"
@@ -13,58 +47,14 @@
         multiple
         use-chips
         size="sm"
-        @update:model-value="island_update"
+        @update:model-value="extraDataUpdate"
       />
     </q-item-section>
   </q-item>
 </template>
 
 <script>
-import _ from "lodash";
-import funcExtraData from "../../extra-data";
-
 export default {
-  name: "Plugin16IslandEdit",
-  setup() {
-    return {
-      ...funcExtraData,
-    };
-  },
-  mounted() {
-    this.$nextTick().then(() => {
-      this.island_data = this.island_data_init;
-    });
-  },
-  data() {
-    return {
-      island_data: [],
-      island_options: [
-        { label: "第一阶段", value: "1" },
-        { label: "第二阶段", value: "2" },
-      ],
-    };
-  },
-  computed: {
-    island_data_init() {
-      const island_data = _.get(this.layer_extra_data, "1_6_island", []) || [];
-      return island_data;
-    },
-    island_data_parsed() {
-      if (_.isNil(this.island_data)) {
-        return null;
-      }
-
-      if (_.isArray(this.island_data) && _.isEmpty(this.island_data)) {
-        return null;
-      }
-
-      return this.island_data;
-    },
-  },
-  methods: {
-    island_update() {
-      this.put_marker_extra_data_entry("1_6_island", this.island_data_parsed);
-    },
-  },
+  name: "ExtraEdit16Island",
 };
 </script>
--- src/components/plugins/extra-adapter.vue
+++ src/components/plugins/extra-adapter.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { computed } from "vue";
-// Import pl16Island from "./1_6_island/edit.vue";
+import pl16Island from "./1_6_island/edit.vue";
 import pl28Island from "./2_8_island/edit.vue";
 import plUnderground from "./underground/edit.vue";
 import { get_map_plugin_config } from "src/api/map";
@@ -31,8 +31,8 @@ const plugin_options = computed(() => {
 <template>
   <template v-for="plugin_tag in plugin_tags" :key="plugin_tag">
     <!-- 海岛 -->
-    <!-- <pl16Island v-if="plugin_tag === '1_6_island'" /> -->
-    <pl28Island v-if="plugin_tag === '2_8_island'" />
+    <pl16Island v-if="plugin_tag === '1_6_island'" />
+    <pl28Island v-else-if="plugin_tag === '2_8_island'" />
 
     <!-- 地下层级 -->
     <plUnderground
