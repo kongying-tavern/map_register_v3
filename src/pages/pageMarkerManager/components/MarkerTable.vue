c7b23c358f09ce5b941efe6c81d13899e2cad222
夕云葛城 <kuroneko7520@outlook.com>
2023-07-24 23:45:22 +0800

    feat: 抽离刷新时间 formatter

--- src/pages/pageMarkerManager/components/MarkerTable.vue
+++ src/pages/pageMarkerManager/components/MarkerTable.vue
@@ -2,6 +2,7 @@
 import { QuestionFilled, VideoCamera } from '@element-plus/icons-vue'
 import { BilibiliVideoPlayer } from '.'
 import { useGlobalDialog } from '@/hooks'
+import { refreshTimeFormatter } from '@/utils'
 
 defineProps<{
   loading: boolean
@@ -34,20 +35,6 @@ const positionFormatter = (row: API.MarkerVo) => {
   const numX = Math.floor(parseFloat(x))
   const numY = Math.floor(parseFloat(y))
   return `(${numX}, ${numY})`
-}
-
-const refreshTimeFormatter = (row: API.MarkerVo) => {
-  const { refreshTime: time } = row
-  if (!time || time < 0)
-    return '不刷新'
-  const labels: string[] = []
-  const days = Math.floor(time / 86_400_000)
-  days > 0 && labels.push(`${days} 天`)
-  const hours = Math.floor(time % 86_400_000 / 3_600_000)
-  hours > 0 && labels.push(`${hours} 小时`)
-  const mins = Math.floor(time % 3600 / 60_000)
-  mins > 0 && labels.push(`${mins} 分钟`)
-  return labels.join(' ')
 }
 
 const { DialogService } = useGlobalDialog()
--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,3 +1,23 @@
 export const urlFormatter = (_: unknown, __: unknown, url = '') => decodeURIComponent(url)
 
 export const timeFormatter = (_: unknown, __: unknown, time = '') => new Date(time).toLocaleString()
+
+export const refreshTimeFormatter = (_: unknown, __: unknown, time = 0) => {
+  if (!time)
+    return '不刷新'
+  if (time < 0) {
+    return ({
+      [-1]: '次日4点',
+      [-2]: '手动刷新',
+      [-3]: '次日0点',
+    } as Record<number, string>)[time]
+  }
+  const labels: string[] = []
+  const days = Math.floor(time / 86_400_000)
+  days > 0 && labels.push(`${days} 天`)
+  const hours = Math.floor(time % 86_400_000 / 3_600_000)
+  hours > 0 && labels.push(`${hours} 小时`)
+  const mins = Math.floor(time % 3600 / 60_000)
+  mins > 0 && labels.push(`${mins} 分钟`)
+  return labels.join(' ')
+}
