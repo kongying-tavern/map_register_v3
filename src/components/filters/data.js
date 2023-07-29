ae9f87dbb5f9c477bc20ebd34330381cf2063352
Alex Fang <deeno@qq.com>
2023-07-29 11:20:05 +0800

    新增：添加可见范围过滤条件

--- src/components/filters/data.js
+++ src/components/filters/data.js
@@ -6,6 +6,7 @@ import JSONPack from "jsonpack";
 import { create_notify } from "src/api/common";
 import { selectorArea, selectorCollapse } from "src/components/selector-data";
 import { map_plugin_config } from "src/api/config";
+import { is_neigui } from "src/service/user_info";
 
 export const filterCardVisible = ref(false);
 
@@ -571,9 +572,75 @@ export const filterTypes = [
       return switchVal === videoPathExists;
     },
   },
+  {
+    id: 9,
+    name: "visibility",
+    icon: "mdi-eye",
+    title: "可见范围",
+    label: "可见范围为",
+    model: "select",
+    modelOpts: {
+      multiple: true,
+      useChips: true,
+      optionValue: "value",
+      optionsFunc: computed(() => {
+        const options = [
+          { label: "显示", value: 0 },
+          { label: "隐藏", value: 1 },
+        ];
+        if (is_neigui()) {
+          options.push({ label: "内鬼", value: 2 });
+        }
+
+        return options;
+      }),
+    },
+    modelVals: {
+      value: [],
+    },
+
+    modelSemantic(values = {}, options = {}, oppositeValue = false) {
+      const selectedValue = values.value || [];
+      if (!_.isArray(selectedValue) || selectedValue.length <= 0) {
+        return "";
+      }
+
+      const optionsFull = options.optionsFunc || [];
+      const optionsMap = _.chain(optionsFull)
+        .keyBy("value")
+        .mapValues((v) => v.label)
+        .value();
+      const selectedNames = _.chain(selectedValue)
+        .map((v) => optionsMap[v])
+        .filter((v) => v)
+        .uniq()
+        .value();
+      const selectedNameStr = selectedNames.join(",");
+
+      if (!selectedNameStr) {
+        return "";
+      }
+
+      return selectedNames.length > 1
+        ? `${oppositeValue ? "不" : ""}属于${selectedNameStr}点位`
+        : `${oppositeValue ? "不" : ""}为${selectedNameStr}点位`;
+    },
+    // eslint-disable-next-line no-unused-vars
+    filterAction(item = {}, values = {}, options = {}) {
+      const allowedValues = values.value || [];
+      if (!_.isArray(allowedValues) || allowedValues.length <= 0) {
+        return true;
+      }
+
+      const hiddenFlag = item.hiddenFlag ?? -1;
+      const allowed = allowedValues.indexOf(hiddenFlag) !== -1;
+      return allowed;
+    },
+  },
   /**
    * Schema Example:
    * {
+   *   id: 0,
    *   name: "",
    *   icon: "",
    *   title: "",
@@ -581,10 +648,10 @@ export const filterTypes = [
    *   model: "",
    *   modelOpts: {},
    *   modelVals: {},
-   * // eslint-disable-next-line no-unused-vars
-   *   modelSemantic: (values = {}, options = {}, oppositeValue = false) => {}, // eslint-disable-line no-unused-vars
-   * // eslint-disable-next-line no-unused-vars
-   *   filterAction: (item = {}, values = {}, options = {}) => {}, // eslint-disable-line no-unused-vars
+   *   // eslint-disable-next-line no-unused-vars
+   *   modelSemantic: (values = {}, options = {}, oppositeValue = false) => {},
+   *   // eslint-disable-next-line no-unused-vars
+   *   filterAction: (item = {}, values = {}, options = {}) => {},
    * }
    */
 ];
