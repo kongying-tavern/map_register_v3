e778b3649395235f29f1d63299c76ef753c50c15
夕云葛城 <kuroneko7520@outlook.com>
2023-08-07 19:10:57 +0800

    fix: 删除冗余代码，减少 useMap 被重复引用的函数定义开销

--- src/pages/pageMapV2/hooks/useMap.ts
+++ src/pages/pageMapV2/hooks/useMap.ts
@@ -13,41 +13,37 @@ const map = shallowRef<GenshinMap>()
 /** 在地图初始化前临时存储回调函数 */
 const tempCallbackSet = shallowRef(new Set<MapReadyCallbackFunction>())
 
+const onMapReady = (cb: MapReadyCallbackFunction) => {
+  if (!map.value) {
+    tempCallbackSet.value.add(cb)
+    return
+  }
+  cb(map.value)
+}
+
+const initMap = async (canvas: HTMLCanvasElement) => {
+  const newMap = await GenshinMap.create({ canvas })
+
+  // 设置底图
+  await newMap.ready
+  newMap.setBaseLayer(LAYER_CONFIGS[0].code)
+  map.value = newMap
+  logger.info('map is ready', newMap)
+
+  // 注册在地图创建以前生成的事件监听器
+  tempCallbackSet.value.forEach(cb => cb(newMap))
+  tempCallbackSet.value.clear()
+
+  // 加载存档
+  const archiveStore = useArchiveStore()
+  const hasArchives = Object.values(archiveStore.archiveSlots).filter(Boolean).length > 0
+  if (!hasArchives && archiveStore.currentArchive.slotIndex === undefined) {
+    await archiveStore.fetchArchive()
+    archiveStore.loadLatestArchive()
+  }
+}
+
 export const useMap = (canvasRef?: Ref<HTMLCanvasElement | null>) => {
-  const onMapReady = (cb: MapReadyCallbackFunction) => {
-    if (!map.value) {
-      tempCallbackSet.value.add(cb)
-      return
-    }
-    cb(map.value)
-  }
-
-  const initMap = async () => {
-    if (!canvasRef?.value || map.value)
-      return
-
-    const newMap = await GenshinMap.create({ canvas: canvasRef.value })
-
-    // 设置底图
-    await newMap.ready
-    newMap.setBaseLayer(LAYER_CONFIGS[0].code)
-    map.value = newMap
-    logger.info('map is ready', newMap)
-
-    // 注册在地图创建以前生成的事件监听器
-    tempCallbackSet.value.forEach(cb => cb(newMap))
-    tempCallbackSet.value.clear()
-
-    // 加载存档
-    const archiveStore = useArchiveStore()
-    const hasArchives = Object.values(archiveStore.archiveSlots).filter(Boolean).length > 0
-    if (!hasArchives && archiveStore.currentArchive.slotIndex === undefined) {
-      await archiveStore.fetchArchive()
-      archiveStore.loadLatestArchive()
-    }
-  }
-
-  canvasRef && onMounted(initMap)
-
+  canvasRef && onMounted(() => canvasRef.value && initMap(canvasRef.value))
   return { map, onMapReady }
 }
