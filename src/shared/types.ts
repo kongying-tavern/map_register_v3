5e7b2233c25ca28cc36cccde7d8ab6308331f88f
夕云葛城 <kuroneko7520@outlook.com>
2023-08-22 00:58:30 +0800

    feat: 抽离公共类型与方法

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -6,3 +6,45 @@ export type AnyArray = any[]
 export type AnyFunction<T = any> = (...args: AnyArray) => T
 export type ElFormType = InstanceType<typeof ElForm>
 export type ElUploadType = InstanceType<typeof ElUpload>
+
+/**
+ * 光标类型
+ * @link https://developer.mozilla.org/zh-CN/docs/Web/CSS/cursor
+ */
+export type CursorType =
+ | 'auto'
+ | 'default'
+ | 'none'
+ | 'context-menu'
+ | 'help'
+ | 'pointer'
+ | 'progress'
+ | 'wait'
+ | 'cell'
+ | 'crosshair'
+ | 'text'
+ | 'vertical-text'
+ | 'alias'
+ | 'copy'
+ | 'move'
+ | 'no-drop'
+ | 'not-allowed'
+ | 'grab'
+ | 'grabbing'
+ | 'all-scroll'
+ | 'col-resize'
+ | 'row-resize'
+ | 'n-resize'
+ | 'e-resize'
+ | 's-resize'
+ | 'w-resize'
+ | 'ne-resize'
+ | 'nw-resize'
+ | 'se-resize'
+ | 'sw-resize'
+ | 'ew-resize'
+ | 'ns-resize'
+ | 'nesw-resize'
+ | 'nwse-resize'
+ | 'zoom-in'
+ | 'zoom-out'
--- src/utils/typeUtils.ts
+++ src/utils/typeUtils.ts
@@ -1,3 +1,5 @@
+import type { Coordinate2D } from '@/pages/pageMapV2/core'
+
 export const isMarkerVo = (v: unknown): v is API.MarkerVo => {
   if (typeof v !== 'object' || v === null)
     return false
@@ -5,5 +7,15 @@ export const isMarkerVo = (v: unknown): v is API.MarkerVo => {
     if (v[key as keyof typeof v] === undefined)
       return false
   }
+  return true
+}
+
+export const isMovingMarker = (v: unknown): v is { origin: API.MarkerVo; offset: Coordinate2D } => {
+  if (typeof v !== 'object' || v === null)
+    return false
+  if (!isMarkerVo((v as Record<string, unknown>).origin))
+    return false
+  if (!Array.isArray((v as Record<string, unknown>).offset))
+    return false
   return true
 }
