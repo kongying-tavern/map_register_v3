213aad42fb5a9a9a1a438c1c1928bb82911682ed
夕云葛城 <kuroneko7520@outlook.com>
2023-08-22 01:00:31 +0800

    fix: 修复了类型提示没有生效的问题

--- src/pages/pageMapV2/core/EventBus.ts
+++ src/pages/pageMapV2/core/EventBus.ts
@@ -1,30 +1,30 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export type EventHandler<P extends any[] = any[]> = (...payloads: P) => void
+export type EventHandler<P> = (...payloads: P extends unknown[] ? P : []) => void
 
-export class EventBus<EventMap extends Record<string, any[]>> {
+export class EventBus<EventMap> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   #handlers: Record<any, any[]> = {}
 
-  on = <T extends keyof EventMap, P extends EventMap[T]>(event: T, handler: EventHandler<P>): void => {
+  on = <T extends keyof EventMap>(event: T, handler: EventHandler<EventMap[T]>): void => {
     if (!this.#handlers[event])
       this.#handlers[event] = []
     this.#handlers[event].push(handler)
   }
 
-  once = <T extends keyof EventMap, P extends EventMap[T]>(event: T, handler: EventHandler<P>): void => {
-    const onceHandler: EventHandler<P> = (...payloads: P) => {
+  once = <T extends keyof EventMap>(event: T, handler: EventHandler<EventMap[T]>): void => {
+    const onceHandler: EventHandler<EventMap[T]> = (...payloads) => {
       handler(...payloads)
       this.off(event, onceHandler)
     }
     this.on(event, onceHandler)
   }
 
-  emit = <T extends keyof EventMap, P extends EventMap[T]>(event: T, ...payloads: P): void => {
+  emit = <T extends keyof EventMap>(event: T, ...payloads: EventMap[T] extends unknown[] ? EventMap[T] : []): void => {
     const eventHandlers = this.#handlers[event]
     if (eventHandlers)
       eventHandlers.forEach(handler => handler(...payloads))
   }
 
-  off = <T extends keyof EventMap, P extends EventMap[T]>(event: T, handler: EventHandler<P>): void => {
+  off = <T extends keyof EventMap>(event: T, handler: EventHandler<EventMap[T]>): void => {
     const eventHandlers = this.#handlers[event]
     if (eventHandlers) {
       this.#handlers[event] = eventHandlers.filter(
