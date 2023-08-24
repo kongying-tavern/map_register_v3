6ad1c478d0ca42de873454ff10ff3184b91a1f55
夕云葛城 <kuroneko7520@outlook.com>
2023-08-24 15:46:21 +0800

    docs: 添加注释

--- src/stores/mapSetting.ts
+++ src/stores/mapSetting.ts
@@ -2,7 +2,11 @@ import { defineStore } from 'pinia'
 
 export const useMapSettingStore = defineStore('map-setting', {
   state: () => ({
+    /** 显示调试信息 */
     showTooltip: false,
+    /** 显示附加图层 */
+    showOverlay: false,
+    /** 隐藏标记点位 */
     hideMarkedMarker: true,
   }),
 
