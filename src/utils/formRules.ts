39e6a86ce51ad0a01f8bce7540458f6333267a6e
夕云葛城 <kuroneko7520@outlook.com>
2023-08-29 23:44:28 +0800

    feat: 增加物品校验逻辑

--- src/utils/formRules.ts
+++ src/utils/formRules.ts
@@ -1,5 +1,6 @@
 import type { Arrayable } from '@vueuse/core'
 import type { FormItemRule } from 'element-plus'
+import db from '@/database'
 
 export type Trigger = 'blur' | 'change' | 'active'
 
@@ -39,4 +40,29 @@ export const passwordCheck = (): FormItemRule => ({
   required: true,
   message: '密码至少需要6个字符',
   validator: (_, v = '') => v.length >= 6,
+})
+
+/** 检查在选取了宝箱品质的情况下，是否有至少一个获取方式 */
+export const isTreasureChestMatched = (): FormItemRule => ({
+  // TODO 暂时使用名称匹配
+  asyncValidator: async (_, linkItems: API.MarkerItemLinkVo[] = []) => {
+    const treasureChestId = (await db.itemType.where('name').equals('宝箱品质').first())?.id
+    const wayToGetId = (await db.itemType.where('name').equals('获取方式').first())?.id
+    if (treasureChestId === undefined || wayToGetId === undefined)
+      return
+
+    const itemsIds = (linkItems ?? []).map(linkItem => linkItem.itemId!)
+    const items = (await db.item.bulkGet(itemsIds)).filter(Boolean) as API.ItemVo[]
+
+    const hasTreasureChest = Boolean(items.find(item => Boolean(item.typeIdList?.find(itemTypeId => itemTypeId === treasureChestId))))
+    if (!hasTreasureChest)
+      return
+
+    const hasWayToGet = Boolean(items.find(item => Boolean(item.typeIdList?.find(itemTypeId => itemTypeId === wayToGetId))))
+    if (hasWayToGet)
+      return
+
+    throw new Error('必须为宝箱选择一个获取方式')
+  },
+  trigger: 'change',
 })
