e1a3b1b3f95f11e5f0def6cbdb264b3e64791abf
夕云葛城 <kuroneko7520@outlook.com>
2023-09-07 18:00:20 +0800

    fix: 使用 bz2 格式的订阅插件配置

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,7 +1,7 @@
 /** 从订阅源获取打点的相关依赖配置 */
 const getDadianConfig = async () => {
-  const res = await fetch('https://assets.yuanshen.site/dadian.json')
-  return await res.json() as API.DadianJSON
+  const res = await fetch(`${import.meta.env.VITE_ASSETS_BASE}/web-map.json.bz2`)
+  return await res.arrayBuffer()
 }
 
 export default {
