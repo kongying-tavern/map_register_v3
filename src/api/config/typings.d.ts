4e765f03ee254a61705b4476039675c854e51c39
夕云葛城 <kuroneko7520@outlook.com>
2023-09-07 17:56:09 +0800

    feat: 完善订阅插件配置类型

--- src/api/config/typings.d.ts
+++ src/api/config/typings.d.ts
@@ -1,65 +1,160 @@
 declare namespace API {
-  type ExtraKeys = '1_6_island' | '2_8_island' | 'underground'
-
-  type Coordinate2D = [number, number]
-
-  type Coordinate3D = [number, number, number]
-
-  interface RecursiveOption {
-    label: string
-    value: string
-    children?: RecursiveOption[]
+  /** 总配置项 */
+  interface DadianJSON {
+    editor?: API.EditorConfig
+    plugins?: Record<string, API.PluginConfig>
+    pluginsNeigui?: Record<string, API.PluginConfig>
+    tiles?: Record<string, API.TileConfig>
+    tilesNeigui?: Record<string, API.TileConfig>
   }
 
+  type Coordinate2D = [number, number]
+  type Coordinate3D = [number, number, number]
+  type OverlayBounds = [[xmin: number, ymin: number], [xmax: number, ymax: number]]
+
   interface EditorConfig {
+    /** 用于控制显示的通告内容 */
     bannerText?: string
+    /** 用于控制快捷输入中可选的字符 */
     quickInputSnippets?: string[]
+    /** 用于提供特殊刷新时间的数据对应 */
     refreshTimeSpecial?: { label: string; value: number }[]
   }
 
   interface ExtraConfig {
     '1_6_island'?: {
-      stages: API.RecursiveOption[]
+      stages: {
+        /** 阶段选项的标签 */
+        label: string
+        /** 阶段选项的值 */
+        value: string
+      }[]
     }
     '2_8_island'?: {
-      stages: API.RecursiveOption[]
+      stages: {
+        /** 海岛的名称 */
+        label: string
+        /** 海岛名称标记值 */
+        value: string
+        /** 海岛形态选项 */
+        children: {
+          /** 海岛形态名 */
+          label: string
+          /** 海岛形态标记值 */
+          value: string
+        }[]
+      }[]
     }
     underground?: {
+      /** 开启地下选项时的提示文本 */
+      textActive?: string
+      /** 关闭地下选项时的提示文本 */
+      textInactive?: string
+      /** 是否使用详细的层级信息，如果为 false 则不显示层级选项，且默认设置层级为空 */
       useDetail?: boolean
-      levels?: API.RecursiveOption[]
+      /** 层级选项 */
+      levels?: {
+        /** 层级分组 */
+        label: string
+        /** 层级分组标记 */
+        value: string
+        children: {
+          /** 层级名称 */
+          label: string
+          /** 层级标记，为实际绑定到层级上的值 */
+          value: string
+        }[]
+      }[]
     }
+  }
+
+  interface OverlayChunkOption {
+    label?: string
+    value?: string
+    url?: string
+    bounds?: OverlayBounds
+  }
+
+  interface OverlayOption extends API.OverlayChunkOption {
+    /** 图片地址生成模板 */
+    urlTemplate?: string
+    /** 叠层项分片，通常用于一个叠层图由多张图片构成的情况，可选 */
+    chunks?: OverlayChunkOption[]
+  }
+
+  interface OverlayGroupOption extends Omit<API.OverlayOption, 'chunks'> {
+    /** 该分组内叠层是否可以同时开启 */
+    multiple?: boolean
+    /** 叠层项 */
+    children?: API.OverlayOption[]
   }
 
   interface OverlayConfig {
-    overlays?: []
+    /** 面板折叠后小图标的图标类（仅 Quasar 图标库，暂不通用） */
     panelIcon?: string
-    /** @template `{{key}}` */
+    /** 面板折叠后小图标的颜色 */
+    panelIconColor?: string
+    /**
+     * 是否启用暗色遮盖
+     * @default false
+     */
+    overlayMask?: boolean
+    /**
+     * 是否在有地下层级选中时才进行暗色覆盖
+     * @default true
+     */
+    overlayMaskOnSelected?: boolean
+    /**
+     * 在暗色遮盖时遮盖部分的透明度，默认值由前端确定
+     * @range `0 ~ 1`
+     */
+    overlayMaskOpacity?: number
+    /** 图片地址生成模板 */
     urlTemplate?: string
+    /** 生成元素ID的模板 */
+    idTemplate?: string
+    /** 同一组内叠层是否可以同时开启 */
+    multiple?: boolean
+    /** 叠层分组 */
+    overlays?: API.OverlayGroupOption[]
   }
 
   interface PluginConfig {
-    extra?: string[]
+    /** 开启的 extra 插件 */
+    extra?: keyof API.ExtraConfig
+    /** extra 插件具体配置 */
     extraConfig?: API.ExtraConfig
+    /** 是否启用叠层 */
     overlay?: boolean
     overlayConfig?: API.OverlayConfig
   }
 
   interface TileConfig {
+    /** 区域代码，用以拼接图片 URL 用，必填 */
     code?: string
+    /**
+     * 继承其他配置，将当前配置与此值表示的配置项合并后的配置作为此项的配置项
+     * @note 仅支持一层继承
+     **/
     extend?: string
+    /**
+     * 将地理坐标投影为 CRS 的纬度和经度偏移
+     * @ntoe 新后台使用像素坐标系统，不需要此项
+     */
     center?: API.Coordinate2D
+    /** 文件扩展名 */
     extension?: string
+    /** 地图图片的总宽高（像素） */
     size?: API.Coordinate2D
+    /**
+     * Leaflet.js <Map options> 的配置
+     * @note 在新后台中用作初始化视口配置
+     */
     settings?: {
       center?: API.Coordinate2D
       zoom?: number
     }
-  }
-
-  type DadianJSON = {
-    editor?: API.EditorConfig
-    plugins?: Record<string, API.PluginConfig>
-    tiles?: Record<string, API.TileConfig>
-    tilesNeigui?: Record<string, API.TileConfig>
+    /** 瓦片偏移量（像素） */
+    tilesOffset?: API.Coordinate2D
   }
 }
