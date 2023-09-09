5318d091bbd24f7485dca5f0f547a8b3dc0803a3
Alex Fang <deeno@qq.com>
2023-09-09 11:46:34 +0800

    feat: 添加物品计数过滤器

--- src/components/filters/data.js
+++ src/components/filters/data.js
@@ -10,6 +10,7 @@ import {
   selectorTypeId,
   selectorItemMap,
   selectorCollapse,
+  selectorItemId,
 } from "src/components/selector-data";
 import { map_plugin_config } from "src/api/config";
 import { is_neigui } from "src/service/user_info";
@@ -742,23 +743,81 @@ export const filterTypes = [
       return minMatch && maxMatch;
     },
   },
-  /**
-   * Schema Example:
-   * {
-   *   id: 0,
-   *   name: "",
-   *   icon: "",
-   *   title: "",
-   *   label: "",
-   *   model: "",
-   *   modelOpts: {},
-   *   modelVals: {},
-   *   // eslint-disable-next-line no-unused-vars
-   *   modelSemantic: (values = {}, options = {}, oppositeValue = false) => {},
-   *   // eslint-disable-next-line no-unused-vars
-   *   filterAction: (item = {}, values = {}, options = {}) => {},
-   * }
-   */
+  {
+    id: 12,
+    name: "item-current-count",
+    icon: "mdi-counter",
+    title: "当前物品计数",
+    label: "当前物品计数为",
+    model: "number-range",
+    modelOpts: {},
+    modelVals: {
+      minValue: null,
+      maxValue: null,
+    },
+    // eslint-disable-next-line no-unused-vars
+    modelSemantic(values = {}, options = {}, oppositeValue = false) {
+      const { minValue } = values;
+      const minVal = _.isNil(minValue) || minValue === "" ? null : minValue;
+      const { maxValue } = values;
+      const maxVal = _.isNil(maxValue) || maxValue === "" ? null : maxValue;
+
+      if (minVal === null && maxVal === null) {
+        return "";
+      }
+
+      if (minVal === null && maxVal !== null) {
+        return `当前物品计数小于等于${maxVal}`;
+      }
+
+      if (minVal !== null && maxVal === null) {
+        return `当前物品计数大于等于${minVal}`;
+      }
+
+      return `当前物品计数为${minVal}~${maxVal}`;
+    },
+    // eslint-disable-next-line no-unused-vars
+    filterAction(item = {}, values = {}, options = {}) {
+      const { minValue } = values;
+      const minVal = _.isNil(minValue) || minValue === "" ? null : minValue;
+      const { maxValue } = values;
+      const maxVal = _.isNil(maxValue) || maxValue === "" ? null : maxValue;
+
+      const itemList = item.itemList || [];
+      const itemCurrent = _.find(
+        itemList,
+        (v) => v.itemId === selectorItemId.value
+      );
+      const itemCurrentCount = itemCurrent?.count || 0;
+      let minMatch = true;
+      let maxMatch = true;
+
+      if (minVal !== null) {
+        minMatch = itemCurrentCount >= minVal;
+      }
+
+      if (maxVal !== null) {
+        maxMatch = itemCurrentCount <= maxVal;
+      }
+
+      return minMatch && maxMatch;
+    },
+  },
+  // Schema Example:
+  // {
+  //   id: 0,
+  //   name: "",
+  //   icon: "",
+  //   title: "",
+  //   label: "",
+  //   model: "",
+  //   modelOpts: {},
+  //   modelVals: {},
+  //   // eslint-disable-next-line no-unused-vars
+  //   modelSemantic: (values = {}, options = {}, oppositeValue = false) => {},
+  //   // eslint-disable-next-line no-unused-vars
+  //   filterAction: (item = {}, values = {}, options = {}) => {},
+  // }
 ];
 
 export const filterTypesNameMap = _.keyBy(filterTypes, "name");
