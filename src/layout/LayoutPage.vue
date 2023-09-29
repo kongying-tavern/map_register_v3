56d694477d1805c4ca9a5d871837d0aa0a00e7ea
夕云葛城 <kuroneko7520@outlook.com>
2023-09-30 00:20:46 +0800

    fix: 删除多余代码

--- src/layout/LayoutPage.vue
+++ src/layout/LayoutPage.vue
@@ -1,13 +1,6 @@
-<script lang="ts" setup>
-const props = defineProps<{
-  keepAlive?: boolean
-}>()
-const ctx = getCurrentInstance()
-</script>
-
 <template>
   <div class="flex flex-col p-4 overflow-hidden transition-all duration-200" v-bind="$attrs">
-    <div v-if="ctx?.slots.header" class="layout-page__header">
+    <div v-if="$slots.header" class="layout-page__header">
       <slot name="header" />
     </div>
 
@@ -15,10 +8,9 @@ const ctx = getCurrentInstance()
       <router-view>
         <template #default="{ Component, route }">
           <transition name="scale" mode="out-in" appear>
-            <keep-alive v-if="props.keepAlive">
+            <keep-alive>
               <component :is="Component" :key="route.fullPath" />
             </keep-alive>
-            <component :is="Component" v-else :key="route.fullPath" />
           </transition>
         </template>
       </router-view>
