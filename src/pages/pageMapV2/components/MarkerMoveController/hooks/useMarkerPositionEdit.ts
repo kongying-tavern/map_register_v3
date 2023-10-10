61b7356b2768faf882f2a7a0a0029d0da470fa19
夕云葛城 <kuroneko7520@outlook.com>
2023-10-11 01:12:39 +0800

    fix: 修复点位修改失败的问题

--- src/pages/pageMapV2/components/MarkerMoveController/hooks/useMarkerPositionEdit.ts
+++ src/pages/pageMapV2/components/MarkerMoveController/hooks/useMarkerPositionEdit.ts
@@ -1,4 +1,5 @@
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElNotification } from 'element-plus'
+import { omit } from 'lodash'
 import { useFetchHook } from '@/hooks'
 import { useCondition } from '@/pages/pageMapV2/hooks'
 import db from '@/database'
@@ -9,20 +10,39 @@ export const useMarkerPositionEdit = () => {
 
   const { refresh: moveMarker, onSuccess, onError, ...rest } = useFetchHook({
     onRequest: async (markers: API.MarkerVo[]) => {
-      await Promise.allSettled(markers.map(marker => Api.marker.updateMarker(marker)))
-
-      const { data: updateMarkers = [] } = await Api.marker.listMarkerById({}, markers.map(marker => marker.id!))
-
+      const results = {
+        success: [] as number[],
+        failed: [] as number[],
+      }
+      await Promise.all(markers.map(marker => Api.marker
+        .updateMarker(omit(marker, 'createTime, updateTime'))
+        .then(() => results.success.push(marker.id!))
+        .catch(() => results.failed.push(marker.id!)),
+      ))
+      const { data: updateMarkers = [] } = await Api.marker.listMarkerById({}, results.success)
       await db.marker.bulkPut(updateMarkers)
-
-      await conditionManager.requestMarkersUpdate()
+      return results
     },
   })
 
-  onSuccess(() => ElMessage.success({
-    message: '移动点位成功',
-    offset: 48,
-  }))
+  onSuccess(async ({ success, failed }) => {
+    if (success.length > 0) {
+      ElNotification.success({
+        title: '移动点位',
+        message: `点位 ${success.join(' , ')} 移动成功`,
+        offset: 48,
+      })
+      await conditionManager.requestMarkersUpdate()
+    }
+    // TODO 不完全成功，提示方式可能需要改进
+    if (failed.length > 0) {
+      ElNotification.error({
+        title: '移动点位',
+        message: `点位 ${failed.join(' , ')} 移动失败`,
+        offset: 48,
+      })
+    }
+  })
 
   onError(err => ElMessage.error({
     message: `移动点位失败，原因为：${err.message}`,
