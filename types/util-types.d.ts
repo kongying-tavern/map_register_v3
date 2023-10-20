7082c37b32967cd8422c3f761e599e396c17adcd
夕云葛城 <KuroNeko7520@outlook.com>
2023-10-20 17:29:44 +0800

    feat: 添加工具类型

--- types/util-types.d.ts
+++ types/util-types.d.ts
@@ -9,4 +9,15 @@ declare global {
 
   /** 提取数组项 */
   type Item<T extends unknown[]> = T[number]
+
+  /** 下划线风格转换为小驼峰风格 */
+  type SnakeCaseToCamelCase<S extends string> =
+  S extends `${infer Prefix}_${infer Rest}`
+    ? `${SnakeCaseToCamelCase<Prefix>}${Capitalize<SnakeCaseToCamelCase<Rest>>}`
+    : S
+
+  /** 转换对象的所有下划线风格 key 为小驼峰风格的 key */
+  type SnakeCaseKeysToCamelCase<T> = {
+    [K in keyof T as SnakeCaseToCamelCase<K>]: T[K];
+  }
 }
