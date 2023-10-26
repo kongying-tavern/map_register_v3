111aafa4738359cfbaa377b4c3460df3c4027748
夕云葛城 <KuroNeko7520@outlook.com>
2023-10-27 04:42:45 +0800

    feat: 图标预览引入裁切组件（未完成）

--- src/pages/pageIconManager/components/IconEditor.vue
+++ src/pages/pageIconManager/components/IconEditor.vue
@@ -1,27 +1,161 @@
 <script lang="ts" setup>
-import { useIconEdit } from '../hooks'
-import { IconDetailForm } from '.'
-import { GlobalDialogController } from '@/hooks'
+import { Check, Close } from '@element-plus/icons-vue'
+import { ElLoading } from 'element-plus'
+import { GlobalDialogController, useFetchHook } from '@/hooks'
+import { sleep } from '@/utils'
+import { AppImageCropper } from '@/components'
 
 const props = defineProps<{
-  icon: API.IconVo
+  icon: API.TagVo
 }>()
 
-const form = ref(props.icon)
-const { formRef, loading, editIcon } = useIconEdit(form)
+const url = ref(props.icon.url)
+const cropImageUrl = ref(props.icon.url)
+
+const selectImage = () => {
+  const loading = ElLoading.service({
+    text: '等待图片...',
+    background: 'var(--el-overlay-color-lighter)',
+  })
+  const input = document.createElement('input')
+  input.type = 'file'
+  input.accept = 'image/*'
+  input.multiple = false
+  input.onchange = () => {
+    loading.close()
+    const file = input.files?.[0]
+    if (!file)
+      return
+    input.remove()
+    if (url.value?.startsWith('blob:'))
+      URL.revokeObjectURL(url.value)
+    url.value = URL.createObjectURL(file)
+  }
+  input.click()
+}
+
+const imageCropperRef = ref<InstanceType<typeof AppImageCropper> | null>(null)
+const cropImage = async () => {
+  if (!imageCropperRef.value)
+    return
+  const image = await imageCropperRef.value.crop()
+  if (cropImageUrl.value?.startsWith('blob:'))
+    URL.revokeObjectURL(cropImageUrl.value)
+  cropImageUrl.value = URL.createObjectURL(image)
+}
+
+const { refresh: updateIconImage, loading } = useFetchHook({
+  onRequest: async () => {
+    await sleep(3000)
+  },
+})
+
+const closeDialog = () => {
+  if (loading.value)
+    return
+  GlobalDialogController.close()
+}
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 p-5">
-    <IconDetailForm ref="formRef" v-model="form" />
+  <div
+    class="rounded-lg
+      flex flex-col overflow-hidden
+      bg-[var(--el-fill-color)]
+      border border-[var(--el-color-primary-light-9)]"
+  >
+    <div class="flex justify-between items-center mb-2 bg-[var(--el-color-primary-light-9)]">
+      <div class="flex-1 p-1.5 px-2 text-xs overflow-hidden text-ellipsis whitespace-nowrap">
+        {{ icon.tag }} 修改图片
+      </div>
 
-    <div class="flex justify-end">
-      <el-button type="primary" :loading="loading" @click="editIcon">
+      <div
+        class="p-1.5 px-2
+          h-full
+          flex
+          items-center
+          hover:bg-[var(--el-color-danger)]
+          hover:text-[var(--el-bg-color)]
+          active:bg-[var(--el-color-danger-light-3)]
+          active:text-[var(--el-bg-color)]
+          transition-all
+          select-none"
+        @click="closeDialog"
+      >
+        <el-icon :size="16">
+          <Close />
+        </el-icon>
+      </div>
+    </div>
+
+    <div
+      class="flex-1 mx-2
+        p-2 overflow-auto
+        border border-[var(--el-border-color)]
+        bg-[var(--el-bg-color)]"
+    >
+      <div class="mb-1">
+        使用新图片：
+      </div>
+      <div class="flex gap-2">
+        <AppImageCropper ref="imageCropperRef" class="flex-shrink-0 w-64 h-64" :url="url" />
+
+        <div class="w-full flex flex-col justify-between">
+          <el-button @click="selectImage">
+            选择图片
+          </el-button>
+          <div class="divider" />
+          <div class="flex-1 grid place-items-center place-content-center gap-2">
+            <div>预览</div>
+            <div v-if="!cropImageUrl" class="w-16 h-16 border border-[var(--el-border-color)]" />
+            <img v-else class="w-16 h-16" :src="cropImageUrl">
+            <div class=" text-xs">
+              64x64 px
+            </div>
+          </div>
+          <div class="divider" />
+          <el-button @click="cropImage">
+            裁切图片
+          </el-button>
+        </div>
+      </div>
+
+      <div class="divider" />
+
+      <div class="mb-1 flex items-center gap-2">
+        <div class=" whitespace-nowrap">
+          使用已有图片：
+        </div>
+        <el-input size="small" placeholder="搜索名称..." />
+      </div>
+      <div class="h-32 grid grid-cols-10 grid-rows-4 overflow-auto">
+        <div v-for="i in 100" :key="i" class="w-8 h-8 border" />
+      </div>
+    </div>
+
+    <div class="flex justify-end p-2">
+      <el-button type="primary" :icon="Check" :loading="loading" disabled @click="updateIconImage">
         确认
       </el-button>
-      <el-button :disabled="loading" @click="GlobalDialogController.close">
+      <el-button :icon="Close" :disabled="loading" @click="closeDialog">
         取消
       </el-button>
     </div>
+
+    <el-alert type="warning" :closable="false">
+      <div>TODOs</div>
+      <ol>
+        <li>1. 支持拖入图片</li>
+        <li>2. 缩放时以视口中心为旋转中心，保持尺寸不变</li>
+      </ol>
+    </el-alert>
   </div>
 </template>
+
+<style scoped>
+.divider {
+  height: 1px;
+  background: var(--el-border-color);
+  margin: 8px 0;
+}
+</style>
--- src/pages/pageIconManager/components/IconPreviewer.vue
+++ src/pages/pageIconManager/components/IconPreviewer.vue
@@ -1,40 +1,122 @@
 <script setup lang="ts">
+import { ElMessage } from 'element-plus'
 import { cloneDeep, isEqual } from 'lodash'
 import type Node from 'element-plus/es/components/tree/src/model/node'
 import { Check, Delete } from '@element-plus/icons-vue'
+import dayjs from 'dayjs'
+import { IconEditor } from '.'
 import { useIconTagStore } from '@/stores'
 import Api from '@/api/api'
-import type { ItemFormRules } from '@/utils'
+import { type ItemFormRules } from '@/utils'
+import { useFetchHook, useGlobalDialog } from '@/hooks'
+import db from '@/database'
 
 const iconTagStore = useIconTagStore()
+const { DialogService } = useGlobalDialog()
 
+// ==================== 用户信息 ====================
+const userCache = ref<Record<number, API.SysUserVo>>({})
+
+const { refresh: getUserInfo, loading: isUserInfoLoading } = useFetchHook<void, [userId: number]>({
+  onRequest: async (userId: number) => {
+    if (userCache.value[userId])
+      return
+    const { data = {} } = await Api.sysUserController
+      .getUserInfo({ userId })
+      .catch(() => ({ data: {} as API.SysUserVo }))
+    userCache.value[userId] = data
+  },
+})
+
+// ==================== 图标信息 ====================
 const iconTag = defineModel<API.TagVo | null>('modelValue', {
   default: null,
 })
 
 const form = ref(cloneDeep(iconTag.value ?? {}))
 
-watch(() => iconTag.value, () => {
-  form.value = cloneDeep(iconTag.value ?? {})
+const isFormChange = computed(() => !isEqual(iconTag.value, form.value))
+const isTypeChange = computed(() => !isEqual(iconTag.value?.typeIdList ?? [], form.value.typeIdList ?? []))
+
+// ==================== 修改图片 ====================
+const openImageEditor = () => {
+  DialogService
+    .config({
+      width: 'fit-content',
+      closeOnClickModal: false,
+      closeOnPressEscape: false,
+      alignCenter: true,
+    })
+    .props({
+      icon: form.value,
+    })
+    .open(IconEditor)
+}
+
+// ==================== 修改类型 ====================
+const { refresh: submitEditIcon, loading: isEditing, onSuccess, onError } = useFetchHook({
+  onRequest: async () => {
+    const { tag = '', typeIdList } = form.value
+    isTypeChange.value && await Api.tag.updateTypeInTag({ tag, typeIdList })
+    const { data = {} } = await Api.tag.getTag({ name: tag })
+    return data
+  },
 })
 
-const isFormChange = computed(() => isEqual(iconTag.value, form.value))
+onSuccess(async (data) => {
+  ElMessage.success({
+    message: '编辑成功',
+    offset: 48,
+  })
+  await db.iconTag.put(data)
+  iconTag.value = data
+})
+
+onError(err => ElMessage.error({
+  message: `编辑失败，原因为：${err.message}`,
+  offset: 48,
+}))
 
 const rules: ItemFormRules<API.TagVo> = {
   tag: [{ required: true, message: '名称不能为空', validator: (_, v = '') => v.trim().length > 0, trigger: 'change' }],
 }
 
+watch(() => iconTag.value, () => {
+  if (isEditing.value)
+    return
+  form.value = cloneDeep(iconTag.value ?? {})
+  form.value.creatorId !== undefined && getUserInfo(form.value.creatorId)
+})
+
+// ==================== 图标类型 ====================
+const tagTypeCache = ref<Record<number, API.TagTypeVo[]>>({})
+
 const loadTagType = async (node: Node, resolve: (data: API.TagTypeVo[]) => void) => {
+  if (tagTypeCache.value[node.data.id] !== undefined)
+    return resolve(tagTypeCache.value[node.data.id])
   const { data: { record = [] } = {} } = await Api.tagType.listTagType({
     typeIdList: [node.level === 0 ? -1 : node.data.id],
     size: 256,
   })
+  tagTypeCache.value[node.data.id] = record
   resolve(record)
+}
+
+// ==================== 其他 ====================
+const timeFormatter = (time?: string) => {
+  if (!time)
+    return ''
+  return dayjs(time).format('YYYY-MM-DD HH:mm:ss')
 }
 </script>
 
 <template>
-  <div class="icon-previewer h-full">
+  <div
+    v-loading.fullscreen="isEditing"
+    element-loading-text="操作中..."
+    element-loading-background="var(--el-overlay-color-lighter)"
+    class="icon-previewer h-full"
+  >
     <div v-if="!iconTag" class="w-64 h-full grid place-items-center">
       选择要预览的图标
     </div>
@@ -44,20 +126,42 @@ const loadTagType = async (node: Node, resolve: (data: API.TagTypeVo[]) => void)
         class="icon-image h-64 grid place-items-center overflow-hidden flex-shrink-0"
         :style="{
           '--bg': `url(${iconTagStore.tagSpriteImage})`,
-          '--x': `${-iconTagStore.iconMapping[iconTag.tag!][0]}px`,
-          '--y': `${-iconTagStore.iconMapping[iconTag.tag!][1]}px`,
+          '--x': `${-iconTagStore.iconMapping[iconTag.tag!]?.[0]}px`,
+          '--y': `${-iconTagStore.iconMapping[iconTag.tag!]?.[1]}px`,
         }"
       >
         <div class="image-box w-16 h-16" />
       </div>
 
-      <div class="icon-detail flex-1 px-4">
-        <el-form label-width="52px" :rules="rules" :model="form">
-          <el-form-item label="名称" prop="tag">
-            <el-input v-model="form.tag" />
+      <div class="icon-detail flex-1 px-2">
+        <el-form label-width="70px" label-position="left" :rules="rules" :model="form" :disabled="isEditing">
+          <el-form-item label-width="0px" class="margin-bottom-0">
+            <el-button class="w-full" @click="openImageEditor">
+              修改图片
+            </el-button>
           </el-form-item>
 
-          <el-form-item label="分类" prop="typeIdList">
+          <el-form-item label="名称" class="margin-bottom-0">
+            <el-text truncated>
+              {{ JSON.stringify(form.tag).slice(1, -1) }}
+            </el-text>
+          </el-form-item>
+
+          <el-form-item label="作者" class="margin-bottom-0">
+            <el-text truncated>
+              {{ isUserInfoLoading ? '...' : userCache[form.creatorId ?? -1]?.nickname }} (id:{{ form.creatorId }})
+            </el-text>
+          </el-form-item>
+
+          <el-form-item label="创建时间" class="margin-bottom-0">
+            <el-text>{{ timeFormatter(form.createTime) }}</el-text>
+          </el-form-item>
+
+          <el-form-item label="修改时间" class="margin-bottom-0">
+            <el-text>{{ timeFormatter(form.updateTime) }}</el-text>
+          </el-form-item>
+
+          <el-form-item label="分类" prop="typeIdList" class="margin-bottom-0">
             <el-tree-select
               v-model="form.typeIdList"
               style="width: 100%"
@@ -75,20 +179,31 @@ const loadTagType = async (node: Node, resolve: (data: API.TagTypeVo[]) => void)
               :load="loadTagType"
             />
           </el-form-item>
-
-          <el-form-item label-width="0px" class="flex">
-            <el-button :disabled="isFormChange" :icon="Check" class="flex-1">
-              保存
-            </el-button>
-            <el-button :icon="Delete" type="danger" plain />
-          </el-form-item>
         </el-form>
+
+        <div class="flex mt-4">
+          <el-button
+            class="flex-1"
+            :disabled="!isFormChange"
+            :type="isFormChange ? 'primary' : 'default'"
+            :icon="Check"
+            :loading="isEditing"
+            @click="submitEditIcon"
+          >
+            保存
+          </el-button>
+          <el-button :icon="Delete" type="danger" plain :disabled="isEditing" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.margin-bottom-0 {
+  margin-bottom: 0;
+}
+
 .icon-previewer {
   overflow: hidden;
 }
@@ -96,10 +211,10 @@ const loadTagType = async (node: Node, resolve: (data: API.TagTypeVo[]) => void)
 .icon-image {
   --s: 32px;
 
-  --color-a: var(--el-fill-color-darker);
-  --color-b: transparent;
+  --color-a: transparent;
+  --color-b: var(--el-fill-color-darker);
 
-  border: 16px solid transparent;
+  border: 8px solid transparent;
   background: conic-gradient(
     from 0deg at 50% 50%,
     var(--color-a) 25%,
