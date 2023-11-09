3ab024acee3fa0f7eadfcfe345b9d8a7f3c3d91f
夕云葛城 <KuroNeko7520@outlook.com>
2023-11-09 18:31:05 +0800

    refactor: 使用 tsx 改善删除 hook 的提示信息

---
+++ src/pages/pageIconManager/hooks/useIconDelete.tsx
@@ -0,0 +1,46 @@
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { useFetchHook } from '@/hooks'
+import Api from '@/api/api'
+import db from '@/database'
+
+export const useIconDelete = () => {
+  const { refresh: submitDeleteIcon, loading, onSuccess, onError, ...rest } = useFetchHook({
+    onRequest: async (tag: API.TagVo) => {
+      await Api.tag.deleteTag({ tagName: tag.tag! })
+      await db.iconTag.delete(tag.tag!)
+    },
+  })
+
+  const confirmDeleteIcon = async (tag: API.TagVo) => ElMessageBox.confirm(
+    <span>该操作不可恢复，确认删除图标<span class="text-orange-400">{tag.tag}</span>？
+    </span>,
+    '警告',
+    {
+      confirmButtonClass: 'el-button--danger',
+      beforeClose: async (action, instance, done) => {
+        if (loading.value)
+          return
+        if (action !== 'confirm')
+          return done()
+        instance.confirmButtonLoading = true
+        instance.confirmButtonClass = 'el-button--danger is-loading'
+        await submitDeleteIcon(tag)
+        instance.confirmButtonLoading = false
+        done()
+      },
+    },
+  )
+    .catch(() => false)
+
+  onSuccess(() => ElMessage.success({
+    message: '删除成功',
+    offset: 48,
+  }))
+
+  onError(err => ElMessage.error({
+    message: `删除图标失败，原因为：${err.message}`,
+    offset: 48,
+  }))
+
+  return { confirmDeleteIcon, onSuccess, ...rest }
+}
