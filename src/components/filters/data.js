7a5ec41d3bf0dde2383fdb4fc53e1698c2f6a145
Alex Fang <deeno@qq.com>
2023-11-13 11:13:09 +0800

    feat: 新增过滤器14 - 特殊刷新时间

--- src/components/filters/data.js
+++ src/components/filters/data.js
@@ -12,7 +12,7 @@ import {
   selectorCollapse,
   selectorItemId,
 } from "src/components/selector-data";
-import { map_plugin_config } from "src/api/config";
+import { map_editor_config, map_plugin_config } from "src/api/config";
 import { is_neigui } from "src/service/user_info";
 
 export const filterCardVisible = ref(false);
@@ -844,6 +844,62 @@ export const filterTypes = [
     },
     filterSlots: {
       append: filterTypeSlots.refreshTimeAppend,
+    },
+  },
+  {
+    id: 14,
+    name: "refresh-time-special",
+    icon: "mdi-clock-time-three-outline",
+    title: "特殊刷新时间",
+    label: "特殊刷新时间为",
+    model: "select",
+    modelOpts: {
+      multiple: true,
+      useChips: true,
+      optionValue: "value",
+      optionsFunc: computed(
+        () => map_editor_config?.value?.refreshTimeSpecial || []
+      ),
+    },
+    modelVals: {
+      value: [],
+    },
+
+    modelSemantic(values = {}, options = {}, oppositeValue = false) {
+      const selectedValue = values.value || [];
+      if (!_.isArray(selectedValue) || selectedValue.length <= 0) {
+        return "";
+      }
+
+      const optionsFull = options.optionsFunc || [];
+      const optionsMap = _.chain(optionsFull)
+        .keyBy("value")
+        .mapValues((v) => v.label)
+        .value();
+      const selectedNames = _.chain(selectedValue)
+        .map((v) => optionsMap[v])
+        .filter((v) => v)
+        .uniq()
+        .value();
+      const selectedNameStr = selectedNames.join(",");
+
+      if (!selectedNameStr) {
+        return "";
+      }
+
+      return `${oppositeValue ? "非" : ""}${selectedNameStr}刷新`;
+    },
+    // eslint-disable-next-line no-unused-vars
+    filterAction(item = {}, values = {}, options = {}) {
+      const allowedValues = values.value || [];
+      if (!_.isArray(allowedValues) || allowedValues.length <= 0) {
+        return true;
+      }
+
+      const refreshTime = item.refreshTime ?? 0;
+      const allowed =
+        refreshTime < 0 && allowedValues.indexOf(refreshTime) !== -1;
+      return allowed;
     },
   },
   // Schema Example:
