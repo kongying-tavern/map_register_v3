db22db7c187f2eb50dc3752d2bf53ebc28b7cc21
夕云葛城 <KuroNeko7520@outlook.com>
2023-11-26 16:59:43 +0800

    refactor: 优化物品图标渲染逻辑，添加项目间隙以及去重优化

--- src/worker/tagSpriteRenderer.worker.ts
+++ src/worker/tagSpriteRenderer.worker.ts
@@ -1,7 +1,5 @@
 import { getObjectFitSize } from '@/utils/getObjectFitSize'
 
-export {}
-
 declare const globalThis: DedicatedWorkerGlobalScope
 
 const FALLBACK_IMAGE_URL = '/icons/unknown.webp'
@@ -12,6 +10,8 @@ export interface WorkerInput {
   tagList: { tag: string; url: string }[]
   /** 图标尺寸 @default 64 */
   size?: number
+  /** 图标之间的空隙，用于避免精度误差导致的重叠 */
+  gap?: number
   /** 并发请求限制 @default 1000 */
   maxRequests?: number
 }
@@ -36,64 +36,122 @@ const calculateGrid = (length: number) => {
   }
 }
 
-const loadImage = async (url: string, fallback?: ImageBitmap) => {
-  const mission = fetch(url, { mode: 'cors' })
+const loadImage = async (url: string) => {
+  return fetch(url, { mode: 'cors' })
     .then(res => res.blob())
     .then(blob => createImageBitmap(blob))
-  return fallback ? mission.catch(() => fallback) : mission
+}
+
+interface LimitPromiseAllOptions<T> {
+  maxRequests?: number
+  initResult?: T[]
+}
+
+const limitPromiseAll = async <D, T>(
+  data: D[],
+  toPromise: (item: D, index: number) => Promise<T | null>,
+  {
+    maxRequests = 10,
+    initResult = [],
+  }: LimitPromiseAllOptions<T> = {},
+) => {
+  let requestQueue: Promise<T | null>[] = []
+
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i]
+    requestQueue.push(toPromise(item, i))
+    if (requestQueue.length >= maxRequests) {
+      initResult = initResult.concat((await Promise.all(requestQueue)).filter(Boolean) as T[])
+      requestQueue = []
+      continue
+    }
+  }
+  if (requestQueue.length > 0)
+    initResult = initResult.concat((await Promise.all(requestQueue)).filter(Boolean) as T[])
+
+  return initResult
 }
 
 const render = async (params: WorkerInput): Promise<WorkerSuccessOutput> => {
   const {
     tagList,
     size = 64,
+    gap = 2,
     maxRequests = 1000,
   } = params
 
   if (!Number.isInteger(size) || size < 0)
     throw new Error('图标尺寸必须为大于 0 的整数。此外，出于渲染效果的考虑，不建议尺寸小于 32。')
 
-  const { cols, rows } = calculateGrid(tagList.length)
-  const canvasSize = cols * size
-  const canvas = new OffscreenCanvas(canvasSize, canvasSize)
-  const ctx = canvas.getContext('2d')!
-
   // 备用图片
   const fallbackImage = await loadImage(FALLBACK_IMAGE_URL)
 
-  // 请求图片
-  let tagImageCaches: ImageBitmap[] = []
-  let requestQueue: Promise<ImageBitmap>[] = []
-  for (let i = 0; i < tagList.length; i++) {
-    const { url } = tagList[i]
-    requestQueue.push(loadImage(url, fallbackImage))
-    if (requestQueue.length >= maxRequests) {
-      tagImageCaches = tagImageCaches.concat(await Promise.all(requestQueue))
-      requestQueue = []
-      continue
-    }
+  // ==================== url 去重 ====================
+
+  const urlTagsMap = new Map<string, string[]>()
+  tagList.forEach(({ tag, url }) => {
+    const tags = urlTagsMap.get(url)
+    if (!tags)
+      urlTagsMap.set(url, [tag])
+    else
+      urlTagsMap.set(url, [...tags, tag])
+  })
+
+  const fetchList: { tags: string[]; url: string }[] = []
+  urlTagsMap.forEach((tags, url) => {
+    fetchList.push({ tags, url })
+  })
+
+  // ==================== 图片请求 ====================
+
+  interface RenderObject {
+    tags: string[]
+    image: ImageBitmap
   }
-  if (requestQueue.length > 0)
-    tagImageCaches = tagImageCaches.concat(await Promise.all(requestQueue))
+
+  const fallbackRenderObject: RenderObject = {
+    tags: [],
+    image: fallbackImage,
+  }
+
+  const renderList = await limitPromiseAll(fetchList, async ({ tags, url }) => {
+    try {
+      const image = await loadImage(url)
+      return {
+        image,
+        tags,
+      } as RenderObject
+    }
+    catch {
+      // 对于请求失败的 tags 使用 fallback 图片所在的位置
+      fallbackRenderObject.tags.push(...tags)
+      return null
+    }
+  }, {
+    maxRequests,
+    initResult: [fallbackRenderObject],
+  })
+
+  // ==================== 绘制矩阵 ====================
+
+  const { cols, rows } = calculateGrid(renderList.length)
+  const canvasSize = cols * size + (cols - 1) * gap
+  const canvas = new OffscreenCanvas(canvasSize, canvasSize)
+  const ctx = canvas.getContext('2d')!
 
   // 生成 mapping 并绘制
   const mapping: Record<string, [number, number]> = {}
-  ;(() => {
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
-        const index = row * cols + col
-        const { tag } = tagList[index] ?? {}
-        const img = tagImageCaches[index]
-        if (!tag || !img)
-          return
-        const x = col * size
-        const y = row * size
-        mapping[tag] = [x, y]
-        const { sx, sy, sw, sh, dx, dy, dw, dh } = getObjectFitSize('contain', size, size, img.width, img.height)
-        ctx.drawImage(img, sx, sy, sw, sh, x + dx, y + dy, dw, dh)
-      }
-    }
-  })()
+  renderList.forEach(({ tags, image }, index) => {
+    const row = Math.floor(index / rows)
+    const col = index - row * cols
+    const x = col * (size + gap)
+    const y = row * (size + gap)
+    const { sx, sy, sw, sh, dx, dy, dw, dh } = getObjectFitSize('contain', size, size, image.width, image.height)
+    ctx.drawImage(image, sx, sy, sw, sh, x + dx, y + dy, dw, dh)
+    tags.forEach((tag) => {
+      mapping[tag] = [x, y]
+    })
+  })
 
   const image = await (await canvas.convertToBlob({ type: 'image/png' })).arrayBuffer()
   return { image, mapping }
