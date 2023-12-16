75804fadcd62ab3c171668dda9ad8f827cde40b6
夕云葛城 <KuroNeko7520@outlook.com>
2023-12-16 17:59:17 +0800

    docs: 添加注释

--- src/utils/limitPromiseAll.ts
+++ src/utils/limitPromiseAll.ts
@@ -3,6 +3,11 @@ export interface LimitPromiseAllOptions<T> {
   initResult?: T[]
 }
 
+/**
+ * 限制 promise 并发数，使用 Promise.all 进行处理
+ *
+ * 注意！返回为 null 的项会被过滤
+ */
 export const limitPromiseAll = async <D, T>(
   data: D[],
   toPromise: (item: D, index: number) => Promise<T | null>,
@@ -19,9 +24,10 @@ export const limitPromiseAll = async <D, T>(
     if (requestQueue.length >= maxRequests) {
       initResult = initResult.concat((await Promise.all(requestQueue)).filter(Boolean) as T[])
       requestQueue = []
-      continue
     }
   }
+
+  // 清理剩余队列任务
   if (requestQueue.length > 0)
     initResult = initResult.concat((await Promise.all(requestQueue)).filter(Boolean) as T[])
 
