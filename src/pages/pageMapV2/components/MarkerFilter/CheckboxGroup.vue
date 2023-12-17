ce078430a4d42338b1af392ef1e64a3958a692e2
夕云葛城 <KuroNeko7520@outlook.com>
2023-12-17 11:33:37 +0800

    refactor: 优化多选组组件的泛型传递

--- src/pages/pageMapV2/components/MarkerFilter/CheckboxGroup.vue
+++ src/pages/pageMapV2/components/MarkerFilter/CheckboxGroup.vue
@@ -1,85 +1,89 @@
-<!-- eslint-disable @typescript-eslint/no-explicit-any -->
-<script lang="ts" setup generic="T extends Record<string, any>, R extends unknown = unknown">
+<script lang="ts" setup generic="T extends Record<string, any>, LK extends keyof T, VK extends keyof T, V extends T[VK], VV extends V[]">
 import { Finished } from '@element-plus/icons-vue'
 import { CheckboxItem } from '.'
 
 const props = defineProps<{
-  modelValue?: any
   options: T[]
-  labelKey: string
-  valueKey: string
+  /** 实际标签的 key 名 */
+  labelKey: LK
+  /** 实际值的 key 名 */
+  valueKey: VK
   /** 多选 */
   multiple?: boolean
   /** 两列 */
   twoCol?: boolean
   /** 是否显示全选按钮，仅在 multiple 模式下生效 */
   showSelectAllBtn?: boolean
-  /** 需要通过列表项值来计算的属性 */
-  getComputed?: (row: T) => R
   /** 拖拽控制 key */
   draggable?: boolean
 }>()
 
 const emits = defineEmits<{
-  (e: 'update:modelValue', v?: any): void
-  (e: 'change', v?: any): void
+  (e: 'change', v?: V | VV): void
 }>()
 
-const isArrayValue = computed(() => Array.isArray(props.modelValue))
-
-/** 多选值 */
-const multipleValue = computed<any[]>({
-  get: () => isArrayValue.value ? props.modelValue : [],
-  set: v => emits('update:modelValue', v),
+/** 单选值 */
+const modelValue = defineModel<V>('modelValue', {
+  required: false,
+  default: undefined,
 })
 
-/** 单选值 */
-const singleValue = computed({
-  get: () => isArrayValue.value ? undefined : props.modelValue,
-  set: v => emits('update:modelValue', v),
+/** 多选值 */
+const modelMultipleValue = defineModel<VV>('multipleValue', {
+  required: false,
+  default: [],
 })
 
 const isActived = (targetOpt: T) => {
   return !props.multiple
-    ? singleValue.value === targetOpt[props.valueKey]
-    : multipleValue.value.findIndex(value => value === targetOpt[props.valueKey]) > -1
-}
-
-const patchArrayValue = (values: any[]) => {
-  multipleValue.value = values
-  emits('change', values)
+    ? modelValue.value === targetOpt[props.valueKey]
+    : modelMultipleValue.value.findIndex(value => value === targetOpt[props.valueKey]) > -1
 }
 
 const patchValue = (patchOpt: T) => {
   // 单选
   if (!props.multiple) {
-    singleValue.value = patchOpt[props.valueKey]
-    emits('change', patchOpt[props.valueKey])
-    return
+    modelValue.value = patchOpt[props.valueKey]
+    emits('change', patchOpt[props.valueKey] as V)
   }
-
   // 多选
-  const shallowCopyValue = [...multipleValue.value]
-  const findIndex = shallowCopyValue.findIndex((value: any) => value === patchOpt[props.valueKey])
-  if (findIndex > -1)
-    shallowCopyValue.splice(findIndex, 1)
-  else
-    shallowCopyValue.push(patchOpt[props.valueKey])
-  patchArrayValue(shallowCopyValue)
+  else {
+    const shallowCopyValue = [...modelMultipleValue.value]
+    const findIndex = shallowCopyValue.findIndex(value => value === patchOpt[props.valueKey])
+    if (findIndex > -1)
+      shallowCopyValue.splice(findIndex, 1)
+    else
+      shallowCopyValue.push(patchOpt[props.valueKey])
+    modelMultipleValue.value = shallowCopyValue as VV
+    emits('change', shallowCopyValue as VV)
+  }
 }
 
 const isAllSelected = computed(() => {
-  const set = new Set(multipleValue.value)
+  const set = new Set(modelMultipleValue.value)
   for (const item of props.options) {
     if (!set.has(item[props.valueKey]))
       return false
   }
   return true
 })
+
 const toggleAllSelect = () => {
   if (!props.multiple)
     return
-  patchArrayValue(isAllSelected.value ? [] : props.options.map(item => item[props.valueKey]))
+  const toggledValue = ((() => {
+    const existValues = new Set(modelMultipleValue.value)
+    isAllSelected.value
+      ? props.options.forEach(item => existValues.delete(item[props.valueKey]))
+      : props.options.forEach((item) => {
+        const value = item[props.valueKey]
+        !existValues.has(value) && existValues.add(value)
+      })
+    return [...existValues]
+  })())
+
+  modelMultipleValue.value = toggledValue as VV
+  emits('change', toggledValue as VV)
 }
 
 // ==================== 拖拽控制 ====================
@@ -115,20 +119,19 @@ const handleDragStart = (ev: DragEvent, row: T) => {
 
       <CheckboxItem
         v-for="opt in options"
-        :key="`${opt[valueKey]}`"
-        :label="opt[labelKey]"
-        :props="getComputed?.(opt)"
+        :key="`${opt[valueKey as keyof T]}`"
+        :label="opt[labelKey as keyof T]"
         :is-actived="isActived(opt)"
         :draggable="draggable"
         @dragstart="(ev: DragEvent) => handleDragStart(ev, opt)"
         @click="() => patchValue(opt)"
       >
         <template v-if="$slots.icon" #icon>
-          <slot name="icon" :row="opt" :actived="isActived(opt)" :props="getComputed?.(opt)" />
+          <slot name="icon" :row="opt" :actived="isActived(opt)" />
         </template>
 
         <template v-if="$slots.default" #default>
-          <slot name="default" :row="opt" :actived="isActived(opt)" :props="getComputed?.(opt)" />
+          <slot name="default" :row="opt" :actived="isActived(opt)" />
         </template>
       </CheckboxItem>
     </div>
