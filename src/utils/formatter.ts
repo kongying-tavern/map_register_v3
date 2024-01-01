a625d75760448df9f63d543417c8e3af94de512b
夕云葛城 <KuroNeko7520@outlook.com>
2024-01-02 00:14:45 +0800

    feat: 时间格式化工具函数

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -27,3 +27,14 @@ export const refreshTimeFormatter = (_: unknown, __: unknown, time = 0) => {
   mins > 0 && labels.push(`${mins} 分钟`)
   return labels.join(' ')
 }
+
+export const plainTimeFormatter = (time = 0) => {
+  const labels: string[] = []
+  const days = Math.floor(time / 86_400_000)
+  days > 0 && labels.push(`${days} 天`)
+  const hours = Math.floor(time % 86_400_000 / 3_600_000)
+  hours > 0 && labels.push(`${hours} 小时`)
+  const mins = Math.floor(time % 3600 / 60_000)
+  mins > 0 && labels.push(`${mins} 分钟`)
+  return labels.join(' ')
+}
