e8714db62a159d8eb83ccd928f6e2d4c94f328f3
夕云葛城 <KuroNeko7520@outlook.com>
2024-01-05 13:53:57 +0800

    refactor: 删除不需要的依赖注入

--- src/pages/pageMapV2/components/MapStateBar/shared/injectionKey.ts
+++
@@ -1,4 +0,0 @@
-import type { InjectionKey } from 'vue'
-
-export const setElementKey = Symbol('set-element-ref') as InjectionKey<(target: HTMLElement) => void>
-export const setContentKey = Symbol('set-content-ref') as InjectionKey<(content: string) => void>
