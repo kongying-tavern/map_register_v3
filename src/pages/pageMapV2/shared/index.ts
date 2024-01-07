823c92fb44084146cbb66a10a8721ff27e00d375
夕云葛城 <KuroNeko7520@outlook.com>
2024-01-07 14:48:05 +0800

    fix: value fix

--- src/pages/pageMapV2/shared/index.ts
+++ src/pages/pageMapV2/shared/index.ts
@@ -5,5 +5,6 @@ export const MAP_FONTFAMILY = 'map-font-zhcn'
 export const mutuallyExclusiveLayerKey = Symbol('mutually-exclusive-layer') as InjectionKey<Ref<HTMLElement | null>>
 export const genshinMapCanvasKey = Symbol('canvas-ref') as InjectionKey<Ref<HTMLCanvasElement | null>>
 export const mapAffixLayerKey = Symbol('map-affix-layer') as InjectionKey<Ref<HTMLElement | null>>
+export const mapSidermenuKey = Symbol('map-sidermenu-ref') as InjectionKey<Ref<HTMLElement | null>>
 
 export * from './transition'
