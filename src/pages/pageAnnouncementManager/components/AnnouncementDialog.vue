abdcb93dff7780e12159806582210741e341ea32
766aya <1494233259@qq.com>
2024-01-09 06:19:13 +0800

    颜色设置

--- src/pages/pageAnnouncementManager/components/AnnouncementDialog.vue
+++ src/pages/pageAnnouncementManager/components/AnnouncementDialog.vue
@@ -15,14 +15,12 @@ const emits = defineEmits<{
 
 const props = defineProps<{
   form: API.NoticeVo,
-  status: string
+  status: "detail" | "create" | "update"
 }>()
 
 const formData = ref<API.NoticeVo>({
   title: '',
 })
-
-
 
 const formRef = ref<ElFormType | null>(null)
 
@@ -30,27 +28,41 @@ const rules: ItemFormRules<API.NoticeVo & { validDateTime: [number, number] }> =
   channel: [
     { required: true, message: '公告频道不能为空', trigger: 'change' },
   ],
-  validDateTime: [
-    {
-      required: true,
-      validator(rule, value, callback) {
-        if (formData.value.validTimeStart && formData.value.validTimeEnd)
-          callback()
-        else
-          callback('公告有效期不能为空')
-      },
-      trigger: 'change',
-    },
-  ],
   title: [
     { required: true, message: '公告标题不能为空', trigger: 'blur' },
+  ],
+  validTimeStart: [
+    {
+      required: !!(formData.value?.validTimeStart && formData.value?.validTimeEnd),
+      validator (rule, value, cb) {
+        if (value && formData.value.validTimeEnd) {
+          if (formData.value.validTimeEnd <= value) {
+            cb("失效时间不能大于发布时间")
+            return
+          }
+        }
+        cb()
+      }
+    }
+  ],
+  validTimeEnd: [
+    {
+      required: !!(formData.value?.validTimeStart && formData.value?.validTimeEnd),
+      validator (rule, value, cb) {
+        if (value && formData.value.validTimeStart) {
+          if (formData.value.validTimeStart >= value) {
+            cb("失效时间不能大于发布时间")
+            return
+          }
+        }
+        cb()
+      }
+    }
   ],
   content: [
     { required: true, message: '公告内容不能为空', trigger: 'blur' },
   ],
 }
-
-const validDateTime = ref()
 
 watch(props.form, (val) => {
   formData.value = cloneDeep(val);
@@ -68,7 +80,7 @@ const { loading, refresh: submit, onSuccess, onError } = useFetchHook({
   onRequest: async () => {
     if (props.status === 'create') {
       await Api.notice.createNotice(formData.value)
-    } else {
+    } else if (props.status === 'update') {
       await Api.notice.updateNotice(formData.value)
     }
   },
@@ -97,25 +109,29 @@ onSuccess(() => {
   emits('success')
 })
 
+const statusMap = {
+  "detail": "详情",
+  "create": "新增",
+  "update": "修改"
+}
+
 onError(err => ElMessage.error({
-  message: `新增公告失败，原因为：${err.message}`,
+  message: `${statusMap[props.status || 'detail']}公告失败，原因为：${err.message}`,
   offset: 48,
 }))
-
-watch(validDateTime, (value) => {
-  if (Array.isArray(value) && value.length === 2) {
-    formData.value.validTimeStart = value[0]
-    formData.value.validTimeEnd = value[1]
-  }
-  else {
-    formData.value.validTimeStart = undefined
-    formData.value.validTimeEnd = undefined
-  }
-})
 
 onBeforeUnmount(() => {
   editor && editor.destroy()
 })
+
+const colors = [
+  "#FF8080", "#FFFF80", "#80FF80", "#00FF80", "#80FFFF", "#0080FF", "#FF80C0", "#FF80FF",
+  "#FF0000", "#FFFF00", "#80FF00", "#00FF40", "#00FFFF", "#0080C0", "#0080C0", "#FF00FF",
+  "#804040", "#FF8040", "#00FF00", "#008080", "#004080", "#8080FF", "#800040", "#FF0080",
+  "#800000", "#FF8000", "#008000", "#008040", "#0000FF", "#0000A0", "#800080", "#8000FF",
+  "#400000", "#804000", "#004000", "#004040", "#000080", "#000040", "#400040", "#400080",
+  "#FFFFFF", "#808000", "#808040", "#808080", "#408080", "#C0C0C0", "#400040",
+]
 
 </script>
 
@@ -128,8 +144,8 @@ onBeforeUnmount(() => {
       <el-form-item label="标题" prop="title">
         <el-input v-model="formData.title" placeholder="请输入标题" clearable />
       </el-form-item>
-      <el-form-item label="生效时间" prop="validTimeStart">
-        <el-date-picker style="--el-date-editor-width: 100%" type="datetime" v-model="formData.validTimeStart" value-format="x" placeholder="请选择生效时间" />
+      <el-form-item label="发布时间" prop="validTimeStart">
+        <el-date-picker style="--el-date-editor-width: 100%" type="datetime" v-model="formData.validTimeStart" value-format="x" placeholder="请选择发布时间" />
       </el-form-item>
       <el-form-item label="失效时间" prop="validTimeEnd">
         <el-date-picker style="--el-date-editor-width: 100%" type="datetime" v-model="formData.validTimeEnd" value-format="x" placeholder="请选择失效时间" />
@@ -143,9 +159,20 @@ onBeforeUnmount(() => {
             <div class="tool-item tool-item-italic" @click="editor.chain().focus().toggleItalic().run()">
               <svg viewBox="0 0 1024 1024" p-id="5393" width="64" height="64"><path d="M415.3 63.9l-9.3 45.8c87.3-3.4 121.8 23.7 103.4 81.4l-130 636.1c-6.5 64.5-52.3 93.3-137.7 86.5l-9.3 45.8h375l9.3-45.8c-91.4 6.8-125.5-22-102.3-86.5l130-636.1c1.8-57.7 47.3-84.8 136.6-81.4l9.3-45.8h-375z" fill="currentColor" p-id="5394"></path></svg>
             </div>
-            <div class="tool-item tool-item-color">
-              <svg viewBox="0 0 1024 1024" p-id="6524" width="64" height="64"><path d="M768 800a31.36 31.36 0 0 1-29.44-19.84L512 214.4l-226.56 565.76a32 32 0 1 1-58.88-24.32l256-640a32.64 32.64 0 0 1 58.88 0l256 640a31.36 31.36 0 0 1-17.28 41.6 29.44 29.44 0 0 1-12.16 2.56z" p-id="6525" fill="currentColor"></path><path d="M657.92 544H358.4a32 32 0 0 1 0-64h299.52a32 32 0 1 1 0 64zM800 960h-576a32 32 0 0 1 0-64h576a32 32 0 0 1 0 64z" p-id="6526" fill="currentColor"></path></svg>
-            </div>
+            <el-popover
+              placement="bottom"
+              width="fit-content"
+              trigger="hover">
+              <template #reference>
+                <div class="tool-item tool-item-color">
+                  <svg viewBox="0 0 1024 1024" p-id="6524" width="64" height="64"><path d="M768 800a31.36 31.36 0 0 1-29.44-19.84L512 214.4l-226.56 565.76a32 32 0 1 1-58.88-24.32l256-640a32.64 32.64 0 0 1 58.88 0l256 640a31.36 31.36 0 0 1-17.28 41.6 29.44 29.44 0 0 1-12.16 2.56z" p-id="6525" fill="currentColor"></path><path d="M657.92 544H358.4a32 32 0 0 1 0-64h299.52a32 32 0 1 1 0 64zM800 960h-576a32 32 0 0 1 0-64h576a32 32 0 0 1 0 64z" p-id="6526" fill="currentColor"></path></svg>
+                </div>
+              </template>
+              <div class="editor-color-layout">
+                <div class="color-item" v-for="item in colors" :key="item" :style="{'--color': item}" @click="editor.chain().focus().setColor(item).run()"></div>
+                <div class="color-item unset" @click="editor.chain().focus().unsetColor().run()"></div>
+              </div>
+            </el-popover>
             <div class="tool-item tool-item-size">
               <svg viewBox="0 0 1024 1024" p-id="7512" width="64" height="64"><path d="M711.328 735.968l-33.792 92.8a32 32 0 0 1-60.16-21.888l130.368-358.08a31.968 31.968 0 0 1 42.016-21.888c10.208 3.712 17.376 12.096 19.968 21.856l130.336 358.08a32 32 0 0 1-60.16 21.92l-33.76-92.8H711.36z m23.264-64h88.256l-44.128-121.28L734.592 672zM234.752 576h202.496L336 297.856 234.752 576z m-23.296 64L144.64 823.648a32 32 0 0 1-60.128-21.888L303.36 200.32a32 32 0 0 1 32.64-20.96 32 32 0 0 1 32.64 20.96l218.88 601.408a32 32 0 0 1-60.16 21.888L460.576 640H211.456z" p-id="7513" fill="currentColor"></path></svg>
             </div>
@@ -193,17 +220,10 @@ onBeforeUnmount(() => {
         width: 100%;
         height: 80%;
       }
-
-      &-bold {
-      }
       &-italic {
         svg {
           height: 60%;
         }
-      }
-      &-color {
-      }
-      &-size {
       }
     }
   }
@@ -212,6 +232,14 @@ onBeforeUnmount(() => {
     width: 100%;
     height: 120px;
     overflow: hidden;
+
+    size {
+      font-size: calc(var(--size, 1) * 16px);
+    }
+
+    color {
+      color: var(--color, #000000);
+    }
 
     .tiptap  {
       width: 100%;
@@ -226,6 +254,34 @@ onBeforeUnmount(() => {
       &:focus {
         border-color: #409EFF;
       }
+    }
+  }
+}
+
+.editor-color-layout {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  gap: 10px;
+  .color-item {
+    position: relative;
+    width: 20px;
+    height: 20px;
+    background-color: var(--color);
+    transition: .3s;
+    border: 1px solid #5f5f5f;
+    overflow: hidden;
+    &:hover {
+      box-shadow: 0px 0px 5px #DCDFE6;
+    }
+    &.unset::before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      height: 1px;
+      width: 120px;
+      transform: translate(-50%, -50%) rotate(45deg);
+      background-color: #FF0000;
     }
   }
 }
