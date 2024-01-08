82dd923590fb83a7ef71564a74a2556cacb8f608
夕云葛城 <KuroNeko7520@outlook.com>
2024-01-08 16:16:37 +0800

    chore: update api

--- src/api/api/resource.ts
+++ src/api/api/resource.ts
@@ -4,17 +4,35 @@ import { request } from '@/utils'
 /** 上传图片 上传图片至图床并返回访问地址 PUT /api/res/upload/image */
 export async function uploadImage(
   body: {
-    file?: string
+    file?: File
     uploadVo?: API.ResourceUploadVo
   },
   options?: AxiosRequestConfig,
 ) {
+  const formData = new FormData()
+
+  Object.keys(body).forEach((ele) => {
+    const item = (body as any)[ele]
+
+    if (item !== undefined && item !== null) {
+      if (typeof item === 'object') {
+        for (const k of item) {
+          const v = item[k]
+          formData.append(k, v)
+        }
+      }
+      else {
+        formData.append(ele, item)
+      }
+    }
+  })
+
   return request<API.RResourceUploadVo>('/api/res/upload/image', {
     method: 'PUT',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'multipart/form-data',
     },
-    data: body,
+    data: formData,
     ...(options || {}),
   })
 }
