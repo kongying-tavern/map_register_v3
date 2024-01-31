862374c289e640d676e61d0730b3b3da6de6cf8d
Alex Fang <deeno@qq.com>
2024-01-31 08:33:00 +0800

    fix: 修复用户列表搜索样式问题

--- src/pages/pageUserManager/components/UserFilter.vue
+++ src/pages/pageUserManager/components/UserFilter.vue
@@ -21,16 +21,13 @@ const internalFilterKey = computed({
 
 <template>
   <el-form>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-8 place-items-end">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-8">
       <el-form-item label="搜索">
-        <el-input v-model="interValue" class="type-input" clearable>
-          <template #prepend>
-            <el-select v-model="internalFilterKey" class="w-24">
-              <el-option label="昵称" value="nickname" />
-              <el-option label="用户名" value="username" />
-            </el-select>
-          </template>
-        </el-input>
+        <el-select v-model="internalFilterKey">
+          <el-option label="昵称" value="nickname" />
+          <el-option label="用户名" value="username" />
+        </el-select>
+        <el-input v-model="interValue" class="type-input" clearable />
       </el-form-item>
 
       <el-form-item />
