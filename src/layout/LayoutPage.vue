122a7700e154e8e10d4f6b8978b1d69ae34b5972
夕云葛城 <KuroNeko7520@outlook.com>
2024-02-04 06:30:19 +0800

    refactor: 优化管理页的路由过渡效果

--- src/layout/LayoutPage.vue
+++ src/layout/LayoutPage.vue
@@ -1,19 +1,70 @@
+<script setup lang="ts">
+const router = useRouter()
+
+const getRouteIndex = (routePath: string) => {
+  return router.getRoutes().findIndex(({ path }) => path === routePath)
+}
+
+const transitionName = ref<'route-slide-up' | 'route-slide-down'>('route-slide-down')
+
+const range = ref(30)
+
+onBeforeRouteUpdate((updateGuard, from, next) => {
+  const startIndex = getRouteIndex(from.path)
+  const endIndex = getRouteIndex(updateGuard.path)
+  range.value = 30 * Math.abs(startIndex - endIndex)
+  transitionName.value = startIndex > endIndex ? 'route-slide-down' : 'route-slide-up'
+  next(true)
+})
+</script>
+
 <template>
   <router-view>
-    <template #default="{ Component, route }">
+    <template #default="{ Component, route: currentRoute }">
       <div
         v-bind="$attrs"
         class="h-full overflow-hidden transition-all duration-200"
-        :class="{
-          'p-4': !route.meta.noPadding,
-        }"
       >
-        <transition name="scale" mode="out-in" appear>
+        <transition :name="transitionName" mode="out-in" appear :style="{ '--range': range }">
           <keep-alive>
-            <component :is="Component" :key="route.fullPath" />
+            <component :is="Component" :key="currentRoute.fullPath" />
           </keep-alive>
         </transition>
       </div>
     </template>
   </router-view>
 </template>
+
+<style lang="scss">
+.route-slide-down {
+  &-enter-active, &-leave-active {
+    transition: all 0.1s ease-out;
+  }
+
+  &-enter-from {
+    opacity: 0;
+    transform: translateY(calc((var(--range)) * -1px));
+  }
+
+  &-leave-to {
+    opacity: 0;
+    transform: translateY(calc(var(--range) * 1px));
+  }
+}
+
+.route-slide-up {
+  &-enter-active, &-leave-active {
+    transition: all 0.1s ease-out;
+  }
+
+  &-enter-from {
+    opacity: 0;
+    transform: translateY(calc(var(--range) * 1px));
+  }
+
+  &-leave-to {
+    opacity: 0;
+    transform: translateY(calc(var(--range) * -1px));
+  }
+}
+</style>
