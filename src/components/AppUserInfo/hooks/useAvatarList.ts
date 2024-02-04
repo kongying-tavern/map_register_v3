47b9903d7e7295dd1991e7140c4b01f44ca2247c
夕云葛城 <KuroNeko7520@outlook.com>
2024-02-04 21:03:12 +0800

    refactor: 使用汉字匹配逻辑代替原来的 name 获取逻辑

--- src/components/AppUserInfo/hooks/useAvatarList.ts
+++ src/components/AppUserInfo/hooks/useAvatarList.ts
@@ -17,30 +17,46 @@ export interface MiyousheAvatarResponse {
   }
 }
 
+const cachedAvatars = shallowRef<MiyousheAvatar[]>([])
+
+const autoResetTimer = ref<number>()
+
 /** 头像列表 */
 export const useAvatarList = () => {
-  const { data: avatarList, ...rest } = useFetchHook({
-    immediate: true,
+  const { refresh, onSuccess, ...rest } = useFetchHook({
+    initialValue: [],
     onRequest: async () => {
       // TODO 米游社接口，不保证后续可用性
       const res = await fetch('https://bbs-api-static.miyoushe.com/misc/wapi/avatar_set')
-      const { data: { list = [] } = {} } = await res.json() as MiyousheAvatarResponse
+      const { data: { list: groups = [] } = {} } = await res.json() as MiyousheAvatarResponse
 
       // 仅使用原神系列头像
-      const avatars = (list.find(avatarGroup => avatarGroup.name === '原神')?.list ?? [])
+      const source = groups.find(({ name = '' }) => name === '原神')?.list ?? []
+
+      const avatars = source
         .sort(({ sort_order: sa = 0 }, { sort_order: sb = 0 }) => sa - sb)
         .map((avatar) => {
-          const name = avatar.name?.replace(/\d/g, '')
+          // 匹配所有汉字作为 name
+          const name = avatar.name?.match(/[\u4E00-\u9FA5]/g)?.join('') || '<社区头像>'
           return ({
             ...avatar,
-            name: name?.split('-')[0] || '<社区头像>',
+            name,
           })
         })
 
       return avatars
     },
-    initialValue: [],
   })
 
-  return { avatarList, ...rest }
+  onMounted(() => {
+    !cachedAvatars.value.length && refresh()
+  })
+
+  onSuccess((avatars) => {
+    cachedAvatars.value = avatars
+    window.clearTimeout(autoResetTimer.value)
+    autoResetTimer.value = window.setTimeout(refresh, 60 * 3600 * 1000)
+  })
+
+  return { avatarList: cachedAvatars, refresh, onSuccess, ...rest }
 }
