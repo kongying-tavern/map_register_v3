4e4abe17c922e2fb1b6d2373e186507e75515187
mikezhuang <2541413819@qq.com>
2024-02-04 14:50:29 +0800

    fix: 修复地图页设置无法保存

--- src/stores/userPreference.ts
+++ src/stores/userPreference.ts
@@ -21,19 +21,31 @@ export const usePreferenceStore = defineStore('global-user-preference', () => {
     },
   })
 
-  const updateUserPreference = async () => {
+  // 获取当前用户的 userId
+  const _getUserId = () => {
     const { auth } = useUserAuthStore()
     if (auth.userId === undefined || preference.value.id === auth.userId)
       return
-    await _updateUserPreference(auth.userId)
+    return auth.userId
+  }
+
+  const updateUserPreference = async () => {
+    const userId = _getUserId()
+    if (userId === undefined)
+      return
+    await _updateUserPreference(userId)
   }
 
   const syncUserPreference = async () => {
+    const userId = _getUserId()
+    if (userId === undefined)
+      return
+
     if (internalUpdateFlag.value) {
       internalUpdateFlag.value = false
       return
     }
-    await db.user.put(JSON.parse(JSON.stringify(preference.value)))
+    await db.user.put(JSON.parse(JSON.stringify({ id: userId, ...preference.value })))
   }
 
   watchDebounced(preference, syncUserPreference, {
