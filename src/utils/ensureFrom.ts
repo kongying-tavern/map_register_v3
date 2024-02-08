76c907beeb779aa54b48a03b34b85600e9fa20a1
夕云葛城 <kuroneko7520@outlook.com>
2024-02-09 03:32:33 +0800

    fix: 修复 ! 运算无法判断 number 0 的问题

--- src/utils/ensureFrom.ts
+++ src/utils/ensureFrom.ts
@@ -1,5 +1,5 @@
 export const ensureFrom = <T, K extends keyof T>(obj: T, key: K, setter: () => T[K]) => {
-  if (!obj[key])
+  if (obj[key] === undefined)
     obj[key] = setter()
   return obj[key]
 }
