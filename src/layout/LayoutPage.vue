5c1341c88efa88ad838d9c65f7bdf9fadd097dcc
夕云葛城 <kuroneko7520@outlook.com>
2024-02-13 23:07:05 +0800

    fix: 修改变量命名使其符合使用意义，添加注释

--- src/layout/LayoutPage.vue
+++ src/layout/LayoutPage.vue
@@ -7,12 +7,17 @@ const getRouteIndex = (routePath: string) => {
 
 const transitionName = ref<'route-slide-up' | 'route-slide-down'>('route-slide-down')
 
-const range = ref(30)
+/** 过渡滚动的距离 */
+const distance = ref(30)
 
+/**
+ * 动态路由过渡，根据 index 判断路由顺序，从下向上导航时使用下滚动过渡，反之同理
+ * @note 如果没有问题的话，route index 就是路由的渲染顺序
+ */
 onBeforeRouteUpdate((updateGuard, from, next) => {
   const startIndex = getRouteIndex(from.path)
   const endIndex = getRouteIndex(updateGuard.path)
-  range.value = 30 * Math.abs(startIndex - endIndex)
+  distance.value = 30 * Math.abs(startIndex - endIndex)
   transitionName.value = startIndex > endIndex ? 'route-slide-down' : 'route-slide-up'
   next(true)
 })
@@ -25,7 +30,7 @@ onBeforeRouteUpdate((updateGuard, from, next) => {
         v-bind="$attrs"
         class="h-full overflow-hidden transition-all duration-200"
       >
-        <transition :name="transitionName" mode="out-in" appear :style="{ '--range': range }">
+        <transition :name="transitionName" mode="out-in" appear :style="{ '--range': distance }">
           <keep-alive>
             <component :is="Component" :key="currentRoute.fullPath" />
           </keep-alive>
