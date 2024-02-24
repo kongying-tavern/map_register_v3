82f6977c515a86fcce9211d6db7a209a0a965f93
夕云葛城 <KuroNeko7520@outlook.com>
2024-02-25 02:10:09 +0800

    docs: 补充角色的等级说明

--- src/shared/roleTypeEnum.ts
+++ src/shared/roleTypeEnum.ts
@@ -1,16 +1,16 @@
 /** 角色类型标记 */
 export enum RoleTypeEnum {
-  /** 系统管理员 */
+  /** 系统管理员 level 5 */
   ADMIN = 'ADMIN',
-  /** 地图管理员 */
+  /** 地图管理员 level 4 */
   MAP_MANAGER = 'MAP_MANAGER',
-  /** 测试打点员 */
+  /** 测试打点员 level 3 */
   MAP_NEIGUI = 'MAP_NEIGUI',
-  /** 地图打点员 */
+  /** 地图打点员 level 2 */
   MAP_PUNCTUATE = 'MAP_PUNCTUATE',
-  /** 地图用户 */
+  /** 地图用户 level 1 */
   MAP_USER = 'MAP_USER',
-  /** 匿名用户 */
+  /** 匿名用户 level 0 */
   VISITOR = 'VISITOR',
 }
 
