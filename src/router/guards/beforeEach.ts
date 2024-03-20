095860dff6ccc2e408162421027bd3959db69c4d
夕云葛城 <KuroNeko7520@outlook.com>
2024-03-21 02:54:27 +0800

    fix: 无权限进入时首屏白屏的问题

--- src/router/guards/beforeEach.ts
+++ src/router/guards/beforeEach.ts
@@ -1,7 +1,6 @@
 import type { NavigationGuardWithThis, Router } from 'vue-router'
 import { useUserAuthStore, useUserInfoStore } from '@/stores'
 import type { Logger } from '@/utils'
-import { RoleLevel } from '@/shared'
 
 const isOfflineMode = import.meta.env.DEV && (import.meta.env.VITE_DEVELOPMENT_MODE === 'offline')
 
@@ -33,7 +32,7 @@ export const beforeEachGuard = (router: Router, logger: Logger): NavigationGuard
     /** 路由是否有权限限制 */
     const isLimited = Number(role !== undefined)
     /** 角色权限等级是否满足 */
-    const isAccess = Number(role !== undefined && userInfoStore.userRoleLevel >= RoleLevel[role])
+    const isAccess = Number(role !== undefined && userInfoStore.hasRole(role))
 
     const permission = Number.parseInt(`${isLogin}${isLimited}${isAccess}`, 2)
 
@@ -61,6 +60,8 @@ export const beforeEachGuard = (router: Router, logger: Logger): NavigationGuard
         redirectOnLogin ? next(redirectOnLogin) : next(true)
       },
       0b110: () => {
+        if (from.path === '/')
+          return next('/login')
         next(new Error('没有访问权限'))
       },
       0b111: () => {
