4c0fd02a14a827270445dbc9db828dca8874496a
Alex Fang <deeno@qq.com>
2024-03-20 10:18:55 +0800

    fix: 删除transition过度动画

--- src/layout/LayoutPage.vue
+++ src/layout/LayoutPage.vue
@@ -1,28 +1,3 @@
-<script setup lang="ts">
-const router = useRouter()
-
-const getRouteIndex = (routePath: string) => {
-  return router.getRoutes().findIndex(({ path }) => path === routePath)
-}
-
-const transitionName = ref<'route-slide-up' | 'route-slide-down'>('route-slide-down')
-
-/** 过渡滚动的距离 */
-const distance = ref(30)
-
-/**
- * 动态路由过渡，根据 index 判断路由顺序，从下向上导航时使用下滚动过渡，反之同理
- * @note 如果没有问题的话，route index 就是路由的渲染顺序
- */
-onBeforeRouteUpdate((updateGuard, from, next) => {
-  const startIndex = getRouteIndex(from.path)
-  const endIndex = getRouteIndex(updateGuard.path)
-  distance.value = 30 * Math.abs(startIndex - endIndex)
-  transitionName.value = startIndex > endIndex ? 'route-slide-down' : 'route-slide-up'
-  next(true)
-})
-</script>
-
 <template>
   <router-view>
     <template #default="{ Component, route: currentRoute }">
@@ -30,46 +5,10 @@ onBeforeRouteUpdate((updateGuard, from, next) => {
         v-bind="$attrs"
         class="h-full overflow-hidden transition-all duration-200"
       >
-        <transition :name="transitionName" mode="out-in" appear>
-          <keep-alive>
-            <component :is="Component" :key="currentRoute.fullPath" />
-          </keep-alive>
-        </transition>
+        <keep-alive>
+          <component :is="Component" :key="currentRoute.fullPath" />
+        </keep-alive>
       </div>
     </template>
   </router-view>
 </template>
-
-<style lang="scss">
-.route-slide-down {
-  &-enter-active, &-leave-active {
-    transition: all 0.1s ease-out;
-  }
-
-  &-enter-from {
-    opacity: 0;
-    transform: translateY(calc(v-bind(distance) * (-1px)));
-  }
-
-  &-leave-to {
-    opacity: 0;
-    transform: translateY(calc(v-bind(distance) * 1px));
-  }
-}
-
-.route-slide-up {
-  &-enter-active, &-leave-active {
-    transition: all 0.1s ease-out;
-  }
-
-  &-enter-from {
-    opacity: 0;
-    transform: translateY(calc(v-bind(distance) * 1px));
-  }
-
-  &-leave-to {
-    opacity: 0;
-    transform: translateY(calc(v-bind(distance) * (-1px)));
-  }
-}
-</style>
