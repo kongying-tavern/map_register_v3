1edc8d8ad18483fe676a80a61ef3a27140c66862
夕云葛城 <KuroNeko7520@outlook.com>
2024-03-24 04:59:09 +0800

    fix: 添加 ArrayBuffer 支持

--- src/utils/getDigest.ts
+++ src/utils/getDigest.ts
@@ -1,4 +1,4 @@
-export const getDigest = async (data: Blob, algorithm: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512' = 'SHA-1') => {
-  const digestBuffer = await crypto.subtle.digest(algorithm, await data.arrayBuffer())
+export const getDigest = async (data: Blob | ArrayBuffer, algorithm: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512' = 'SHA-1') => {
+  const digestBuffer = await crypto.subtle.digest(algorithm, data instanceof Blob ? await data.arrayBuffer() : data)
   return [...new Uint8Array(digestBuffer)].map(num => num.toString(16).padStart(2, '0')).join('')
 }
