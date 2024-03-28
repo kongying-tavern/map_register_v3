b6ddb9d14d47a3e41001b5ef4256593b6faec8e5
夕云葛城 <KuroNeko7520@outlook.com>
2024-03-28 22:54:31 +0800

    docs: 添加注释

--- src/stores/mapState/hooks/useMapCursor.ts
+++ src/stores/mapState/hooks/useMapCursor.ts
@@ -3,6 +3,7 @@ import type { CursorType } from '@/shared'
 export const useMapCursor = () => {
   const cursor = ref<CursorType>()
 
+  /** 设置覆盖光标样式 */
   const setCursor = (type?: CursorType) => {
     cursor.value = type
   }
