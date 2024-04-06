17ef72175ebf3499d988fb2bf467c4f556a616b9
夕云葛城 <KuroNeko7520@outlook.com>
2024-04-06 20:48:39 +0800

    feat: 批量操作相关类型

---
+++ src/pages/pageMapV2/components/MarkerMultiSelectController/types.d.ts
@@ -0,0 +1,26 @@
+export declare namespace Tweak {
+  enum Prop {
+    Title = 'title',
+    Content = 'content',
+    RefreshTime = 'refreshTime',
+    HiddenFlag = 'hiddenFlag',
+    Extra = 'extra',
+    ItemList = 'itemList',
+  }
+
+  type Type =
+  | 'update'
+  | 'replace'
+  | 'replaceRegex'
+  | 'prepend'
+  | 'append'
+  | 'trimLeft'
+  | 'trimRight'
+  | 'removeLeft'
+  | 'removeRight'
+  | 'merge'
+  | 'insertIfAbsent'
+  | 'insertOrUpdate'
+  | 'removeItem'
+  | 'preserveItem'
+}
