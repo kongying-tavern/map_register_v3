be491fa2b60ce3d136bc8ee29eac4fa1fec8bf02
夕云葛城 <KuroNeko7520@outlook.com>
2024-04-10 01:32:36 +0800

    feat: 批量修改器的包装组件

---
+++ src/pages/pageMapV2/components/MarkerMultiSelectController/components/ModifierPreview.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { Modifier } from '../core'
+import type { GSMapState } from '@/stores/types/genshin-map-state'
+
+defineProps<{
+  modifier: Modifier
+  meta: API.TweakConfigMetaVo
+  data: GSMapState.MarkerWithRenderConfig
+}>()
+</script>
+
+<template>
+  <Suspense>
+    <component
+      :is="modifier.previewer"
+      :value="modifier.getValue(data)"
+    />
+
+    <template #fallback>
+      <div>加载中...</div>
+    </template>
+  </Suspense>
+</template>
