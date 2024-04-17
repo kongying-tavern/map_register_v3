05eba784e74663ed29917cdfcf2d3df32ef7140c
夕云葛城 <KuroNeko7520@outlook.com>
2024-04-17 14:57:08 +0800

    style: 文本修改预览组件样式优化

--- src/pages/pageMapV2/components/MarkerMultiSelectController/core/PreviewImpls/CommonText.vue
+++ src/pages/pageMapV2/components/MarkerMultiSelectController/core/PreviewImpls/CommonText.vue
@@ -5,7 +5,21 @@ defineProps<{
 </script>
 
 <template>
-  <div class="overflow-hidden whitespace-nowrap text-ellipsis" :title="value">
-    <pre class="text-xs font-mono">{{ value }}</pre>
+  <div class="common-text h-full overflow-hidden text-xs py-1" :title="value">
+    <div class="text-wrapper">
+      {{ value }}
+    </div>
   </div>
 </template>
+
+<style scoped>
+.text-wrapper {
+  text-align: left;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  white-space: normal;
+  overflow: hidden;
+}
+</style>
