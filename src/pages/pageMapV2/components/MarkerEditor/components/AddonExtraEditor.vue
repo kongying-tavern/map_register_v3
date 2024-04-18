9066a9af98fb219da04740c4a3f79adcd0bebe60
夕云葛城 <KuroNeko7520@outlook.com>
2024-04-19 03:14:10 +0800

    fix: 修复层级字段存在值但未显示对应名称的问题

--- src/pages/pageMapV2/components/MarkerEditor/components/AddonExtraEditor.vue
+++ src/pages/pageMapV2/components/MarkerEditor/components/AddonExtraEditor.vue
@@ -65,6 +65,30 @@ const island28Options = computed(() => {
   }) as CascaderOption[]
 })
 
+// ==================== 地下字段特殊处理 ====================
+
+/** 由于原始数据只存储末级层级的值，这里需要查找父级层级以便显示 */
+const regionLevels = computed({
+  get: () => {
+    const childValue = props.modelValue?.underground?.region_levels?.[0]
+    const { levels = [] } = props.extraConfig.underground ?? {}
+    if (!childValue || !levels.length)
+      return []
+    const parentValue = levels.find(parent => parent.children.find(child => child.value === childValue) !== undefined)
+    if (!parentValue)
+      return []
+    return [parentValue.value, childValue]
+  },
+  set: (values) => {
+    if (values.length < 2) {
+      extraForm.value.underground.region_levels = []
+      return true
+    }
+    const [_, childValue] = values
+    extraForm.value.underground.region_levels = [childValue]
+  },
+})
+
 // ==================== 数据下行 ====================
 const isInternalUpdate = ref(false)
 watch(() => props.modelValue, (externalValue) => {
@@ -90,22 +114,6 @@ watch(() => props.extraConfig, () => {
 
 <template>
   <div class="w-full flex flex-col gap-2">
-    <div v-if="extraConfig.underground" class="flex gap-2">
-      <el-switch
-        v-model="extraForm.underground.is_underground"
-        :active-text="extraConfig.underground.textActive ?? '地下'"
-        :inactive-text="extraConfig.underground.textInactive ?? '地上'"
-        inline-prompt
-      />
-      <el-cascader
-        v-if="extraConfig.underground.useDetail && extraForm.underground.is_underground"
-        v-model="extraForm.underground.region_levels"
-        :options="extraConfig.underground.levels"
-        :show-all-levels="false"
-        class="flex-1"
-      />
-    </div>
-
     <div v-if="extraConfig['1_6_island']" class="flex gap-2">
       <el-cascader
         v-model="extraForm['1_6_island']"
@@ -115,7 +123,7 @@ watch(() => props.extraConfig, () => {
       />
     </div>
 
-    <div v-if="extraConfig['2_8_island']" class="flex gap-2">
+    <div v-else-if="extraConfig['2_8_island']" class="flex gap-2">
       <el-cascader
         v-model="island28BindValues"
         :options="island28Options"
@@ -126,6 +134,21 @@ watch(() => props.extraConfig, () => {
         collapse-tags
         collapse-tags-tooltip
         clearable
+        class="flex-1"
+      />
+    </div>
+
+    <div v-else-if="extraConfig.underground" class="flex gap-2">
+      <el-switch
+        v-model="extraForm.underground.is_underground"
+        :active-text="extraConfig.underground.textActive ?? '地下'"
+        :inactive-text="extraConfig.underground.textInactive ?? '地上'"
+        inline-prompt
+      />
+      <el-cascader
+        v-if="extraConfig.underground.useDetail && extraForm.underground.is_underground"
+        v-model="regionLevels"
+        :options="extraConfig.underground.levels"
         class="flex-1"
       />
     </div>
