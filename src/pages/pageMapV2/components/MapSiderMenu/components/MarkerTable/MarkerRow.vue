23e4e73ede7e4f979d7b30dcea442c5e0c5dccb5
夕云葛城 <KuroNeko7520@outlook.com>
2024-05-01 03:42:40 +0800

    refactor: 重构点位列表（未完成）

---
+++ src/pages/pageMapV2/components/MapSiderMenu/components/MarkerTable/MarkerRow.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  data: API.MarkerVo
+}>()
+</script>
+
+<template>
+  <div class="marker-table-item">
+    {{ props.data.markerTitle }}
+  </div>
+</template>
+
+<style scoped>
+.marker-table-item {
+  height: 100%;
+  overflow: hidden;
+
+  display: flex;
+  align-items: center;
+  background: #F4EFE280;
+  clip-path: inset(0 4px 4px 4px round 0px);
+  margin-right: 8px;
+
+  padding: 0 12px 4px 12px;
+}
+</style>
--- src/pages/pageMapV2/components/MapSiderMenu/components/MarkerTable/MarkerTable.vue
+++ src/pages/pageMapV2/components/MapSiderMenu/components/MarkerTable/MarkerTable.vue
@@ -1,77 +1,35 @@
 <script lang="tsx" setup>
-import type { Column } from 'element-plus'
-import { MarkerButton } from '.'
+import MarkerRow from './MarkerRow.vue'
 import { useMapStateStore } from '@/stores'
-
-const tableContainerRef = ref<HTMLElement | null>()
-const { width, height } = useElementSize(tableContainerRef)
+import { AppVirtualTable } from '@/components'
 
 const mapStateStore = useMapStateStore()
-
-const columns = ref<Column[]>([
-  { title: 'ID', key: 'id', dataKey: 'id', width: 60, align: 'right' },
-  { title: '点位名称', key: 'markerTitle', width: 110, cellRenderer: ({ rowData: data }) => <MarkerButton data={data} /> },
-  { title: '点位说明', key: 'content', dataKey: 'content', width: 200 },
-])
 </script>
 
 <template>
-  <div ref="tableContainerRef" class="marker-filter h-full">
-    <el-table-v2
-      :columns="columns"
+  <div class="marker-filter h-full">
+    <AppVirtualTable
+      class="pt-1"
+      always-scrollbar
       :data="mapStateStore.currentLayerMarkers"
-      :cache="15"
-      :width="width"
-      :height="height"
-      :estimated-row-height="40"
-      expand-column-key="content"
-      class="marker-table"
-      header-class="marker-table-header"
-      fixed
-      :h-scrollbar-size="0"
+      :item-height="40"
+      :cached-rows="2"
+      :scrollbar-style="{
+        '--el-scrollbar-bg-color': '#D3BC8E',
+        '--el-scrollbar-opacity': '1',
+        '--el-scrollbar-hover-bg-color': '#D3BC8E',
+        '--el-scrollbar-hover-opacity': '0.5',
+      }"
     >
-      <template #empty>
-        <div class="flex items-center justify-center h-100%">
-          <el-empty image="/icons/qiliangliang.png" :image-size="300" />
-        </div>
+      <template #default="{ item }">
+        <MarkerRow :data="item" />
       </template>
-    </el-table-v2>
+    </AppVirtualTable>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .marker-filter {
-  width: 350px;
-}
-
-.marker-table {
-  --el-bg-color: transparent;
-  --el-table-bg-color: transparent;
-  --el-table-border-color: transparent;
-  --el-table-row-hover-bg-color: #ffffff20;
-
-  color: #E4DDD1;
-  font-size: 12px;
-
-  :deep(.el-table-v2__row-depth-0) {
-    height: 40px;
-  }
-
-  :deep(.el-table-v2__header-row) {
-    background: #00000020;
-  }
-
-  :deep(.el-table-v2__cell-text) {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-
-  :deep(.expand-content) {
-    background: #141b22;
-    .min-height-text {
-      min-height: 1.5em;
-    }
-  }
+  width: 400px;
 }
 </style>
