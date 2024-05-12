9d9616b4961b9a13095b3504660e21c815f26044
夕云葛城 <KuroNeko7520@outlook.com>
2024-05-12 18:20:24 +0800

    feat: role map

--- src/pages/pageUserManager/hooks/useRoleList.ts
+++ src/pages/pageUserManager/hooks/useRoleList.ts
@@ -4,6 +4,11 @@ import Api from '@/api/api'
 export const useRoleList = () => {
   const roleList = ref<API.SysRoleVo[]>([])
 
+  const roleMap = computed(() => roleList.value.reduce((map, role) => {
+    map.set(role.id!, role)
+    return map
+  }, new Map<number, API.SysRoleVo>()))
+
   const { onSuccess } = useFetchHook({
     immediate: true,
     onRequest: () => Api.role.listRole(),
@@ -13,5 +18,5 @@ export const useRoleList = () => {
     roleList.value = data.sort(({ sort: sortA = 0 }: API.SysRoleVo, { sort: sortB = 0 }: API.SysRoleVo) => sortA - sortB)
   })
 
-  return { roleList }
+  return { roleList, roleMap }
 }
