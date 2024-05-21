a0d919b8ffc91b4ab44e5b0cbbe77b57f4eb315d
夕云葛城 <KuroNeko7520@outlook.com>
2024-05-21 19:41:20 +0800

    fix: 取消隐式大图上传

--- src/pages/pageMapV2/components/MarkerEditor/hooks/useMarkerPicture.ts
+++ src/pages/pageMapV2/components/MarkerEditor/hooks/useMarkerPicture.ts
@@ -1,9 +1,6 @@
 import { useFetchHook } from '@/hooks'
 
 export const useMarkerPicture = (picture: Ref<string | undefined>) => {
-  const largeImage = shallowRef<Blob>()
-  const largeImageUrl = useObjectUrl(largeImage)
-
   const thumbImage = shallowRef<Blob>()
   const thumbImageUrl = useObjectUrl(thumbImage)
 
@@ -15,7 +12,7 @@ export const useMarkerPicture = (picture: Ref<string | undefined>) => {
       if (!pictureUrl || pictureUrl.toLowerCase().startsWith('blob:'))
         return
 
-      const { protocol, origin, pathname } = new URL(pictureUrl)
+      const { protocol, pathname } = new URL(pictureUrl)
 
       if (!['http:', 'https:'].includes(protocol))
         throw new Error(`协议 "${protocol}" 不支持`)
@@ -26,38 +23,22 @@ export const useMarkerPicture = (picture: Ref<string | undefined>) => {
       if (!nameunits || nameunits.length < 2)
         throw new Error(`路径 "${pathname}" 未包含可解析的文件名`)
 
-      // 提取拓展名
-      const extname = nameunits.at(-1)!
-
-      // 提取文件名（不含拓展名）
-      const filename = nameunits.slice(0, -1).join('.')
-
-      // 合成大图地址
-      const largeUrl = `${origin}${paths.slice(0, -1).join('/')}/${filename}_large.${extname}`
-
       // 加载缩略图
       const thumb = await fetch(pictureUrl, { mode: 'cors' }).then(r => r.blob()).catch(() => undefined)
 
-      // 加载大图
-      const large = await fetch(largeUrl, { mode: 'cors' }).then(r => r.blob()).catch(() => thumb)
-
-      return { large, thumb }
+      return thumb
     },
   })
 
-  onInitSuccess((data) => {
-    if (!data)
-      return
-    const { large, thumb } = data
-    largeImage.value = large
+  onInitSuccess((thumb) => {
     thumbImage.value = thumb
   })
 
   watch(picture, initPicture, { immediate: true })
 
   return {
+    initPicture,
     initLoading,
-    largeImageUrl,
     thumbImageUrl,
   }
 }
