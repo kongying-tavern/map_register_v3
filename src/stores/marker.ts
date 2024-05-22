958289f170e1fcaffd3498d8c01ee6f08fac88ad
夕云葛城 <KuroNeko7520@outlook.com>
2024-05-23 02:17:00 +0800

    feat: 为 ws 事件通知添加更多信息

--- src/stores/marker.ts
+++ src/stores/marker.ts
@@ -1,15 +1,17 @@
 import { defineStore } from 'pinia'
-import { ElNotification } from 'element-plus'
+import { AddLocation, DeleteLocation, Location } from '@element-plus/icons-vue'
 import { liveQuery } from 'dexie'
 import { useBackendUpdate, userHook } from './hooks'
 import { useAccessStore, useSocketStore, useUserAuthStore } from '.'
 import { Zip } from '@/utils'
 import Api from '@/api/api'
 import db from '@/database'
+import { HistoryRecordType } from '@/shared'
 
 /** 全量点位的全局数据 */
 export const useMarkerStore = defineStore('global-marker', () => {
   const accessStore = useAccessStore()
+  const socketStore = useSocketStore()
 
   const _markerList = shallowRef<API.MarkerVo[]>([])
 
@@ -33,58 +35,90 @@ export const useMarkerStore = defineStore('global-marker', () => {
     _markerList.value = list
   })
 
-  const socketStore = useSocketStore()
-
   // 点位压缩数据更新
-  socketStore.event.on('MarkerBinaryPurged', async () => {
-    await backendUpdater.refresh()
-    ElNotification.info({
-      message: `点位数据库更新`,
-      position: 'bottom-right',
-    })
-  })
+  socketStore.event.on('MarkerBinaryPurged', backendUpdater.refresh)
 
   // 单个点位更新
   socketStore.event.on('MarkerUpdated', async (id) => {
-    const { data: [markerInfo] = [] } = await Api.marker.listMarkerById([id])
+    const { data: [markerInfo] = [], users = {} } = await Api.marker.listMarkerById([id])
     if (!markerInfo)
       return
+
     await db.marker.put(markerInfo)
-    ElNotification.info({
-      message: `点位 ${id} 更新`,
-      position: 'bottom-right',
+
+    const { markerTitle, creatorId } = markerInfo
+    const { username = `(uid: ${creatorId})`, nickname = username } = users[creatorId!] ?? {}
+
+    socketStore.notice('MarkerUpdated', {
+      message: `${nickname} 更新了点位 ${markerTitle} (id:${id})`,
+      icon: Location,
+      customClass: 'text-[var(--el-color-primary)]',
+      offset: 48,
     })
   })
 
   // 单个点位新增
   socketStore.event.on('MarkerAdded', async (id) => {
-    const { data: [markerInfo] = [] } = await Api.marker.listMarkerById([id])
+    const { data: [markerInfo] = [], users = {} } = await Api.marker.listMarkerById([id])
     if (!markerInfo)
       return
+
     await db.marker.put(markerInfo)
-    ElNotification.info({
-      message: `新增点位 ${id}`,
-      position: 'bottom-right',
+
+    const { markerTitle, creatorId } = markerInfo
+    const { username = `(uid: ${creatorId})`, nickname = username } = users[creatorId!] ?? {}
+
+    socketStore.notice('MarkerAdded', {
+      message: `${nickname} 新增了点位 ${markerTitle} (id:${id})`,
+      icon: AddLocation,
+      customClass: 'text-[var(--el-color-success)]',
+      offset: 48,
     })
   })
 
   // 单个点位删除
   socketStore.event.on('MarkerDeleted', async (id) => {
     await db.marker.delete(id)
-    ElNotification.info({
-      message: `删除点位 ${id}`,
-      position: 'bottom-right',
+
+    const { data: { record: [history] = [] } = {}, users = {} } = await Api.history.getList1({
+      current: 0,
+      id: [id],
+      size: 1,
+      type: HistoryRecordType.MARKER,
+      sort: ['updateTime-'],
+    })
+
+    if (!history)
+      return
+
+    const { markerTitle, creatorId } = JSON.parse(history.content ?? '{}') as API.MarkerVo
+    const { username = `(uid: ${creatorId})`, nickname = username } = users[history.creatorId!] ?? {}
+
+    socketStore.notice('MarkerDeleted', {
+      message: `${nickname} 删除了点位 ${markerTitle} (id:${id})`,
+      icon: DeleteLocation,
+      customClass: 'text-[var(--el-color-danger)]',
+      offset: 48,
     })
   })
 
   // 点位批量更新
   socketStore.event.on('MarkerTweaked', async (ids) => {
-    const { data = [] } = await Api.marker.listMarkerById(ids)
+    const { data = [], users = {} } = await Api.marker.listMarkerById(ids)
+    if (!data.length)
+      return
+
     await db.marker.bulkPut(data)
-    ElNotification.info({
-      title: '点位批量更新',
-      message: `点位 ${ids.join(', ')} 更新`,
-      position: 'bottom-right',
+
+    const [{ updaterId }] = data
+
+    const { username = `(uid: ${updaterId})`, nickname = username } = users[updaterId!] ?? {}
+
+    socketStore.notice('MarkerTweaked', {
+      message: `${nickname} 批量更新了点位: ${ids.join(', ')}`,
+      icon: Location,
+      customClass: 'text-[var(--el-color-success)]',
+      offset: 48,
     })
   })
 
