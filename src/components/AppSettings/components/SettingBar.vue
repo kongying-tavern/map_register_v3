0a0bbd73784a07a3721cfa02a4e496830664143a
夕云葛城 <KuroNeko7520@outlook.com>
2024-05-23 02:11:28 +0800

    feat: 优化设置组标题样式，优化detail下拉过渡动画，添加控制下拉可用性的属性

--- src/components/AppSettings/components/SettingBar.vue
+++ src/components/AppSettings/components/SettingBar.vue
@@ -6,6 +6,7 @@ const props = defineProps<{
   note?: string
   icon?: Component
   open?: boolean
+  detailDisabled?: boolean
 }>()
 
 const slots = defineSlots<{
@@ -15,9 +16,16 @@ const slots = defineSlots<{
   note: unknown
 }>()
 
-const collapse = ref(!props.open)
+const collapse = ref(props.detailDisabled ? false : !props.open)
+
+watch(() => props.detailDisabled, (disabled) => {
+  if (!disabled)
+    return
+  collapse.value = true
+})
+
 const toggleCollapse = () => {
-  if (!slots.detail)
+  if (props.detailDisabled || !slots.detail)
     return
   collapse.value = !collapse.value
 }
@@ -37,7 +45,7 @@ const toggleCollapse = () => {
     <slot name="default">
       <div
         class="min-h-[66px] flex flex-shrink-0 items-center px-4 py-3 transition-[background-color]"
-        :class="$slots.detail ? 'hover:bg-[var(--el-fill-color)] active:bg-[var(--el-fill-color-darker)]' : ''"
+        :class="(!detailDisabled && $slots.detail) ? 'hover:bg-[var(--el-fill-color)] active:bg-[var(--el-fill-color-darker)]' : ''"
       >
         <div class="flex-1 flex items-center">
           <div v-if="icon" class="row-span-2 grid place-items-center pr-4">
@@ -58,10 +66,12 @@ const toggleCollapse = () => {
           </div>
         </div>
 
-        <slot name="setting" />
+        <div @click.stop="">
+          <slot name="setting" />
+        </div>
 
         <div v-if="$slots.detail" class="pl-4 flex items-center">
-          <el-icon :size="14">
+          <el-icon :size="14" :color="detailDisabled ? 'var(--el-text-color-disabled)' : ''">
             <ArrowDown v-if="collapse" />
             <ArrowUp v-else />
           </el-icon>
@@ -71,7 +81,7 @@ const toggleCollapse = () => {
 
     <div
       v-if="$slots.detail"
-      class="flex flex-col transition-all ease-linear"
+      class="flex flex-col transition-all ease-in-out"
       :class="collapse ? 'max-h-0' : 'max-h-[1000px]'"
       @click.stop=""
     >
--- src/components/AppSettings/components/SettingGroup.vue
+++ src/components/AppSettings/components/SettingGroup.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div class="mb-4">
-    <div class="pb-1">
+    <div class="pb-1 font-bold">
       {{ name }}
     </div>
     <div class="flex flex-col gap-1">
