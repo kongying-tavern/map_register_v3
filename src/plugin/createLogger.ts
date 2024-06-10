2e7d9d6df9fb30cb437c8fb9ed0752d65d0cbb7a
夕云葛城 <KuroNeko7520@outlook.com>
2024-06-11 03:20:54 +0800

    refactor: createLogger 插件的日志打印职责转交给 Logger 本体

--- src/plugin/createLogger.ts
+++ src/plugin/createLogger.ts
@@ -1,13 +1,9 @@
 import type { Plugin } from 'vue'
 import { useDevStore } from '@/stores'
-import { Logger } from '@/utils'
 
 /** 用于在主线程打印并记录日志的插件 */
 export const createLogger = (): Plugin => ({
   install: () => {
-    const devStore = useDevStore()
-    Logger.event.on('log', (log) => {
-      devStore.logList.push(log)
-    })
+    useDevStore().initLogger()
   },
 })
