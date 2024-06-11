c67af5a877ac87142d31f90853ffd01ed10f3635
夕云葛城 <KuroNeko7520@outlook.com>
2024-06-12 03:44:32 +0800

    feat(workerProxy): 实现 worker 代理操作中间件

--- src/database/middleware/workerProxy.ts
+++ src/database/middleware/workerProxy.ts
@@ -1,4 +1,91 @@
-import type { DBCore, Middleware } from 'dexie'
+import type {
+  DBCore,
+  DBCoreAddRequest,
+  DBCoreMutateRequest,
+  DBCoreMutateResponse,
+  DBCorePutRequest,
+  DBCoreTable,
+  Middleware,
+} from 'dexie'
+import { createWorkerHelper } from '@/utils'
+import Worker from '@/worker/db.worker?worker'
+import type { WorkerInput, WorkerOutput } from '@/worker/db.worker'
+
+const send = createWorkerHelper<WorkerInput, WorkerOutput>(new Worker({ name: '数据库代理线程' }))
+
+const collectTransferable = (value: unknown, transferable: Transferable[] = []) => {
+  if (!value || typeof value !== 'object')
+    return
+  if (value instanceof ArrayBuffer)
+    return transferable.push(value)
+  if (ArrayBuffer.isView(value))
+    return transferable.push(value.buffer)
+  if (Array.isArray(value))
+    return value.forEach(item => collectTransferable(item, transferable))
+}
+
+const handlerAdd = async (req: DBCoreAddRequest, table: DBCoreTable): Promise<DBCoreMutateResponse> => {
+  const defaultRes = {
+    failures: {},
+    lastResult: undefined,
+    numFailures: 0,
+    results: [],
+  }
+
+  if (!req.values.length)
+    return defaultRes
+
+  return table.mutate(req)
+}
+
+const handlerPut = async (req: DBCorePutRequest, table: DBCoreTable): Promise<DBCoreMutateResponse> => {
+  const defaultRes = {
+    failures: {},
+    lastResult: undefined,
+    numFailures: 0,
+    results: [],
+  }
+
+  if (!req.values.length)
+    return defaultRes
+
+  const transferable: Transferable[] = []
+  collectTransferable(req.values, transferable)
+
+  const res = await send({
+    tableName: table.name,
+    type: 'put',
+    values: req.values,
+  }, transferable)
+
+  if (typeof res === 'string') {
+    const error = new Error(res)
+    defaultRes.failures = req.values.map(() => error)
+    defaultRes.numFailures = req.values.length
+  }
+
+  return defaultRes
+}
+
+/**
+ * @todo 转发到 worker 进行处理
+ */
+const workerMutate = (req: DBCoreMutateRequest, table: DBCoreTable): Promise<DBCoreMutateResponse> => {
+  // 目前性能瓶颈主要是添加和修改，暂且只代理这两种操作
+  switch (req.type) {
+    case 'add':
+      return handlerAdd(req, table)
+    case 'put':
+      return handlerPut(req, table)
+    default:
+      return Promise.resolve<DBCoreMutateResponse>({
+        failures: {},
+        lastResult: undefined,
+        numFailures: 0,
+        results: [],
+      })
+  }
+}
 
 /**
  * 数据库操作交由 worker 代理进行
@@ -6,19 +93,13 @@ import type { DBCore, Middleware } from 'dexie'
 export const workerProxy: Middleware<DBCore> = {
   stack: 'dbcore',
   name: 'worker-proxy',
-  create: downlevelDB => ({
-    ...downlevelDB,
-    transaction: (stores, mode, options) => {
-      return downlevelDB.transaction(stores, mode, options)
-    },
+  create: core => ({
+    ...core,
     table: (tableName) => {
-      const downlevelTable = downlevelDB.table(tableName)
+      const downlevelTable = core.table(tableName)
       return {
         ...downlevelTable,
-        mutate: (req) => {
-          // TODO 转发到 worker 进行处理
-          return downlevelTable.mutate(req)
-        },
+        mutate: req => workerMutate(req, downlevelTable),
       }
     },
   }),
---
+++ src/worker/db.worker.ts
@@ -0,0 +1,67 @@
+import type {
+  DBCoreAddRequest,
+  DBCoreMutateResponse,
+  DBCorePutRequest,
+} from 'dexie'
+import { WorkerThreadDB } from '@/database/db/worker'
+import { useLoggerWorker } from '@/hooks/useWorkerLogger'
+import type { Logger } from '@/utils/logger'
+
+declare const globalThis: DedicatedWorkerGlobalScope
+
+type MutateRequestOptions<T> = {
+  tableName: string
+} & T
+
+export type WorkerInput = MutateRequestOptions<
+  | Pick<DBCoreAddRequest, 'type' | 'values'>
+  | Pick<DBCorePutRequest, 'type' | 'values'>
+>
+
+export type WorkerSuccessOutput = DBCoreMutateResponse
+
+/** 主线程接收数据 */
+export type WorkerOutput =
+  | string // 错误原因
+  | WorkerSuccessOutput
+
+const db = new WorkerThreadDB()
+
+Reflect.set(globalThis, 'db', db)
+
+const mutate = async (ev: MessageEvent<WorkerInput>, logger: Logger): Promise<DBCoreMutateResponse> => {
+  const { data } = ev
+
+  logger.info(`正在代理表 ${data.tableName} 的 ${data.type} 操作, 数据长度: ${data.values.length}`)
+
+  switch (data.type) {
+    case 'add':
+      await db.table(data.tableName).bulkAdd(data.values)
+      break
+    case 'put':
+      await db.table(data.tableName).bulkPut(data.values)
+      break
+    default:
+      throw new Error(`无法处理的操作类型`)
+  }
+
+  return {
+    failures: [],
+    lastResult: undefined,
+    numFailures: 0,
+    results: [],
+  }
+}
+
+globalThis.addEventListener('message', async (ev: MessageEvent<WorkerInput>) => {
+  const { send, logger } = useLoggerWorker<WorkerInput, WorkerOutput>(ev, '数据库代理')
+
+  try {
+    const res = await mutate(ev, logger)
+    send(res)
+  }
+  catch (err) {
+    logger.error(err instanceof Error ? err.message : `${err}`)
+    send(err instanceof Error ? err.message : `${err}`)
+  }
+})
