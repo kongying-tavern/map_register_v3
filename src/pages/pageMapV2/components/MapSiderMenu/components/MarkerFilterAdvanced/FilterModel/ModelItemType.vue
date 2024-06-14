ec39a385e4aed7ede65b993234a7d44b7d088b0f
Alex Fang <deeno@qq.com>
2024-06-14 13:05:01 +0800

    feat: 添加高级过滤器-类别选择器

--- src/pages/pageMapV2/components/MapSiderMenu/components/MarkerFilterAdvanced/FilterModel/ModelItemType.vue
+++ src/pages/pageMapV2/components/MapSiderMenu/components/MarkerFilterAdvanced/FilterModel/ModelItemType.vue
@@ -1,18 +1,47 @@
 <script lang="ts" setup>
-import type { MAFOptionDummy, MAFValueDummy } from '@/stores/types'
+import { computed } from 'vue'
+import { storeToRefs } from 'pinia'
+import { MarkerFilterButton } from '../../MarkerFilterComponent'
+import { SelectBase } from '.'
+import type { MAFOptionSelect, MAFValueNumberArray } from '@/stores/types'
+import { IconApplication } from '@/components/AppIcons'
+import { useItemTypeStore } from '@/stores'
 
 defineProps<{
-  options: MAFOptionDummy
+  options: MAFOptionSelect<API.ItemTypeVo>
 }>()
 
-defineModel<MAFValueDummy>('modelValue', {
+const modelValue = defineModel<MAFValueNumberArray>('modelValue', {
   required: false,
-  default: {},
+  default: {
+    na: [],
+  },
 })
+
+const { itemTypeMap } = storeToRefs(useItemTypeStore())
+
+const typeTag = computed(() => modelValue.value.na
+  .map(typeId => itemTypeMap.value[typeId]?.name)
+  .filter(v => v)
+  .join(','),
+)
 </script>
 
 <template>
   <div class="flex-auto flex gap-1 items-center">
     <span class="flex-none">类别</span>
+    <SelectBase
+      v-model="modelValue.na"
+      :options="options"
+    >
+      <MarkerFilterButton theme="dark">
+        <template #icon>
+          <IconApplication />
+        </template>
+        <template v-if="typeTag" #default>
+          {{ typeTag }}
+        </template>
+      </MarkerFilterButton>
+    </SelectBase>
   </div>
 </template>
--- src/stores/mapState/hooks/useMarkerAdvancedFilter/modelItemType.ts
+++ src/stores/mapState/hooks/useMarkerAdvancedFilter/modelItemType.ts
@@ -1,28 +1,42 @@
+import { useItemTypeStore } from '@/stores'
 import type {
   MAFConfig,
   MAFMetaDummy,
-  MAFOptionDummy,
-  MAFValueDummy,
+  MAFOptionSelect,
+  MAFValueNumberArray,
 } from '@/stores/types'
 
 export class ItemType implements MAFConfig {
   id = 102
   name = '分类'
-  option: MAFOptionDummy = {}
+  option: ComputedRef<MAFOptionSelect<API.ItemTypeVo>> = computed(() => {
+    const { itemTypeList } = useItemTypeStore()
 
-  get defaultVal(): MAFValueDummy {
+    return {
+      dialogTitle: '选择分类',
+      dialogListClass: 'grid grid-cols-2',
+      options: itemTypeList,
+      optionLabel: 'name',
+      optionValue: 'id',
+      optionSelectMultiple: true,
+    }
+  })
+
+  get defaultVal(): MAFValueNumberArray {
+    return {
+      na: [],
+    }
+  }
+
+  prepare(_val: MAFValueNumberArray): MAFMetaDummy {
     return {}
   }
 
-  prepare(_val: MAFValueDummy): MAFMetaDummy {
-    return {}
-  }
-
-  semantic(_val: MAFValueDummy, _opt: MAFOptionDummy, _meta: MAFMetaDummy, _opposite: boolean): string {
+  semantic(_val: MAFValueNumberArray, _opt: MAFOptionSelect<API.ItemTypeVo>, _meta: MAFMetaDummy, _opposite: boolean): string {
     return ''
   }
 
-  filter(_val: MAFValueDummy, _opt: MAFOptionDummy, _meta: MAFMetaDummy, _marker: API.MarkerVo): boolean {
+  filter(_val: MAFValueNumberArray, _opt: MAFOptionSelect<API.ItemTypeVo>, _meta: MAFMetaDummy, _marker: API.MarkerVo): boolean {
     return false
   }
 }
