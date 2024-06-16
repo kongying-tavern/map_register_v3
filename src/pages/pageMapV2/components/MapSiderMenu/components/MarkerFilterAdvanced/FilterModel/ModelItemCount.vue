232b97492a6abdb30ec946ba24c3845817a906f7
Alex Fang <deeno@qq.com>
2024-06-16 15:26:58 +0800

    feat: 新增高级过滤-物品计数模型

--- src/pages/pageMapV2/components/MapSiderMenu/components/MarkerFilterAdvanced/FilterModel/ModelItemCount.vue
+++ src/pages/pageMapV2/components/MapSiderMenu/components/MarkerFilterAdvanced/FilterModel/ModelItemCount.vue
@@ -1,18 +1,42 @@
 <script lang="ts" setup>
-import type { MAFOptionDummy, MAFValueDummy } from '@/stores/types'
+import { MarkerFilterButton } from '../../MarkerFilterComponent'
+import { NumberRangeBase } from '.'
+import type { MAFOptionRange, MAFOptionSwitch, MAFValueBoolean, MAFValueNumberRange } from '@/stores/types'
 
 defineProps<{
-  options: MAFOptionDummy
+  options: MAFOptionSwitch & MAFOptionRange
 }>()
 
-defineModel<MAFValueDummy>('modelValue', {
+const modelValue = defineModel<MAFValueBoolean & MAFValueNumberRange>('modelValue', {
   required: false,
-  default: {},
+  default: {
+    b: false,
+    nMin: null,
+    nMax: null,
+  },
 })
+
+const toggleMatchType = () => {
+  modelValue.value.b = !modelValue.value.b
+}
 </script>
 
 <template>
   <div class="flex-auto flex gap-1 items-center">
+    <MarkerFilterButton
+      theme="light"
+      @click="toggleMatchType"
+    >
+      <template #default>
+        {{ modelValue.b ? options.textActive : options.textInactive }}
+      </template>
+    </MarkerFilterButton>
     <span class="flex-none">物品计数</span>
+    <NumberRangeBase
+      v-model:min="modelValue.nMin"
+      v-model:max="modelValue.nMax"
+      class="flex-auto"
+      :options="options"
+    />
   </div>
 </template>
--- src/stores/mapState/hooks/useMarkerAdvancedFilter/modelItemCount.ts
+++ src/stores/mapState/hooks/useMarkerAdvancedFilter/modelItemCount.ts
@@ -1,28 +1,39 @@
 import type {
   MAFConfig,
   MAFMetaDummy,
-  MAFOptionDummy,
-  MAFValueDummy,
+  MAFOptionRange,
+  MAFOptionSwitch,
+  MAFValueBoolean,
+  MAFValueNumberRange,
 } from '@/stores/types'
 
 export class ItemCount implements MAFConfig {
   id = 106
   name = '物品计数'
-  option: MAFOptionDummy = {}
+  option: MAFOptionSwitch & MAFOptionRange = {
+    textActive: '全部',
+    textInactive: '任意',
+    placeholderMin: '不限',
+    placeholderMax: '不限',
+  }
 
-  get defaultVal(): MAFValueDummy {
+  get defaultVal(): MAFValueBoolean & MAFValueNumberRange {
+    return {
+      b: false,
+      nMin: null,
+      nMax: null,
+    }
+  }
+
+  prepare(_val: MAFValueBoolean & MAFValueNumberRange): MAFMetaDummy {
     return {}
   }
 
-  prepare(_val: MAFValueDummy): MAFMetaDummy {
-    return {}
-  }
-
-  semantic(_val: MAFValueDummy, _opt: MAFOptionDummy, _meta: MAFMetaDummy, _opposite: boolean): string {
+  semantic(_val: MAFValueBoolean & MAFValueNumberRange, _opt: MAFOptionSwitch & MAFOptionRange, _meta: MAFMetaDummy, _opposite: boolean): string {
     return ''
   }
 
-  filter(_val: MAFValueDummy, _opt: MAFOptionDummy, _meta: MAFMetaDummy, _marker: API.MarkerVo): boolean {
+  filter(_val: MAFValueBoolean & MAFValueNumberRange, _opt: MAFOptionSwitch & MAFOptionRange, _meta: MAFMetaDummy, _marker: API.MarkerVo): boolean {
     return false
   }
 }
