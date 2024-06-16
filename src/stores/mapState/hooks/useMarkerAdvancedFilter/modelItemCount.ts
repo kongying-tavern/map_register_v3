ba51bf4014ed688ffcc7a2d03215f74232ea56bf
Alex Fang <deeno@qq.com>
2024-06-16 16:09:00 +0800

    feat: 添加高级过滤-物品计数语义化与过滤逻辑

--- src/stores/mapState/hooks/useMarkerAdvancedFilter/modelItemCount.ts
+++ src/stores/mapState/hooks/useMarkerAdvancedFilter/modelItemCount.ts
@@ -29,11 +29,32 @@ export class ItemCount implements MAFConfig {
     return {}
   }
 
-  semantic(_val: MAFValueBoolean & MAFValueNumberRange, _opt: MAFOptionSwitch & MAFOptionRange, _meta: MAFMetaDummy, _opposite: boolean): string {
+  semantic(val: MAFValueBoolean & MAFValueNumberRange, opt: MAFOptionSwitch & MAFOptionRange, _meta: MAFMetaDummy, opposite: boolean): string {
+    const optionTag = val.b ? opt.textActive! : opt.textInactive!
+    if (!Number.isFinite(val.nMin) && !Number.isFinite(val.nMax)) {
+      return opposite ? `无${optionTag}物品计数` : `不限${optionTag}物品计数`
+    }
+    else if (Number.isFinite(val.nMin) && Number.isFinite(val.nMax)) {
+      if (val.nMin === val.nMax)
+        return `${optionTag}物品计数${opposite ? '非' : ''}【${val.nMin}】`
+
+      else
+        return `${optionTag}物品计数${opposite ? '非' : ''}【${val.nMin}~${val.nMax}】`
+    }
+    else if (Number.isFinite(val.nMin) && !Number.isFinite(val.nMax)) {
+      return `${optionTag}物品计数【${opposite ? '＜' : '≥'}${val.nMin}】`
+    }
+    else if (!Number.isFinite(val.nMin) && Number.isFinite(val.nMax)) {
+      return `${optionTag}物品计数【${opposite ? '＞' : '≤'}${val.nMax}】`
+    }
     return ''
   }
 
-  filter(_val: MAFValueBoolean & MAFValueNumberRange, _opt: MAFOptionSwitch & MAFOptionRange, _meta: MAFMetaDummy, _marker: API.MarkerVo): boolean {
-    return false
+  filter(val: MAFValueBoolean & MAFValueNumberRange, _opt: MAFOptionSwitch & MAFOptionRange, _meta: MAFMetaDummy, marker: API.MarkerVo): boolean {
+    const minVal: number = val.nMin === undefined || val.nMin === null ? Number.NEGATIVE_INFINITY : val.nMin
+    const maxVal: number = val.nMax === undefined || val.nMax === null ? Number.POSITIVE_INFINITY : val.nMax
+    const itemList = marker.itemList ?? []
+    const isMatch = (item: API.MarkerItemLinkVo): boolean => item.count! >= minVal && item.count! <= maxVal
+    return val.b ? itemList.every(isMatch) : itemList.some(isMatch)
   }
 }
