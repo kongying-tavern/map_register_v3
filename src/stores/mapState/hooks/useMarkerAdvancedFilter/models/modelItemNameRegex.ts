2294d662c692493f070ee95be197c6e357f448a9
Alex Fang <deeno@qq.com>
2024-06-20 00:04:27 +0800

    feat(SemanticText): 新增语义化-物品正则

--- src/stores/mapState/hooks/useMarkerAdvancedFilter/models/modelItemNameRegex.ts
+++ src/stores/mapState/hooks/useMarkerAdvancedFilter/models/modelItemNameRegex.ts
@@ -51,8 +51,13 @@ export class ItemNameRegex implements MAFConfig {
     return meta
   }
 
-  semantic(_val: MAFValueString, _opt: MAFOptionInput, _meta: MAFMetaItemNameRegex, _opposite: boolean): MAFSemanticUnit[] {
-    return []
+  semantic(val: MAFValueString, _opt: MAFOptionInput, meta: MAFMetaItemNameRegex, opposite: boolean): MAFSemanticUnit[] {
+    return [
+      { type: 'text', text: '物品名' },
+      opposite ? { type: 'opposite-indicator', text: '不' } : null,
+      { type: 'text', text: '满足正则' },
+      meta.re === null ? { type: 'error', text: '正则错误' } : { type: 'regex', text: val.s },
+    ].filter(v => v) as MAFSemanticUnit[]
   }
 
   filter(_val: MAFValueString, _opt: MAFOptionInput, meta: MAFMetaItemNameRegex, marker: API.MarkerVo): boolean {
