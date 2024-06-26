294e2b4d2665663c5701f4dbd2a2261effe24618
夕云葛城 <KuroNeko7520@outlook.com>
2024-06-27 02:12:48 +0800

    fix(AppDraggableTable): 修复递归的文本输入类组件被拖拽事件覆盖而无法进行选中操作的问题

--- src/components/AppDraggableTable/AppDraggableTable.vue
+++ src/components/AppDraggableTable/AppDraggableTable.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts" generic="T">
+import { debounceTime, filter, finalize, map, race, switchMap, takeUntil, tap } from 'rxjs'
+import { fromEvent as fromRefEvent, useSubscription } from '@vueuse/rxjs'
+import { parentDragId } from './shared'
+
 const props = withDefaults(defineProps<{
   getKey?: (data: T) => string
+  /** 拖拽排序动画的过渡时间 */
   transitionDuration?: number
+  /** 是否在 focus-within 状态下进行拖拽（可能会阻止 input 等元素的文本选择操作） */
+  draggableInFocusWithin?: boolean
 }>(), {
   transitionDuration: 150,
 })
@@ -29,8 +36,53 @@ const modelValue = defineModel<T[]>('modelValue', {
   default: () => [],
 })
 
+const containerRef = ref() as Ref<HTMLElement>
+
 /** 拖拽识别 id */
 const DRAG_ID = [...crypto.getRandomValues(new Uint8Array(5))].map(num => num.toString(16).padStart(2, '0')).join('')
+provide(parentDragId, DRAG_ID)
+
+/** 用于处理递归场景 */
+const parent = inject(parentDragId, undefined)
+
+/** 过渡样式只需要一个 */
+if (!parent) {
+  const { load, unload } = useStyleTag(computed(() => `
+::view-transition-group(*) {
+  animation-duration: ${props.transitionDuration}ms;
+  animation-timing-function: ease;
+}
+::view-transition-
+`), {
+    immediate: false,
+  })
+
+  onActivated(load)
+  onDeactivated(unload)
+}
+
+const pointerdown = fromRefEvent(containerRef, 'pointerdown')
+const pointerup = fromRefEvent(containerRef, 'pointerup')
+const dragend = fromRefEvent(containerRef, 'dragend')
+const dragstart = fromRefEvent(containerRef, 'dragstart')
+const dragenter = fromRefEvent(containerRef, 'dragenter')
+
+/** 是否可拖拽 */
+const draggable = ref(false)
+
+// 控制列表是否可进行拖拽的逻辑
+useSubscription(pointerdown.pipe(
+  filter(() => !props.draggableInFocusWithin),
+  tap((ev) => {
+    const text = ev.composedPath().find(el => el instanceof HTMLInputElement || el instanceof HTMLTextAreaElement)
+    draggable.value = !text
+  }),
+  switchMap(() => race(pointerup, dragend).pipe(
+    tap(() => {
+      draggable.value = false
+    }),
+  )),
+).subscribe())
 
 const isDragging = ref(false)
 
@@ -58,39 +110,6 @@ watch(() => modelValue.value, (list) => {
   shadowData.value = initShadowList(list)
 }, { deep: true })
 
-const handleDragStart = (ev: DragEvent) => {
-  const result = ev
-    .composedPath()
-    .find((target) => {
-      return target instanceof HTMLElement && target.dataset.dragId === DRAG_ID
-    }) as HTMLElement | undefined
-  if (!result)
-    return
-
-  const { key = '' } = result.dataset
-  const data = dataKeyMap.value.get(key)
-  if (!data)
-    return
-
-  sourceData.value = data
-  isDragging.value = true
-}
-
-const handleDragEnd = () => {
-  sourceData.value = undefined
-  targetData.value = undefined
-  isDragging.value = false
-  const result = shadowData.value.map(({ _raw }) => _raw)
-  modelValue.value = result
-  emits('change', result)
-}
-
-const move = (fromIndex: number, toIndex: number) => {
-  const element = shadowData.value.splice(fromIndex, 1)[0]
-  shadowData.value.splice(toIndex, 0, element)
-  targetData.value = undefined
-}
-
 const getDragWrapper = (target: EventTarget | null): HTMLElement | undefined => {
   if (!target)
     return
@@ -104,58 +123,81 @@ const getDragWrapper = (target: EventTarget | null): HTMLElement | undefined => 
   return getDragWrapper(target.parentElement)
 }
 
-const handleDragEnter = useDebounceFn((ev: DragEvent) => {
-  const result = getDragWrapper(ev.target)
-
-  if (!result || !sourceData.value)
-    return
-
-  const { key = '' } = result.dataset
-  if (key === sourceData.value.key || key === targetData.value?.key)
-    return
-
-  const data = dataKeyMap.value.get(key)
-  if (data === undefined)
-    return
-
-  targetData.value = data
-
-  let fromIndex: number | undefined
-  let toIndex: number | undefined
-
-  const { length } = shadowData.value
-  for (let index = 0; index < length; index++) {
-    const item = shadowData.value[index]
-    if (fromIndex === undefined && sourceData.value.key === item.key)
-      fromIndex = index
-    else if (toIndex === undefined && data.key === item.key)
-      toIndex = index
-    if (fromIndex !== undefined && toIndex !== undefined)
-      break
-  }
-
-  if (fromIndex === undefined || toIndex === undefined)
-    return
-
-  document.startViewTransition(() => {
-    move(fromIndex, toIndex)
-  })
-}, computed(() => props.transitionDuration))
-
-const { load, unload } = useStyleTag(computed(() => `
-::view-transition-group(*) {
-  animation-duration: ${props.transitionDuration}ms;
-  animation-timing-function: ease;
+const move = (fromIndex: number, toIndex: number) => {
+  const element = shadowData.value.splice(fromIndex, 1)[0]
+  shadowData.value.splice(toIndex, 0, element)
+  targetData.value = undefined
 }
-::view-transition-
-`), {
-  immediate: false,
-})
 
-onActivated(load)
-onDeactivated(unload)
+// 拖拽排序核心逻辑
+useSubscription(dragstart.pipe(
+  map((event) => {
+    const triggerElement = event.composedPath().find((target) => {
+      return target instanceof HTMLElement && target.dataset.dragId === DRAG_ID
+    }) as HTMLElement | undefined
+    const data = triggerElement === undefined
+      ? undefined
+      : dataKeyMap.value.get(triggerElement.dataset.key ?? '')
+    return { data, event }
+  }),
 
-const containerRef = ref<HTMLElement>() as Ref<HTMLElement>
+  filter(({ data }) => data !== undefined),
+
+  tap(({ data, event }) => {
+    event.stopPropagation()
+    sourceData.value = data
+    isDragging.value = true
+  }),
+
+  switchMap(() => dragenter.pipe(
+    debounceTime(props.transitionDuration),
+
+    map((ev) => {
+      const result = getDragWrapper(ev.target)
+      if (!result || !sourceData.value)
+        return {}
+
+      const { key = '' } = result.dataset
+      if (key === sourceData.value.key || key === targetData.value?.key)
+        return {}
+
+      const data = dataKeyMap.value.get(key)
+      targetData.value = data!
+
+      let fromIndex: number | undefined
+      let toIndex: number | undefined
+      const { length } = shadowData.value
+      for (let index = 0; index < length; index++) {
+        const item = shadowData.value[index]
+        if (fromIndex === undefined && sourceData.value!.key === item.key)
+          fromIndex = index
+        else if (toIndex === undefined && data!.key === item.key)
+          toIndex = index
+        if (fromIndex !== undefined && toIndex !== undefined)
+          break
+      }
+
+      return { fromIndex, toIndex }
+    }),
+
+    filter(({ fromIndex, toIndex }) => fromIndex !== undefined && toIndex !== undefined),
+
+    tap(({ fromIndex, toIndex }) => document.startViewTransition(() => {
+      move(fromIndex!, toIndex!)
+    })),
+
+    takeUntil(dragend),
+
+    finalize(() => {
+      sourceData.value = undefined
+      targetData.value = undefined
+      isDragging.value = false
+      const result = shadowData.value.map(({ _raw }) => _raw)
+      modelValue.value = result
+      emits('change', result)
+    }),
+  )),
+).subscribe())
 </script>
 
 <template>
@@ -165,9 +207,6 @@ const containerRef = ref<HTMLElement>() as Ref<HTMLElement>
     :class="{
       'is-dragging': isDragging,
     }"
-    @dragend="handleDragEnd"
-    @dragenter="handleDragEnter"
-    @dragstart="handleDragStart"
   >
     <template
       v-for="(item, index) in shadowData"
@@ -179,7 +218,7 @@ const containerRef = ref<HTMLElement>() as Ref<HTMLElement>
         :style="{
           'view-transition-name': `key-${item.key}`,
         }"
-        draggable="true"
+        :draggable="draggable"
       >
         <slot
           name="default"
