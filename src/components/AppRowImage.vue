c50772c629a3b9223d2cf151f0dc6e2085bda95b
夕云葛城 <KuroNeko7520@outlook.com>
2024-07-06 22:09:26 +0800

    style(AppRowImage): 优化图标容器样式

--- src/components/AppRowImage.vue
+++ src/components/AppRowImage.vue
@@ -6,12 +6,16 @@ defineProps<{
 
 <template>
   <div
-    class="w-8 h-8 rounded border flex-shrink-0"
-    style="background-color: var(--el-color-primary-light-9); border-color: var(--el-color-primary-light-8);"
+    class="w-8 h-8
+      rounded-full
+      border border-[var(--el-border-color)]
+      bg-[var(--el-color-primary-light-9)]
+      flex-shrink-0
+    "
   >
     <img
       v-if="src"
-      class="w-full h-full object-contain rounded border"
+      class="w-full h-full object-contain rounded"
       :src="src"
       crossorigin=""
     >
