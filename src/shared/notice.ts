4559e59dbca8523409de1f903bd7aae86bb80820
夕云葛城 <KuroNeko7520@outlook.com>
2024-07-16 03:50:16 +0800

    chore(notice): 修改了频道选项的排序和文本说明，使其更易于理解

--- src/shared/notice.ts
+++ src/shared/notice.ts
@@ -1,24 +1,24 @@
 /** 公告频道 */
 export enum NoticeChannel {
-  /** 通用 */
+  /** 全部频道 */
   COMMON = 'COMMON',
 
-  /** 前台（应用端） */
+  /** 前台全部 */
   APPLICATION = 'APPLICATION',
 
-  /** App客户端 */
+  /** 前台 - 移动端 */
   CLIENT_APP = 'CLIENT_APP',
 
-  /** PC客户端 */
+  /** 前台 - PC 端 */
   CLIENT_PC = 'CLIENT_PC',
 
-  /** 网页版 */
+  /** 前台 - Web 端 */
   WEB = 'WEB',
 
-  /** 后台（管理面板） */
+  /** 后台全部 */
   DASHBOARD = 'DASHBOARD',
 
-  /** 打点页 */
+  /** 后台 - 打点页 */
   DADIAN = 'DADIAN',
 
   /** 维系天理 */
@@ -26,12 +26,12 @@ export enum NoticeChannel {
 }
 
 export const NOTICE_NAME_MAP = new Map<string, string>([
+  [NoticeChannel.COMMON, '全部'],
   [NoticeChannel.APPLICATION, '前台'],
-  [NoticeChannel.CLIENT_APP, 'App客户端'],
-  [NoticeChannel.CLIENT_PC, 'PC客户端'],
-  [NoticeChannel.COMMON, '通用'],
-  [NoticeChannel.DADIAN, '打点页'],
+  [NoticeChannel.CLIENT_APP, '前台 - 移动端'],
+  [NoticeChannel.CLIENT_PC, '前台 - PC端'],
+  [NoticeChannel.WEB, '前台 - Web端'],
   [NoticeChannel.DASHBOARD, '后台'],
+  [NoticeChannel.DADIAN, '后台 - 打点页'],
   [NoticeChannel.TIANLI, '维系天理'],
-  [NoticeChannel.WEB, '网页版'],
 ])
