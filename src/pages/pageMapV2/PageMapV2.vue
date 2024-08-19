a609e70d2c1c851ea4eee302041ae8ce91ac834a
夕云葛城 <KuroNeko7520@outlook.com>
2024-08-19 18:50:29 +0800

    refactor(PageMapV2): 图层整理

--- src/pages/pageMapV2/PageMapV2.vue
+++ src/pages/pageMapV2/PageMapV2.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { useInteractionLayer, useMap, useMapSiderMenu } from './hooks'
-import { genshinMapCanvasKey, mapAffixLayerKey, mutuallyExclusiveLayerKey } from './shared'
+import { genshinMapCanvasKey, mapAffixLayerKey } from './shared'
 import {
   CollapseButton,
   MapContextMenu,
@@ -18,7 +18,6 @@ import { useAccessStore } from '@/stores'
 const accessStore = useAccessStore()
 
 const canvasRef = ref() as Ref<HTMLCanvasElement>
-const mutuallyExclusiveLayerRef = ref<HTMLElement>()
 const mapAffixLayerRef = ref<HTMLElement>()
 
 useMap(canvasRef)
@@ -31,18 +30,15 @@ const { visible: interactionLayerVisible } = useInteractionLayer()
 const { collapse } = useMapSiderMenu(canvasRef)
 
 provide(genshinMapCanvasKey, canvasRef)
-provide(mutuallyExclusiveLayerKey, mutuallyExclusiveLayerRef)
 provide(mapAffixLayerKey, mapAffixLayerRef)
 </script>
 
 <template>
   <div class="genshin-map-container w-full h-full relative">
-    <div class="background-mask absolute left-0 top-0 w-full h-full pointer-events-none z-[1]" />
-
     <canvas ref="canvasRef" class="map-renderer w-full h-full bg-black" />
 
-    <div class="map-interaction-layer absolute left-0 top-0 w-full h-full pointer-events-none transition-all">
-      <div ref="mapAffixLayerRef" class="map-affix-provider">
+    <div class="virtual-zone bg-[radial-gradient(transparent_50%,#00000060)]">
+      <div ref="mapAffixLayerRef" class="affix-layer">
         <MarkerPopover />
         <MapContextMenu />
       </div>
@@ -52,30 +48,29 @@ provide(mapAffixLayerKey, mapAffixLayerRef)
         class="z-10 transition-all"
         :class="[interactionLayerVisible ? 'pointer-events-auto' : '-translate-x-full']"
       />
+
       <CollapseButton
         v-model:collapse="collapse"
         :class="[interactionLayerVisible ? 'pointer-events-auto' : '-translate-x-full']"
         :style="{ '--tw-translate-x': interactionLayerVisible ? '0%' : '-300%' }"
       />
-    </div>
 
-    <MapWindowManager />
-    <ZoomController class="absolute right-0 top-1/2" :delta-zoom="0.2" />
-    <MapStateBar />
-    <MapCursor />
+      <MapWindowManager />
 
-    <div
-      ref="mutuallyExclusiveLayerRef"
-      class="mutually-exclusive-layer absolute left-0 top-0 w-full h-full pointer-events-none"
-    >
+      <ZoomController class="absolute right-0 top-1/2" :delta-zoom="0.2" />
+
+      <MapStateBar />
+
+      <MapCursor />
+
       <MarkerMoveController v-if="accessStore.get('MARKER_EDIT')" />
       <MarkerTweakController v-if="accessStore.get('MARKER_BATCH_EDIT')" />
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.background-mask {
-  background: radial-gradient(transparent 50%, #00000060);
+<style scoped>
+.virtual-zone {
+  @apply absolute left-0 top-0 w-full h-full pointer-events-none;
 }
 </style>
