59c9d2018e893794a8a5be16dc0a571b1e9b81c5
夕云葛城 <KuroNeko7520@outlook.com>
2024-08-22 04:01:35 +0800

    refactor(pageMapV2/shared): 删除多余变量

--- src/pages/pageMapV2/shared/index.ts
+++ src/pages/pageMapV2/shared/index.ts
@@ -1,6 +1,5 @@
 import type { InjectionKey } from 'vue'
 
-export const mutuallyExclusiveLayerKey = Symbol('mutually-exclusive-layer') as InjectionKey<Ref<HTMLElement | undefined>>
 export const genshinMapCanvasKey = Symbol('canvas-ref') as InjectionKey<Ref<HTMLCanvasElement | undefined>>
 export const mapAffixLayerKey = Symbol('map-affix-layer') as InjectionKey<Ref<HTMLElement | undefined>>
 export const mapSidermenuKey = Symbol('map-sidermenu-ref') as InjectionKey<Ref<HTMLElement | undefined>>
