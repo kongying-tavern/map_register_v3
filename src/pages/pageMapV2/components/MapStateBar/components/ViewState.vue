85808f61ffc6c721bcd7fbd58a9f3ed0e077eef0
夕云葛城 <KuroNeko7520@outlook.com>
2024-08-22 23:51:30 +0800

    feat(ViewState): 使用 DOM 显示视口状态，并添加重置视口状态的功能

--- src/pages/pageMapV2/components/MapStateBar/components/ViewState.vue
+++ src/pages/pageMapV2/components/MapStateBar/components/ViewState.vue
@@ -1,45 +1,54 @@
 <script setup lang="ts">
 import BarItem from './BarItem.vue'
 import { useMapStateStore, useTileStore } from '@/stores'
+import { EaseoutInterpolator } from '@/pages/pageMapV2/core/interpolator'
 
 const tileStore = useTileStore()
 const mapStateStore = useMapStateStore()
 
-const canvasRef = ref<HTMLCanvasElement>()
+const resetViewState = () => {
+  mapStateStore.event.emit('setViewState', {
+    target: [3568, 6969],
+    zoom: 0,
+    transitionDuration: 200,
+    transitionInterpolator: new EaseoutInterpolator(['target', 'zoom']),
+  })
+}
 
 const markerView = computed(() => {
   const { target: [x, y], zoom } = mapStateStore.viewState
   const [mx, my] = tileStore.toMarkerCoordinate([x, y])
   return [mx, my, zoom]
 })
-
-onMounted(() => {
-  const canvas = canvasRef.value
-  if (!canvas)
-    return
-
-  const ctx = canvas.getContext('2d')!
-
-  const fontSize = 10
-
-  ctx.font = `${fontSize + 2}px monospace`
-  ctx.fillStyle = '#C6C2BA'
-
-  const draw = () => {
-    const { width, height } = canvas
-    const [x, y, z] = toValue(markerView)
-    ctx.clearRect(0, 0, width, height)
-    ctx.fillText(`X${x.toFixed(0).padStart(9, ' ')}`, 2, 1 * fontSize, width)
-    ctx.fillText(`Y${y.toFixed(0).padStart(9, ' ')}`, 2, 2 * fontSize, width)
-    ctx.fillText(`Z${z.toFixed(2).padStart(9, ' ')}`, 2, 3 * fontSize, width)
-    requestAnimationFrame(draw)
-  }
-  draw()
-})
 </script>
 
 <template>
-  <BarItem label="视口信息">
-    <canvas ref="canvasRef" width="64" height="32" />
+  <BarItem label="视口信息" class="w-14 grid place-content-center" @click="resetViewState">
+    <div class="w-16 text-xs translate-x-1.5 translate-y-[0.5px] leading-none overflow-hidden">
+      <div class="flex gap-2 scale-[0.8] origin-left overflow-hidden -mb-[2px]">
+        <div class="flex-shrink-0">
+          X
+        </div>
+        <div class="flex-1 text-right overflow-hidden whitespace-nowrap text-ellipsis">
+          {{ Math.floor(markerView[0]) }}
+        </div>
+      </div>
+      <div class="flex gap-2 scale-[0.8] origin-left overflow-hidden">
+        <div class="flex-shrink-0">
+          Y
+        </div>
+        <div class="flex-1 text-right overflow-hidden whitespace-nowrap text-ellipsis">
+          {{ Math.floor(markerView[1]) }}
+        </div>
+      </div>
+      <div class="flex gap-2 scale-[0.8] origin-left overflow-hidden -mt-[2px]">
+        <div class="flex-shrink-0">
+          Z
+        </div>
+        <div class="flex-1 text-right overflow-hidden whitespace-nowrap text-ellipsis">
+          {{ markerView[2].toFixed(2) }}
+        </div>
+      </div>
+    </div>
   </BarItem>
 </template>
