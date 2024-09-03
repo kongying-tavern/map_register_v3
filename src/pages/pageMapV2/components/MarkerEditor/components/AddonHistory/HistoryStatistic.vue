0cf7ffd9b25856b1a2fc6de9f612adfff23275d6
夕云葛城 <KuroNeko7520@outlook.com>
2024-09-03 18:33:58 +0800

    feat(HistoryStatistic): 历史记录图表组件

---
+++ src/pages/pageMapV2/components/MarkerEditor/components/AddonHistory/HistoryStatistic.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { ElDialog } from 'element-plus'
+import { useHistoryChart } from './hooks'
+import { WinDialog, WinDialogTabPanel, WinDialogTitleBar } from '@/components'
+
+const props = defineProps<{
+  record: API.HistoryVo[]
+  current: API.MarkerVo
+  users: Map<string, API.SysUserSmallVo>
+}>()
+
+const visible = defineModel<boolean>('visible', {
+  required: true,
+})
+
+const containerRef = ref<HTMLElement>()
+useHistoryChart(containerRef, {
+  data: computed(() => props.record),
+  users: computed(() => props.users),
+  current: computed(() => props.current),
+})
+</script>
+
+<template>
+  <ElDialog
+    v-model="visible"
+    class="hidden-header bg-transparent"
+    width="fit-content"
+    align-center
+    destroy-on-close
+    append-to-body
+    :style="{
+      '--el-dialog-border-radius': '8px',
+      '--el-dialog-padding-primary': '0',
+    }"
+  >
+    <WinDialog>
+      <WinDialogTitleBar @close="visible = false">
+        历史记录统计图表
+      </WinDialogTitleBar>
+
+      <WinDialogTabPanel class="w-[512px] h-[384px] mb-2 flex flex-col overflow-hidden">
+        <div ref="containerRef" class="flex-1 w-full" />
+      </WinDialogTabPanel>
+    </WinDialog>
+  </ElDialog>
+</template>
+
+<style>
+.hidden-header {
+  .el-dialog__header {
+    display: none;
+  }
+}
+</style>
