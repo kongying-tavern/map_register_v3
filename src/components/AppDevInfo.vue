961b6ecc799cffa370728ede7f6167cb68f34397
夕云葛城 <KuroNeko7520@outlook.com>
2024-09-09 19:26:40 +0800

    style(AppDevInfo): 字体颜色修改

--- src/components/AppDevInfo.vue
+++ src/components/AppDevInfo.vue
@@ -10,7 +10,7 @@ const {
   <div
     class="
       fixed left-0 bottom-0 w-full h-8 leading-8 px-8
-      font-[HYWenHei-85W] text-white text-sm
+      font-[HYWenHei-85W] text-[var(--el-text-color-regular)] text-sm
       pointer-events-none
     "
     style="-webkit-text-stroke: 0.25px black;"
