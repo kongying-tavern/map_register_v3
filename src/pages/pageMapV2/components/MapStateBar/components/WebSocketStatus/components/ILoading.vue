57f7e12cabad18e4886a0a53b6553a827faa0310
夕云葛城 <KuroNeko7520@outlook.com>
2024-09-13 04:35:13 +0800

    feat: 抽离用于显示 WS 事件记录的各种消息组件

---
+++ src/pages/pageMapV2/components/MapStateBar/components/WebSocketStatus/components/ILoading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="message-component-loading">
+    Loading...
+  </div>
+</template>
+
+<style scoped>
+@property --anime-ratio {
+  syntax: '<percentage>';
+  inherits: false;
+  initial-value: 0%;
+}
+
+@keyframes loading-anime {
+  from {
+    --anime-ratio: 0%;
+  }
+  to {
+    --anime-ratio: 100%;
+  }
+}
+
+.message-component-loading {
+  @apply p-2 text-[var(--el-text-color-primary)];
+  background: linear-gradient(
+    to right,
+    var(--el-fill-color-lighter),
+    var(--el-color-primary-light-7) var(--anime-ratio),
+    var(--el-fill-color-lighter)
+  );
+  animation: loading-anime 1s ease-in-out infinite both alternate;
+}
+</style>
