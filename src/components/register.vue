f51668ae8bd8e5d91ccc30c142e343865b3f5456
Alex Fang <deeno@qq.com>
2024-09-23 04:21:32 +0800

    fix: 修复排序

--- src/components/register.vue
+++ src/components/register.vue
@@ -596,6 +596,7 @@ export default {
           areaIdList: [selectorAreaId.value],
           current: 0,
           size: 9999,
+          sort: ['sortIndex-'],
         },
         this
       ).then((res) => {
