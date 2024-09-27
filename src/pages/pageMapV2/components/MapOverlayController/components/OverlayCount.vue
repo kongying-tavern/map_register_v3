f5436d50ad4619a0766078068649192137305dec
夕云葛城 <KuroNeko7520@outlook.com>
2024-09-27 18:26:08 +0800

    style(OverlayCount): 激活时的样式优化

--- src/pages/pageMapV2/components/MapOverlayController/components/OverlayCount.vue
+++ src/pages/pageMapV2/components/MapOverlayController/components/OverlayCount.vue
@@ -1,24 +1,28 @@
 <script setup lang="ts">
-withDefaults(defineProps<{
+const props = withDefaults(defineProps<{
   current?: number
   total?: number
   label: string
+  maxLength?: number
 }>(), {
   current: 0,
   total: 0,
+  maxLength: 2,
 })
+
+const isActived = computed(() => props.current > 0)
 </script>
 
 <template>
   <div class="flex-1 flex items-center overflow-hidden gap-1">
     <div
-      class="h-5 min-w-[20px] px-1 grid place-content-center rounded text-xs border border-[currentColor] font-mono"
-      :style="{
-        opacity: current > 0 ? '1' : '0.4',
-      }"
+      class="h-5 min-w-[20px] px-1 grid place-content-center rounded text-xs border font-mono"
+      :class="isActived ? 'text-[#CCFF33] border-[#FFF] bg-[#809933]' : 'border-[currentColor] opacity-40'"
       title="已启用 / 图层总数"
     >
-      {{ `${current}`.padStart(2, '0') }}/{{ `${total}`.padStart(2, '0') }}
+      {{ !current ? '--' : `${current}`.padStart(maxLength, '0') }}
+      /
+      {{ !total ? '--' : `${total}`.padStart(maxLength, '0') }}
     </div>
     <div
       class="flex-1 overflow-hidden whitespace-nowrap text-ellipsis"
