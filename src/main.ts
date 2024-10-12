3780e90717c34aebd73d51a861b3717ac0e550b0
夕云葛城 <KuroNeko7520@outlook.com>
2024-10-12 12:42:38 +0800

    chore(main): 重新引入依赖

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
 import { createApp } from 'vue'
-
-// import { createPinia } from 'pinia'
-// import ElementPlus from 'element-plus'
-// import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import App from './App.vue'
+import { ElementIcons, createLogger, createPWA, customPaint } from '@/plugin'
 import './style/app.scss'
-
-// import { ElementIcons, createLogger, createPWA, customPaint } from '@/plugin'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
 const app = createApp(App)
 
 app
-  // .use(createPinia())
-  // .use(createLogger())
-  // .use(createPWA())
-  // .use(customPaint())
-  // .use(ElementPlus, {
-  //   locale: zhCn,
-  // })
-  // .use(ElementIcons)
+  .use(createPinia())
+  .use(createLogger())
+  .use(createPWA())
+  .use(customPaint())
+  .use(ElementPlus, {
+    locale: zhCn,
+  })
+  .use(ElementIcons)
   .mount('#app')
