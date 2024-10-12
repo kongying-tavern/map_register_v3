0ce89fd47fb06239b003baef6347b923f19940d8
夕云葛城 <KuroNeko7520@outlook.com>
2024-10-12 12:42:04 +0800

    refactor(GenshinMapDeck): 将部分配置移动到 props 中以便组件外定制

--- src/packages/map/src/components/GenshinMapDeck.vue
+++ src/packages/map/src/components/GenshinMapDeck.vue
@@ -1,22 +1,58 @@
 <script setup lang="ts">
-import type { GenshinMapViewState } from '../types'
+import type { LayersList } from '@deck.gl/core'
+import type { GenshinMapProps, GenshinMapViewState } from '../types'
 import { useGenshinMap } from '../hooks'
+import type { GenshinMap } from '../core'
 
-defineProps<{
+const props = defineProps<{
+  layers?: LayersList
+  cursor?: string
   initialViewState?: GenshinMapViewState
+  disableViewStateChange?: boolean
+  getTooltip?: GenshinMapProps['getTooltip']
+}>()
+
+const emits = defineEmits<{
+  load: [GenshinMap]
 }>()
 
 const canvasRef = shallowRef<HTMLCanvasElement>()
 
 const { instanceRef } = useGenshinMap(canvasRef, {
-
+  initialViewState: props.initialViewState,
+  layers: props.layers,
+  controller: {
+    scrollZoom: {
+      transitionDuration: 60,
+    },
+  },
+  getCursor: ({ isDragging, isHovering }) => {
+    if (props.cursor?.trim().length)
+      return props.cursor
+    if (isHovering)
+      return 'pointer'
+    if (isDragging)
+      return 'grabbing'
+    return 'inherit'
+  },
+  getTooltip: (info) => {
+    return props.getTooltip?.(info) ?? null
+  },
+  onViewStateChange: (params) => {
+    if (props.disableViewStateChange)
+      return
+    return params.viewState
+  },
+  onLoad: () => {
+    if (!instanceRef.value)
+      return
+    emits('load', instanceRef.value)
+  },
 })
 
-watch(instanceRef, () => {
-  console.log('instance', instanceRef.value)
-})
+watch(() => props.layers, layers => instanceRef.value?.setProps({ layers }))
 </script>
 
 <template>
-  <canvas ref="canvasRef" />
+  <canvas ref="canvasRef" draggable="false" />
 </template>
