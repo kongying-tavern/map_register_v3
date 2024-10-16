0eb26354af40e22cbc39d9381b274ffc30d8ac72
夕云葛城 <KuroNeko7520@outlook.com>
2024-10-16 16:12:53 +0800

    style(AppDevInfo): 样式优化

--- src/components/AppDevInfo.vue
+++ src/components/AppDevInfo.vue
@@ -9,8 +9,9 @@ const {
 <template>
   <div
     class="
-      fixed left-0 bottom-0 w-full h-8 leading-8 px-8
+      fixed left-0 bottom-0 w-full h-8 leading-8 px-4
       font-[HYWenHei-85W] text-[var(--el-color-white)] text-sm
+      overflow-hidden text-ellipsis whitespace-nowrap
       pointer-events-none
     "
     style="-webkit-text-stroke: 2px #363D4D; paint-order: stroke"
