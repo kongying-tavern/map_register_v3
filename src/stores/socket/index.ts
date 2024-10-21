a6df2d424086028f836f191e5d2f21ebd993c9cb
夕云葛城 <KuroNeko7520@outlook.com>
2024-10-21 17:07:22 +0800

    fix(socketStore): 移除 socket 的强提醒

--- src/stores/socket/index.ts
+++ src/stores/socket/index.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ElNotification, type NotificationHandle } from 'element-plus'
+import { ElNotification } from 'element-plus'
 import { useArchiveStore, useUserStore } from '..'
 import { useMessageEvent, useMessageList, useSocket } from './hooks'
 import { EventBus } from '@/utils'
@@ -48,42 +48,6 @@ export const useSocketStore = defineStore('global-web-socket', () => {
 
   onClose(() => {
     _userId.value = undefined
-  })
-
-  const socketNotice = shallowRef<NotificationHandle>()
-
-  const statusHandler: Record<number, () => void> = {
-    [WebSocket.CLOSED]: () => {
-      socketNotice.value?.close()
-      socketNotice.value = ElNotification({
-        title: 'Error',
-        message: 'WebSocket 连接已断开',
-        type: 'error',
-      })
-    },
-    [WebSocket.CONNECTING]: () => {
-      socketNotice.value?.close()
-      socketNotice.value = ElNotification({
-        title: 'Info',
-        message: '正在建立 WebSocket 连接',
-        type: 'info',
-        onClose: () => {},
-      })
-    },
-    [WebSocket.OPEN]: () => {
-      socketNotice.value?.close()
-      socketNotice.value = ElNotification({
-        title: 'Success',
-        message: 'WebSocket 已连接',
-        type: 'success',
-      })
-    },
-  }
-
-  watch(status, (newStatus, oldStatus) => {
-    if (newStatus === oldStatus)
-      return
-    statusHandler[newStatus]?.()
   })
 
   watch(() => userStore.info?.id, (userId) => {
