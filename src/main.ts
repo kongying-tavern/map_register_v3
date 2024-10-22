68bc7f75f2c4aa959487188e97ee0f60878f0b15
夕云葛城 <KuroNeko7520@outlook.com>
2024-10-23 05:41:13 +0800

    refactor(AppError): 将全局错误组件移动到 components

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import App from './App.vue'
-import PageError from '@/pages/pageError/pageError.vue'
 import { ElementIcons, createLogger, createPWA, customPaint } from '@/plugin'
 import './style/app.scss'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import { useArchiveStore, useDadianStore, useUserStore } from '@/stores'
+import { AppError } from '@/components'
 
 const container = '#app'
 
@@ -29,7 +29,7 @@ try {
   app.mount('#app')
 }
 catch (err) {
-  createApp(PageError, {
+  createApp(AppError, {
     message: err instanceof Error ? err.message : `${err}`,
   }).mount(container)
 }
