18df34491ee859ba3c6dc62fd86de562dae884c8
夕云葛城 <KuroNeko7520@outlook.com>
2024-11-03 20:16:23 +0800

    refactor: remove routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,20 +1,6 @@
 import type { RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/404',
-    meta: {
-      title: '404',
-    },
-    component: () => import('@/pages/pageNotFound/PageNotFound.vue'),
-  },
-  {
-    path: '/error',
-    meta: {
-      title: 'Error',
-    },
-    component: () => import('@/pages/pageError/pageError.vue'),
-  },
 ]
 
 export default routes
