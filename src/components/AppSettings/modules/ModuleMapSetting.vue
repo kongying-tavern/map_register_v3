5225b832c527e8515cba250ece59951578090663
夕云葛城 <KuroNeko7520@outlook.com>
2024-11-06 13:30:49 +0800

    fix(ModuleMapSetting): 首选项绑定方式修改

--- src/components/AppSettings/modules/ModuleMapSetting.vue
+++ src/components/AppSettings/modules/ModuleMapSetting.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
 import { SettingBar, SettingGroup, SettingPanel } from '../components'
 import { usePreferenceStore } from '@/stores'
 
-const { preference } = storeToRefs(usePreferenceStore())
+const preferenceStore = usePreferenceStore()
 </script>
 
 <template>
@@ -11,12 +10,12 @@ const { preference } = storeToRefs(usePreferenceStore())
     <SettingGroup name="筛选器设置">
       <SettingBar label="条件预设" note="当前存储的自定义条件预设总数">
         <template #setting>
-          <span class="text-sm">{{ preference['markerFilter.setting.presets']?.filter(p => p.name !== 'temp')?.length ?? 0 }}</span>
+          <span class="text-sm">{{ preferenceStore.presets.filter(p => p.name !== 'temp')?.length ?? 0 }}</span>
         </template>
       </SettingBar>
       <SettingBar label="自动跳转下级筛选" note="点位筛选器满足条件后自动跳转下一级筛选器">
         <template #setting>
-          <el-switch v-model="preference['markerFilter.setting.autoNext']" />
+          <el-switch v-model="preferenceStore.autoNext" />
         </template>
       </SettingBar>
     </SettingGroup>
@@ -24,33 +23,25 @@ const { preference } = storeToRefs(usePreferenceStore())
     <SettingGroup name="地图设置">
       <SettingBar label="显示区域标签" note="地图上各国、地区、子地区的标签">
         <template #setting>
-          <el-switch v-model="preference['map.setting.showZoneTag']" />
+          <el-switch v-model="preferenceStore.showZoneTag" />
         </template>
       </SettingBar>
       <SettingBar label="弱化已完成点位" note="是否以半透明状态显示已完成点位">
         <template #setting>
-          <el-switch v-model="preference['map.setting.transparentMarked']" />
+          <el-switch v-model="preferenceStore.transparentMarked" />
         </template>
       </SettingBar>
       <SettingBar label="滚轮缩放过渡时间（毫秒）" note="使用滚轮进行缩放时，过渡所消耗的时间，设置为 0 时则取消过渡">
         <template #setting>
           <div class="flex flex-col">
             <el-input-number
-              v-model="preference['map.setting.zoomTransitionDuration']"
+              v-model="preferenceStore.zoomTransitionDuration"
               style="width: 140px"
               :min="0"
               :step="10"
               :max="200"
             />
           </div>
-        </template>
-      </SettingBar>
-    </SettingGroup>
-
-    <SettingGroup name="性能优化">
-      <SettingBar label="低频拾取" note="在视图更新时暂停点位拾取计算">
-        <template #setting>
-          <el-switch v-model="preference['map.setting.pauseViewChangingPicking']" />
         </template>
       </SettingBar>
     </SettingGroup>
--- src/stores/userPreference.ts
+++ src/stores/userPreference.ts
@@ -14,44 +14,29 @@ export const usePreferenceStore = defineStore('global-user-preference', () => {
     },
   }) as WritableComputedRef<NonNullable<UserPreference[K]>>
 
-  const advancedFilter = model('markerFilter.filter.advancedFilter', () => [])
-  const advancedFilterCache = model('markerFilter.filter.advancedFilterCache', () => [])
-  const autoNext = model('markerFilter.setting.autoNext', () => false)
-  const areaCode = model('markerFilter.state.areaCode', () => '')
-  const defaultMarkingItemId = model('markerFilter.state.defaultMarkingItemId', () => -1)
-  const enableLoggers = model('developer.setting.enableLoggers', () => [])
-  const filterType = model('markerFilter.setting.filterType', () => 'basic')
-  const itemIds = model('markerFilter.state.itemIds', () => [])
-  const itemTypeId = model('markerFilter.state.itemTypeId', () => -1)
-  const maxLogs = model('developer.setting.maxLogs', () => 100)
-  const noticeRead = model('notice.state.read', () => [])
-  const noticeShowTime = model('notice.state.showTime', () => 0)
-  const pageSize = model('manager.setting.pageSize', () => [])
-  const parentAreaCode = model('markerFilter.state.parentAreaCode', () => '')
-  const presets = model('markerFilter.setting.presets', () => [])
-  const settingActivedKey = model('settingPanel.state.activedKey', () => '')
-  const step = model('markerFilter.state.step', () => -1)
-  const tabName = model('mapSiderMenu.state.tabName', () => '')
-
   return {
-    advancedFilter,
-    advancedFilterCache,
-    autoNext,
-    areaCode,
-    defaultMarkingItemId,
-    enableLoggers,
-    filterType,
-    itemIds,
-    itemTypeId,
-    maxLogs,
-    noticeRead,
-    noticeShowTime,
-    pageSize,
-    parentAreaCode,
-    presets,
-    settingActivedKey,
-    step,
-    tabName,
+    advancedFilter: model('markerFilter.filter.advancedFilter', () => []),
+    advancedFilterCache: model('markerFilter.filter.advancedFilterCache', () => []),
+    autoNext: model('markerFilter.setting.autoNext', () => false),
+    areaCode: model('markerFilter.state.areaCode', () => ''),
+    defaultMarkingItemId: model('markerFilter.state.defaultMarkingItemId', () => -1),
+    enableLoggers: model('developer.setting.enableLoggers', () => []),
+    filterType: model('markerFilter.setting.filterType', () => 'basic'),
+    itemIds: model('markerFilter.state.itemIds', () => []),
+    itemTypeId: model('markerFilter.state.itemTypeId', () => -1),
+    maxLogs: model('developer.setting.maxLogs', () => 100),
+    noticeRead: model('notice.state.read', () => []),
+    noticeShowTime: model('notice.state.showTime', () => 0),
+    pageSize: model('manager.setting.pageSize', () => []),
+    parentAreaCode: model('markerFilter.state.parentAreaCode', () => ''),
+    presets: model('markerFilter.setting.presets', () => []),
+    settingActivedKey: model('settingPanel.state.activedKey', () => ''),
+    showZoneTag: model('map.setting.showZoneTag', () => true),
+    step: model('markerFilter.state.step', () => 0),
+    tabName: model('mapSiderMenu.state.tabName', () => ''),
+    transparentMarked: model('map.setting.transparentMarked', () => false),
+    zoomTransitionDuration: model('map.setting.zoomTransitionDuration', () => 66),
+
     preference: archiveStore.currentArchive.body.Preference,
   }
 })
