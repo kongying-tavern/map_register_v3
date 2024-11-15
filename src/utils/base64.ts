d290b62f7a3d8fd74a6e564ca9a4fb8b01fead7e
Alex Fang <deeno@qq.com>
2024-11-15 22:40:29 +0800

    fix(base64): 修复 base64 转 Uint8Array 方法

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,12 +1,10 @@
 export const base64ToUint8Array = (base64: string) => {
   if (!base64)
     return new Uint8Array()
-  const binString = decodeURIComponent(atob(base64))
-  const binLen = binString.length
-  const binArr = new Uint8Array(binLen)
-  for (let i = 0; i < binLen; i++)
-    binArr[i] = binString.charCodeAt(i)
-  return binArr
+  return new Uint8Array(atob(base64)
+    .split('')
+    .map(c => c.charCodeAt(0)),
+  )
 }
 
 export const uint8ArrayToBase64 = (data: Uint8Array) => {
