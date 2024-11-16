cf6e9ded4b0dd58877145ec5e2e97c97aa6595a4
Alex Fang <deeno@qq.com>
2024-11-16 22:19:01 +0800

    chore(config): 添加配置存档地址

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,6 +1,6 @@
 /** 从订阅源获取打点的相关依赖配置 */
 const getDadianConfig = async () => {
-  const res = await fetch(`${import.meta.env.VITE_ASSETS_BASE}/dadian.json.bz2`)
+  const res = await fetch(`${import.meta.env.VITE_CONFIG_ARCHIVE}`)
   return await res.arrayBuffer()
 }
 
