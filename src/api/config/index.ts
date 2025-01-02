4ab1a857b02bb9da30d72b632a3e5f2e6e486f07
夕云葛城 <KuroNeko7520@outlook.com>
2025-01-02 12:15:29 +0800

    feat(api/config): 为 dadian.json 的获取添加默认的超时时间

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,6 +1,17 @@
 /** 从订阅源获取打点的相关依赖配置 */
-const getDadianConfig = async () => {
-  const res = await fetch(`${import.meta.env.VITE_CONFIG_ARCHIVE}`)
+const getDadianConfig = async (options: {
+  timeout: number
+}) => {
+  const {
+    timeout = 10000,
+  } = options
+  const abortController = new AbortController()
+  setTimeout(() => {
+    abortController.abort()
+  }, timeout)
+  const res = await fetch(`${import.meta.env.VITE_CONFIG_ARCHIVE}`, {
+    signal: abortController.signal,
+  })
   return await res.arrayBuffer()
 }
 
