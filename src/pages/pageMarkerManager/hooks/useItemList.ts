04f1de280ddd0eee2c67990d6a41fe21da37c2d6
夕云葛城 <KuroNeko7520@outlook.com>
2025-01-19 17:19:10 +0800

    refactor(useItemList.ts): 重构数据绑定为 store 值

--- src/pages/pageMarkerManager/hooks/useItemList.ts
+++ src/pages/pageMarkerManager/hooks/useItemList.ts
@@ -1,6 +1,5 @@
 import type { FetchHookOptions } from '@/hooks'
-import { useFetchHook } from '@/hooks'
-import db from '@/database'
+import { useItemStore } from '@/stores'
 
 interface ItemListHookOption extends Omit<FetchHookOptions<API.RListItemVo>, 'immediate'> {
   params?: () => {
@@ -13,44 +12,30 @@ interface ItemListHookOption extends Omit<FetchHookOptions<API.RListItemVo>, 'im
 export const useItemList = (options: ItemListHookOption = {}) => {
   const { params } = options
 
-  const fetchParams = computed(() => params?.())
+  const itemStore = useItemStore()
 
-  const itemList = ref<API.ItemVo[]>([])
+  const fetchParams = computed(() => params?.() ?? {})
+
+  const itemList = computed(() => {
+    const { areaIdList = [], typeIdList = [] } = fetchParams.value
+    let result = itemStore.itemList
+
+    if (areaIdList.length) {
+      const areaIdSet = new Set(areaIdList)
+      result = result.filter(({ areaId = -1 }) => areaIdSet.has(areaId))
+    }
+
+    if (typeIdList.length) {
+      const typeIdSet = new Set(typeIdList)
+      result = result.filter(({ typeIdList = [] }) => new Set(typeIdList).intersection(typeIdSet).size > 0)
+    }
+    return result
+  })
 
   const itemOptions = computed(() => itemList.value.map(itemvo => ({
     label: `${itemvo.name} (id: ${itemvo.id})`,
     value: itemvo.id,
   })))
 
-  const { refresh: getItemList, onSuccess, ...rest } = useFetchHook<API.ItemVo[]>({
-    immediate: true,
-    onRequest: () => {
-      const { areaIdList = [], typeIdList = [] } = fetchParams.value ?? {}
-      const isArea = areaIdList.length > 0
-      const isType = typeIdList.length > 0
-
-      // 如果地区和类型都没有值在，则返回全部物品
-      if (!isArea && !isType)
-        return db.item.toArray()
-
-      // 如果只有类型，则按类型筛选
-      if (!isArea)
-        return db.item.where('typeIdList').anyOf(typeIdList).toArray()
-
-      // 如果只有地区，则按地区筛选
-      if (!isType)
-        return db.item.where('id').anyOf(areaIdList).toArray()
-
-      // 如果地区和类型同时存在，则先筛类型后筛地区
-      return db.item.where('typeIdList').anyOf(typeIdList).and(({ areaId = -9999 }) => areaIdList.includes(areaId)).toArray()
-    },
-  })
-
-  onSuccess((data) => {
-    itemList.value = data.sort(({ sortIndex: a = -1 }, { sortIndex: b = -1 }) => b - a)
-  })
-
-  watch(fetchParams, getItemList, { deep: true })
-
-  return { itemList, itemOptions, getItemList, onSuccess, ...rest }
+  return { itemList, itemOptions }
 }
