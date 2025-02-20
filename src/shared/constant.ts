44d08c550ef33689b97e369eddbb3fe95842324b
夕云葛城 <KuroNeko7520@outlook.com>
2025-02-20 18:58:33 +0800

    feat: 增加一个初始时间

--- src/shared/constant.ts
+++ src/shared/constant.ts
@@ -3,3 +3,8 @@ export const FALLBACK_ITEM_ICON_URL = 'https://assets.yuanshen.site/icons/-1.png
 
 /** 个人头像地址的回退 url */
 export const FALLBACK_AVATAR_URL = 'https://webstatic.mihoyo.com/upload/contentweb/2022/10/20/62cb7fb1815d9d05d3ece2e0d8e85c7d_8731082942818264376.png'
+
+/**
+ * 部分时间范围查询接口，有意义的开始值时间
+ */
+export const DATA_START_TIME = 1661961600000 // 2022-09-01 00:00:00 GMT+0800
