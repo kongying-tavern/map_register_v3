1a5f52f635b3afd73d149384de65093564cbeb68
夕云葛城 <KuroNeko7520@outlook.com>
2025-02-27 16:48:23 +0800

    style(api/config/index.ts): format code

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -2,16 +2,16 @@
 const getDadianConfig = async (options: {
   timeout?: number
 } = {}) => {
-  const {
-    timeout = 10000,
-  } = options
+  const { timeout = 10000 } = options
+
   const abortController = new AbortController()
-  setTimeout(() => {
-    abortController.abort()
-  }, timeout)
+
+  setTimeout(() => abortController.abort(), timeout)
+
   const res = await fetch(`${import.meta.env.VITE_CONFIG_ARCHIVE}`, {
     signal: abortController.signal,
   })
+
   return await res.arrayBuffer()
 }
 
