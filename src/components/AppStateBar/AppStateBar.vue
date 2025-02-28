a5ab0ca9a6ee67dd5b65363157ae688f435e236c
夕云葛城 <KuroNeko7520@outlook.com>
2025-02-28 16:35:29 +0800

    fix(AppStateBar.vue): 修改点位关联权限

--- src/components/AppStateBar/AppStateBar.vue
+++ src/components/AppStateBar/AppStateBar.vue
@@ -37,7 +37,7 @@ useEventListener('click', checkTooltip)
 <template>
   <div class="map-state-bar">
     <div ref="contentZoneRef" class="bar-content">
-      <PaintMarkerLink v-if="accessStore.get('ADMIN_COMPONENT')" />
+      <PaintMarkerLink v-if="accessStore.get('MANAGER_COMPONENT')" />
       <SwitchMarkerPopover />
       <SwitchOverlay />
       <WebSocketStatus />
