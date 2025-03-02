3829f4f03e2f66d6d747234dec02086b21ca5e1a
夕云葛城 <KuroNeko7520@outlook.com>
2025-03-02 14:04:15 +0800

    chore: API 生成失败时在开发控制台打印错误

--- plugins/openapi2ts/index.ts
+++ plugins/openapi2ts/index.ts
@@ -23,60 +23,56 @@ export const openapi2ts = (optionList: Openapi2tsOptions[]): Plugin => {
     name: 'openapi-to-ts',
     apply: 'serve',
     configResolved: async ({ root }) => {
-      for await (const options of optionList) {
-        const {
-          serversPath = path.join(root, 'src'),
-          projectName = 'api',
-          ...args
-        } = options
+      try {
+        await Promise.all(optionList.map(async (options) => {
+          const {
+            serversPath = path.join(root, 'src'),
+            projectName = 'api',
+            ...args
+          } = options
 
-        const isGeneratedError = await generateService({
-          serversPath,
-          projectName,
-          ...args,
-        }).catch(err => err instanceof Error ? err : new Error(`${err}`))
-        if (isGeneratedError) {
-          console.error('[openAPI]', isGeneratedError)
-          return
-        }
+          await generateService({ serversPath, projectName, ...args })
 
-        const dtsFolderPath = path.join(serversPath, projectName)
-        const dts = await fs.readdir(dtsFolderPath)
+          const dtsFolderPath = path.join(serversPath, projectName)
+          const dts = await fs.readdir(dtsFolderPath)
 
-        await Promise.all(dts.map(async (filename) => {
-          const filePath = path.join(serversPath, projectName, filename)
-          const fileContent = (await fs.readFile(filePath, { encoding: 'utf8' })).toString()
-          const writeContent = fileContent
-            // 删除头部 eslint 禁用注释
-            .replace('// @ts-ignore\n/* eslint-disable */\n', '')
-            // 删除类型为 header 的 params 参数声明
-            // .replace(/\s+\/\/ header([\s\S]+?)(?=(}|(\/\/)))/g, '')
-            // 格式化
-            // .replace(/{\/\//g, '{\n    \/\/')
-            // params 上的空对象类型替换
-            .replace(/params: \{\}/g, 'params: NonNullable<unknown>')
-            // 尾部分号去除
-            // .replace(/;(?=(\r?\n))/g, '')
-            // 尾换行符使用 windows 风格
-            // .replace(/\n/g, '\r\n')
-            // 所有的 any 类型改为 unknown
-            .replace(/(?<=[,:]\s+)any/g, 'unknown')
-            // 将 options 类型指定为 AxiosRequestConfig
-            .replace('import { request } from \'@/utils\'', 'import type { AxiosRequestConfig } from \'axios\'\r\nimport { request } from \'@/utils\'')
-            .replace(/options\?: \{ \[key: string\]: unknown \}/g, 'options?: AxiosRequestConfig')
-          await fs.writeFile(filePath, writeContent)
+          await Promise.all(dts.map(async (filename) => {
+            const filePath = path.join(serversPath, projectName, filename)
+            const fileContent = (await fs.readFile(filePath, { encoding: 'utf8' })).toString()
+            const writeContent = fileContent
+              // 删除头部 eslint 禁用注释
+              .replace('// @ts-ignore\n/* eslint-disable */\n', '')
+              // 删除类型为 header 的 params 参数声明
+              // .replace(/\s+\/\/ header([\s\S]+?)(?=(}|(\/\/)))/g, '')
+              // 格式化
+              // .replace(/{\/\//g, '{\n    \/\/')
+              // params 上的空对象类型替换
+              .replace(/params: \{\}/g, 'params: NonNullable<unknown>')
+              // 尾部分号去除
+              // .replace(/;(?=(\r?\n))/g, '')
+              // 尾换行符使用 windows 风格
+              // .replace(/\n/g, '\r\n')
+              // 所有的 any 类型改为 unknown
+              .replace(/(?<=[,:]\s+)any/g, 'unknown')
+              // 将 options 类型指定为 AxiosRequestConfig
+              .replace('import { request } from \'@/utils\'', 'import type { AxiosRequestConfig } from \'axios\'\r\nimport { request } from \'@/utils\'')
+              .replace(/options\?: \{ \[key: string\]: unknown \}/g, 'options?: AxiosRequestConfig')
+            await fs.writeFile(filePath, writeContent)
+          }))
         }))
+        // run eslint, no need to await
+        new Promise<string>((resolve, reject) => {
+          const childProcess = cp.exec(`eslint ${path.resolve(__dirname, '../../src/api/api')} --fix`, (err, stdout) => {
+            childProcess.kill()
+            if (err)
+              return reject(err)
+            resolve(stdout)
+          })
+        }).catch(() => false)
       }
-
-      // run eslint, no need to await
-      new Promise<string>((resolve, reject) => {
-        const childProcess = cp.exec(`eslint ${path.resolve(__dirname, '../../src/api/api')} --fix`, (err, stdout) => {
-          childProcess.kill()
-          if (err)
-            return reject(err)
-          resolve(stdout)
-        })
-      }).catch(() => false)
+      catch (err) {
+        console.log('[OpenAPI] 生成失败：', err instanceof Error ? err.message : `${err}`)
+      }
     },
   }
   return plugin
