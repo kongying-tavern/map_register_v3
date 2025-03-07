d4ff749f4be25735bbf8f122dbe8063b512ec863
夕云葛城 <KuroNeko7520@outlook.com>
2025-03-07 20:03:55 +0800

    style(observable.ts): lint fix

--- src/shared/observable.ts
+++ src/shared/observable.ts
@@ -1,8 +1,8 @@
 import { filter, fromEvent } from 'rxjs'
 
-export const globalKeydown$ = fromEvent<KeyboardEvent>(window, 'keydown')
-  // chrome 自动填充会触发无 key 的 keydown 事件
-  .pipe(filter(ev => ev.key !== undefined))
+export const globalKeydown$ = fromEvent<KeyboardEvent>(window, 'keydown').pipe(
+  filter(ev => ev.key !== undefined), // chrome 自动填充会触发无 key 的 keydown 事件
+)
 export const globalKeyUp$ = fromEvent<KeyboardEvent>(window, 'keyup')
 export const globalPointerDown$ = fromEvent<PointerEvent>(window, 'pointerdown')
 export const globalPointerMove$ = fromEvent<PointerEvent>(window, 'pointermove')
