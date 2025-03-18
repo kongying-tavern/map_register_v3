2a15f82ce3850b7b234ca5e2a351c58878d26008
夕云葛城 <KuroNeko7520@outlook.com>
2025-03-19 02:30:53 +0800

    style: 将点位移动信息栏改动至界面上方，并添加进入和退出过渡

--- src/components/MapMarkerMoveController/MapMarkerMoveController.vue
+++ src/components/MapMarkerMoveController/MapMarkerMoveController.vue
@@ -94,45 +94,41 @@ useSubscription(MapSubject.dragStart.pipe(
 </script>
 
 <template>
-  <div
-    v-if="isDraggingProcessing"
-    class="marker-move-controller"
-  >
-    <div class="mission-info">
-      {{ draggingMission.size }}
+  <Transition name="draw-y" appear mode="in-out">
+    <div v-if="isDraggingProcessing" class="marker-move-controller">
+      <div class="controller-info-bar">
+        <div class="flex-1 text-center">
+          <div>正在编辑点位坐标</div>
+          <div class="text-xs">
+            共移动 {{ draggingMission.size }} 项
+          </div>
+        </div>
+
+        <div class="flex gap-2">
+          <GSButton
+            icon="submit"
+            size="small"
+            :disabled="!draggingMission.size"
+            :loading="loading"
+            @click="moveMarker"
+          >
+            确定
+          </GSButton>
+          <GSButton
+            icon="cancel"
+            size="small"
+            :disabled="loading"
+            @click="clearState"
+          >
+            取消
+          </GSButton>
+        </div>
+      </div>
     </div>
-    <div class="controller-footer">
-      <div>正在编辑点位坐标</div>
-      <GSButton
-        icon="submit"
-        :disabled="!draggingMission.size"
-        :loading="loading"
-        @click="moveMarker"
-      >
-        确认
-      </GSButton>
-      <GSButton
-        icon="cancel"
-        :disabled="loading"
-        @click="clearState"
-      >
-        取消
-      </GSButton>
-    </div>
-  </div>
+  </Transition>
 </template>
 
 <style scoped>
-@keyframes controller-info-anime-in {
-  from { translate: 0 -200px; }
-  to { translate: 0 0; }
-}
-
-@keyframes controller-footer-anime-in {
-  from { translate: 0 200px; }
-  to { translate: 0 0; }
-}
-
 .marker-move-controller {
   @apply
     absolute left-0 top-0 w-full h-full z-[10]
@@ -141,23 +137,15 @@ useSubscription(MapSubject.dragStart.pipe(
   ;
 }
 
-.mission-info {
+.controller-info-bar {
   @apply
-    p-1 px-2
-    absolute right-[8px] bottom-[96px] z-[10]
-    text-sm rounded bg-[#252F39]
-    pointer-events-auto
-  ;
-}
-
-.controller-footer {
-  @apply
-    w-full py-6
-    absolute bottom-0
-    flex gap-4 items-center justify-center
+    w-1/2 min-w-[360px] max-w-full py-4 px-4 rounded-lg shadow-2xl
+    absolute left-1/2 top-0 sm:translate-y-4
+    -translate-x-1/2 translate-y-[80px]
+    flex gap-4 justify-between items-center
     bg-[#252F39]
+    outline outline-1 outline-[gray] -outline-offset-4
     pointer-events-auto
   ;
-  animation: controller-footer-anime-in 150ms ease;
 }
 </style>
