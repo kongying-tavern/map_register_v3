268a26239e0010d1207bcd3c95fa874522b19b28
夕云葛城 <KuroNeko7520@outlook.com>
2025-03-19 01:14:16 +0800

    feat(ModuleNetwork.vue): 添加导出 ws 通信日志的功能

--- src/components/AppSettings/modules/ModuleNetwork.vue
+++ src/components/AppSettings/modules/ModuleNetwork.vue
@@ -2,15 +2,28 @@
 import { WEBSOCKET_WORKER_CONFIG } from '@/configs'
 import { usePreferenceStore, useSocketStore, useUserStore } from '@/stores'
 import { SettingBar, SettingGroup, SettingPanel } from '../components'
+import { SocketStatus } from '@/shared'
+import { useFetchHook } from '@/hooks'
+import { wsdb } from '@/database'
+import { liveQuery } from 'dexie'
+import { useSubscription } from '@vueuse/rxjs'
+import type { WS } from '@/worker/webSocket/types'
 
 const socketStore = useSocketStore()
 const userStore = useUserStore()
 const preferenceStore = usePreferenceStore()
 
 const text = computed(() => {
-  return socketStore.status === WebSocket.OPEN
-    ? `${socketStore.delay} ms`
-    : '未连接'
+  return SocketStatus[socketStore.status]
+})
+
+const linkDisabled = computed(() => {
+  if (!userStore.info)
+    return true
+  const { status } = socketStore
+  if (status === SocketStatus.OPEN || status === SocketStatus.CONNECTING)
+    return true
+  return false
 })
 
 const wsEvents: { label: string, value: API.WSEventType, divider?: boolean }[] = [
@@ -28,17 +41,46 @@ const wsEvents: { label: string, value: API.WSEventType, divider?: boolean }[] =
   // 点位关联
   { label: '关联新增', value: 'MarkerLinked' },
   { label: '关联数据库更新', value: 'MarkerLinkageBinaryPurged' },
-  // 物品
-  { label: '物品数据库更新', value: 'ItemBinaryPurged' },
   // 图标
   { label: '图标数据库更新', value: 'IconTagBinaryPurged' },
   // 公告
   { label: '公告新增', value: 'NoticeAdded' },
   { label: '公告删除', value: 'NoticeDeleted' },
   { label: '公告更新', value: 'NoticeUpdated' },
-  // 用户
-  { label: '用户踢出', value: 'UserKickedOut' },
 ]
+
+const allowNum = computed(() => {
+  const allKeys = wsEvents.reduce((set, { value }) => set.add(value), new Set<string>())
+  const archivedKeys = new Set(preferenceStore.noticeEvents)
+  return allKeys.intersection(archivedKeys).size
+})
+
+const wsLogs = shallowRef({
+  total: 0,
+  list: [] as WS.Log[],
+})
+
+useSubscription(liveQuery(() => wsdb.logs.toArray()).subscribe((logs) => {
+  wsLogs.value = { total: logs.length, list: logs.slice(-10) }
+}))
+
+const { refresh: exportLogs, loading: exportLoading } = useFetchHook({
+  onRequest: async () => {
+    const logs = await wsdb.logs.toArray()
+    const encoder = new TextEncoder()
+    const uarray = encoder.encode(JSON.stringify(logs))
+    const blob = new Blob([uarray], {
+      type: 'application/json'
+    })
+    const a = document.createElement('a')
+    const url = URL.createObjectURL(blob)
+    a.href = url
+    a.download = `${new Date().toLocaleString()}.json`
+    a.click()
+    a.remove()
+    URL.revokeObjectURL(url)
+  },
+})
 </script>
 
 <template>
@@ -50,9 +92,26 @@ const wsEvents: { label: string, value: API.WSEventType, divider?: boolean }[] =
         </template>
       </SettingBar>
 
+      <SettingBar label="日志" :note="`latest 10 of ${wsLogs.total}`">
+        <template #setting>
+          <el-button :disabled="exportLoading" @click="exportLogs">
+            导出日志
+          </el-button>
+        </template>
+        <template #detail>
+          <div
+            v-for="log in wsLogs.list"
+            :key="log.id"
+            class="text-xs"
+          >
+            <b>{{ `[${new Date(log.t).toLocaleString()}]` }}</b> {{ log.msg }}
+          </div>
+        </template>
+      </SettingBar>
+
       <SettingBar label="操作">
         <template #setting>
-          <el-button @click="() => socketStore.connect(userStore.info?.id)">
+          <el-button :disabled="linkDisabled" @click="() => socketStore.connect(userStore.info?.id)">
             连接
           </el-button>
         </template>
@@ -60,7 +119,7 @@ const wsEvents: { label: string, value: API.WSEventType, divider?: boolean }[] =
 
       <SettingBar
         label="允许 socket 事件通知"
-        :note="preferenceStore.enableNotice ? `已允许 ${preferenceStore.noticeEvents.length ?? 0} 个事件的通知` : '已关闭通知功能'"
+        :note="preferenceStore.enableNotice ? `已允许 ${allowNum} 个事件的通知` : '已关闭通知功能'"
         :detail-disabled="!preferenceStore.enableNotice"
       >
         <template #setting>
