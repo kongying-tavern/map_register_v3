dff8022cadf88a9401700a8b4375dac0a115c980
夕云葛城 <KuroNeko7520@outlook.com>
2025-04-14 04:08:40 +0800

    feat(shared): [iconStyle] 新增名称映射

--- src/shared/iconStyle.ts
+++ src/shared/iconStyle.ts
@@ -9,3 +9,10 @@ export enum IconStyle {
   /** 类神瞳无对钩 */
   NO_TICK = 3,
 }
+
+export const ICON_STYLE_NAME_MAP = new Map<number, string>([
+  [IconStyle.DEFAULT, '默认图标'],
+  [IconStyle.NO_BORDER, '无边框'],
+  [IconStyle.PUPIL, '类神瞳'],
+  [IconStyle.NO_TICK, '类神瞳无对钩'],
+])
