294aa36d68047270c20845f0ceb2274f289d6586
夕云葛城 <KuroNeko7520@outlook.com>
2025-04-14 03:20:22 +0800

    feat(hooks): [useBinaryFlags] 使该 hook 支持非响应式值

--- src/hooks/useBinaryFlag.ts
+++ src/hooks/useBinaryFlag.ts
@@ -1,16 +1,18 @@
 /**
  * 将一个数值转换为二进制标识列表进行处理
  */
-export const useBinaryFlag = <T extends Record<string, number>>(flagNumber: Ref<number | undefined>, binaryIndexMap: T) => {
+export const useBinaryFlag = <T extends Record<string, number>>(flagNumber: MaybeRef<number | undefined>, binaryIndexMap: T) => {
   const flagBoolMap = computed({
     get: () => {
-      const binaryBits = (flagNumber.value ?? 0).toString(2).split('').map(Number).reverse()
+      const binaryBits = (toValue(flagNumber) ?? 0).toString(2).split('').map(Number).reverse()
       const boolMap = {} as Record<keyof typeof binaryIndexMap, boolean>
       for (const key in binaryIndexMap)
         boolMap[key] = Boolean(binaryBits[binaryIndexMap[key]])
       return boolMap
     },
     set: (boolMap) => {
+      if (!isRef(flagNumber))
+        return false
       const binaryBits: number[] = []
       for (const key in binaryIndexMap)
         binaryBits[binaryIndexMap[key]] = Number(boolMap[key])
