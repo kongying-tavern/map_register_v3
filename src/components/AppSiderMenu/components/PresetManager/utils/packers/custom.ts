c7fac8858fdd9d6f7ac96ca6d997561905287f49
Alex Fang <deeno@qq.com>
2026-08-06 00:32:14 +0800

    feat(components): [linkActionPacker] 添加高级过滤器二进制打包器：点位关联类型

--- src/components/AppSiderMenu/components/PresetManager/utils/packers/custom.ts
+++ src/components/AppSiderMenu/components/PresetManager/utils/packers/custom.ts
@@ -1,11 +1,13 @@
 import type { ValuePacker } from './types'
 import { MAFModelId } from '@/shared'
 import { customIdRangePacker } from './customIdRange'
+import { customLinkActionPacker } from './customLinkAction'
 import { msgpackPacker } from './msgpack'
 
 /** 自定义打包器映射：key 为 MAFModelId，缺省时使用默认 msgpack 打包器 */
 export const valuePackers: Partial<Record<MAFModelId, ValuePacker>> = {
   [MAFModelId.ID_RANGE]: customIdRangePacker,
+  [MAFModelId.LINKAGE_ACTION]: customLinkActionPacker,
 }
 
 /** 获取指定过滤条件的打包器 */
